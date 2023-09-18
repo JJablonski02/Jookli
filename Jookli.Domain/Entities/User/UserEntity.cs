c5b9258937a926dc8ff118f03346290ef2a849a6
JJablonski02 <kuba2805@icloud.com>
2023-09-18 23:00:52 +0200

    User entity

---
+++ Jookli.Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using Jookli.Domain.Entities.Message;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jookli.Domain.Entities.User
+{
+    public class User
+    {
+        [Key]
+        public Guid UserID { get; set; }
+        [StringLength(50)]
+        public string? UserName { get; set; }
+        [StringLength(50)]
+        public string? Email { get; set; }
+        public bool IsSystemAccount { get; set; }
+        public bool Premium { get; set; }
+        public ICollection<MessageReceived>? MessagesReceived { get; set; }
+        public ICollection<MessageSent>? MessagesSent { get; set; }
+    }
+}
