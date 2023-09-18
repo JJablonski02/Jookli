ad9067b3e031b55994d2763ff24f7eabdf7f303f
JJablonski02 <kuba2805@icloud.com>
2023-09-18 23:01:49 +0200

    Message response dto

---
+++ Jookli.Application/DTO/MessageResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.DTO
+{
+    public class MessageResponse
+    {
+        public int PhoneNumber { get; set; }
+        public string? Message {  get; set; }
+    }
+}
