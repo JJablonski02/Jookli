18b83ca032baa9897a0d6a243f74dc6e94ae35eb
JJablonski02 <kuba2805@icloud.com>
2023-09-18 23:01:21 +0200

    IMessageAdderService

---
+++ Jookli.Application/ServiceContracts/IMessageAdderService.cs
@@ -0,0 +1,14 @@
+﻿using Jookli.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.ServiceContracts
+{
+    internal interface IMessageAdderService
+    {
+        Task<MessageResponse> AddMessage(MessageAddRequest? messageAddRequest);
+    }
+}
