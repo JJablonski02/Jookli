2b5b352139d1bb6e8072001b762aae2cf2c8a3f6
JJablonski02 <kuba2805@icloud.com>
2023-09-18 22:06:37 +0200

    postMessage

---
+++ Jookli.Api/Modules/Messages/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.Messages
+{
+    public class MessageController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostMessage(string message)
+        {
+            return Ok();
+        }
+    }
+}
