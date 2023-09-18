ea3e2a882464a44e56a1b79b505923e653616023
JJablonski02 <kuba2805@icloud.com>
2023-09-18 23:01:02 +0200

    message received entity

---
+++ Jookli.Domain/Entities/Message/MessageReceived.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Domain.Entities.Message
+{
+    public class MessageReceived
+    {
+        public Guid MessageId { get; set; }
+        public DateTime MessageTime { get; set; }
+        public string? MessageContent { get; set; }
+    }
+}
