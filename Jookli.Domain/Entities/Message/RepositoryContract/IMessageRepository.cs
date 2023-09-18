6084302ee1db6a05ceb45a745682ddf46082c39a
JJablonski02 <kuba2805@icloud.com>
2023-09-19 00:09:59 +0200

    IMessageRepository content

---
+++ Jookli.Domain/Entities/Message/RepositoryContract/IMessageRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Domain.Entities.Message.RepositoryContract
+{
+    public interface IMessageRepository
+    {
+        /// <summary>
+        /// Adds a message object to the data store
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>Returns the message object after adding it to the table</returns>
+        Task<MessageEntity> AddMessage(MessageEntity message);
+
+        /// <summary>
+        /// Returns message in the data store
+        /// </summary>
+        /// <param name="messageID"></param>
+        /// <returns>Returns message from table</returns>
+        Task<MessageEntity> GetMessage(Guid messageID);
+    }
+}
