e63059aae45cc171fdd3efa7cc52aff59e30e2e6
JJablonski <kuba2805@icloud.com>
2023-09-21 08:05:44 +0200

    message getter service comments and new getter

--- Jookli.Application/ServiceContracts/IMessageGetterService.cs
+++ Jookli.Application/ServiceContracts/IMessageGetterService.cs
@@ -4,6 +4,19 @@ namespace Jookli.Application.ServiceContracts
 {
     public interface IMessageGetterService
     {
+        /// <summary>
+        /// Get message by any content
+        /// </summary>
+        /// <param name="messageResponse"></param>
+        /// <returns>Returns all found matching messages</returns>
         Task<MessageRequest> GetMessage(MessageResponse? messageResponse);
+
+        /// <summary>
+        /// Get message by Id
+        /// </summary>
+        /// <param name="messageId"></param>
+        /// <returns>Returns message details</returns>
+        Task<MessageRequest> GetMessageById(Guid messageId);
+
     }
 }
