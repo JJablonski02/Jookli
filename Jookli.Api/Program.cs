429b0c61b1ea5361f698f9499f36ca4c3c9af28b
JJablonski <kuba2805@icloud.com>
2023-09-21 10:24:30 +0200

    first conf of program.cs

--- Jookli.Api/Program.cs
+++ Jookli.Api/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
+app.UseHsts();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
 
