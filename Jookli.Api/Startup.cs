52dfd6bfe772e3ab7e9097984685f26fffc12fc5
JJablonski02 <kuba2805@icloud.com>
2023-09-22 23:24:45 +0200

    startup class

---
+++ Jookli.Api/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace Jookli.Api
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment webHostEnvironment, IServiceProvider provider)
+        {
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            if (webHostEnvironment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
