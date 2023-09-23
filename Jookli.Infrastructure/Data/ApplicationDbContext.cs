286aaeab9e08680690164a0f01f80b59892f8cfc
JJablonski02 <kuba2805@icloud.com>
2023-09-23 21:56:04 +0200

    first dbContext

--- Jookli.Infrastructure/Data/ApplicationDbContext.cs
+++ Jookli.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Jookli.Domain.Entities.User;
 
 namespace Jookli.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public virtual DbSet<UserEntity> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
