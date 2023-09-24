a0f1404ec46381d6b9509264ba8163bac710d10e
JJablonski02 <kuba2805@icloud.com>
2023-09-24 21:26:06 +0200

    connectionstring service

--- Jookli.Api/Startup.cs
+++ Jookli.Api/Startup.cs
@@ -1,19 +1,32 @@
-﻿namespace Jookli.Api
+﻿using Jookli.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jookli.Api
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public void  ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            
+            services.AddEndpointsApiExplorer();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment webHostEnvironment, IServiceProvider provider)
         {
             app.UseStaticFiles();
-
            
 
-            if (webHostEnvironment.IsDevelopment())
+            if (!webHostEnvironment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
