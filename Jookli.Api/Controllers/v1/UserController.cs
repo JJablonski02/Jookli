2ae5e0c570f17e45b0e74aa41fa41cbdda807da1
JJablonski02 <kuba2805@icloud.com>
2023-09-25 22:35:54 +0200

    User controller first getter

---
+++ Jookli.Api/Controllers/v1/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Jookli.Domain.Entities.User;
+using Jookli.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jookli.Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class UserController : CustomControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUsers()
+        {
+            if (_context.User == null)
+            {
+                return NotFound();
+            }
+            return await _context.User.ToListAsync();
+        }
+    }
+}
