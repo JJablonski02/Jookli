7306656f159f1f7de08bae5e8b632ffd07c95861
JJablonski02 <kuba2805@icloud.com>
2023-09-25 21:50:57 +0200

    base controller

---
+++ Jookli.Api/CustomControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class CustomControllerBase : ControllerBase
+    {
+    }
+}
