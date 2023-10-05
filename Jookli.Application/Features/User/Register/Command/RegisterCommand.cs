3e5b1b0c7d33e06a4103170b9cff7479cc078799
JJablonski02 <kuba2805@icloud.com>
2023-10-05 22:42:57 +0200

    register dto in mediator

---
+++ Jookli.Application/Features/User/Register/Command/RegisterCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.User.Register.Command
+{
+    public class RegisterCommand
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage =("Password and Confirm password must match"))]
+        public string ConfirmPassword { get; set; }
+    }
+}
