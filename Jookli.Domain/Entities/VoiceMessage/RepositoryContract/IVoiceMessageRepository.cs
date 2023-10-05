be1ba0a79211b6751326a6454b2afa1689503965
JJablonski02 <kuba2805@icloud.com>
2023-10-05 20:30:23 +0200

    IVoiceMessageRepository

---
+++ Jookli.Domain/Entities/VoiceMessage/RepositoryContract/IVoiceMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Domain.Entities.VoiceMessage.RepositoryContract
+{
+    public interface IVoiceMessageRepository
+    {
+        Task AddVoiceMessageAsync(VoiceMessageEntity voiceMessage);
+        Task<VoiceMessageEntity?> GetVoiceMessageByIdAsync(Guid voiceMessageId);
+    }
+}
