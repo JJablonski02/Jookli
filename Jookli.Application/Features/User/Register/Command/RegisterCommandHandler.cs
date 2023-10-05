9b37742a79e360094d0ecab5469e63cb09e67139
JJablonski02 <kuba2805@icloud.com>
2023-10-05 22:42:42 +0200

    Mediator pattern implementing

---
+++ Jookli.Application/Features/User/Register/Command/RegisterCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Jookli.Domain.Entities.User.RepositoryContract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.User.Register.Command
+{
+    public class RegisterCommandHandler : IRequest<RegisterCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RegisterCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository= userRepository;
+        }
+
+        public async Task<Unit> Handle(RegisterCommand command)
+        {
+            var userEmail = _userRepository.GetByUserEmailAsync(command.Email);
+
+            if(userEmail is not null)
+            {
+                throw new ArgumentException($"User with email: {command.Email} exists");
+            }
+
+
+
+            return Unit.Value;
+        }
+    }
+}
