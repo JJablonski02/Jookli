ff2b31ed5727e599691b82eda969b974ef5e44cb
JJablonski02 <kuba2805@icloud.com>
2023-10-05 23:26:42 +0200

    login command implementation

---
+++ Jookli.Application/Features/User/Login/Command/LoginCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.User.Login.Command
+{
+    internal class LoginCommand
+    {
+    }
+}
