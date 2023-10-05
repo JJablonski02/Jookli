f7695cfa54277e9eb6497d6f3d26fb9a80c39024
JJablonski02 <kuba2805@icloud.com>
2023-10-05 19:49:24 +0200

    VoiceMessageEntity

---
+++ Jookli.Domain/Entities/VoiceMessage/VoiceMessageEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Domain.Entities.VoiceMessage
+{
+    public class VoiceMessageEntity
+    {
+        public Guid VoiceMessageId { get; set; }
+        public DateTime VoiceMessageDateTime { get; set; }
+        public decimal VoiceMessageLength { get; set; }
+    }
+}
