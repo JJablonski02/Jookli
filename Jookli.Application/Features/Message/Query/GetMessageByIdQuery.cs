7f18d71c2245f74530dbbd8a842d331e9f5cd599
JJablonski02 <kuba2805@icloud.com>
2023-10-05 23:26:58 +0200

    get message by id query

---
+++ Jookli.Application/Features/Message/Query/GetMessageByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.Message.Query
+{
+    internal class GetMessageByIdQuery
+    {
+    }
+}
