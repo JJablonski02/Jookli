b6f121de0fd8c60fd4aa8036e56ffc01714bc50a
JJablonski02 <kuba2805@icloud.com>
2023-10-05 23:27:14 +0200

    create user implementation

--- Jookli.Api/Controllers/v1/UserController.cs
+++ Jookli.Api/Controllers/v1/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Jookli.Domain.Entities.User;
+﻿using Jookli.Application.Features.User.Register.Command;
+using Jookli.Application.ServiceContracts;
+using Jookli.Domain.Entities.User;
 using Jookli.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +11,7 @@ namespace Jookli.Api.Controllers.v1
     public class UserController : CustomControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IUserService _userService;
         public UserController(ApplicationDbContext context)
         {
             _context = context;
@@ -22,6 +25,14 @@ namespace Jookli.Api.Controllers.v1
                 return NotFound();
             }
             return await _context.User.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterCommand command)
+        {
+            await _userService.ExecuteCommandAsync(command);
+
+            return Ok();
         }
     }
 }
