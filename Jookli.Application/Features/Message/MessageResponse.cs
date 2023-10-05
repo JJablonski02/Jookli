3fce34a9fbd1ac3ff3f53813de501fd571d908c4
JJablonski02 <kuba2805@icloud.com>
2023-10-05 19:50:22 +0200

    clean code

---
+++ Jookli.Application/Features/Message/MessageResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.Message
+{
+    public class MessageResponse
+    {
+        public Guid MessageId { get; set; }
+        public DateTime MessageTime { get; set; }
+        public int MessagePhoneNumber { get; set; }
+        public string? MessageContent { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj.GetType() != typeof(MessageResponse))
+            {
+                return false;
+            }
+            MessageResponse messageResponse = (MessageResponse)obj;
+
+            return MessageId == messageResponse.MessageId &&
+                MessageTime == messageResponse.MessageTime &&
+                MessagePhoneNumber == messageResponse.MessagePhoneNumber &&
+                MessageContent == messageResponse.MessageContent;
+        }
+    }
+}
