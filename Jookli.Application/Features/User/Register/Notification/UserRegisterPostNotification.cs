f528015338ea3efb22f64a5feeda365de65afb19
JJablonski02 <kuba2805@icloud.com>
2023-10-09 18:46:20 +0200

    UserRegisterPostNotification

---
+++ Jookli.Application/Features/User/Register/Notification/UserRegisterPostNotification.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.User.Register.Notification
+{
+    public class UserRegisterPostNotification : INotification
+    {
+        public string WhatToWrite { get; set; }
+    }
+}
