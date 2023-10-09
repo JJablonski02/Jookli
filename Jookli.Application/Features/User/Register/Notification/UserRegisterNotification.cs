d0e11ec11b05f3fab9ce107a0dd181f4cf2a7908
JJablonski02 <kuba2805@icloud.com>
2023-10-09 18:46:10 +0200

    UserRegisterNotification

---
+++ Jookli.Application/Features/User/Register/Notification/UserRegisterNotification.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Features.User.Register.Notification
+{
+    public class UserRegisterNotification : INotificationHandler<UserRegisterPostNotification>
+    {
+        public Task Handle(UserRegisterPostNotification notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
