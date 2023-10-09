3a19d98353a208974e3337207cff6c5712695aea
JJablonski02 <kuba2805@icloud.com>
2023-10-09 20:52:57 +0200

    user composition root created

---
+++ Jookli.Infrastructure/Configuration/Processing/UserCompositionRoot.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration.Processing
+{
+    internal static class UserCompositionRoot
+    {
+        private static IContainer _container;
+
+        internal static void SetContainer(IContainer container)
+        {
+            _container = container;
+        }
+
+        internal static ILifetimeScope BeginLifetimeScope()
+        {
+            return _container.BeginLifetimeScope();
+        }
+    }
+}
