f6a24b8d179e5c62cf7213b0c067b8c1745bad68
JJablonski02 <kuba2805@icloud.com>
2023-10-09 20:52:42 +0200

    Implementation of commandsExecutor for Mediator

---
+++ Jookli.Infrastructure/Configuration/Processing/CommandsExecutor.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using MediatR;
+
+
+namespace Jookli.Infrastructure.Configuration.Processing
+{
+    internal static class CommandsExecutor
+    {
+        internal static async Task Execute(IRequest request)
+        {
+            using (var scope = UserCompositionRoot.BeginLifetimeScope())
+            {
+                var mediator = scope.Resolve<IMediator>();
+                await mediator.Send(request);
+            }
+        }
+    }
+}
