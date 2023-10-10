8b83df57e3ff117ac8a8a4e24f6b42e50055a3b9
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:56:30 +0200

    integration event

---
+++ Jookli.Infrastructure/Configuration/EventsBus/IntegrationEvent.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration.EventsBus
+{
+    public abstract class IntegrationEvent : INotification
+    {
+        public Guid Id { get; set; }
+        public DateTime OccuredOn { get; set; }
+
+        protected IntegrationEvent (Guid id, DateTime occuredOn)
+        {
+            Id = id;
+            OccuredOn = occuredOn;
+        }
+    }
+}
