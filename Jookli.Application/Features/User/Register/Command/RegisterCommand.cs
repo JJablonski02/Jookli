afa6fe8e47af67c78232c66d5bbf93e74c29111a
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:57:33 +0200

    register command changed to record - immutable

--- Jookli.Application/Features/User/Register/Command/RegisterCommand.cs
+++ Jookli.Application/Features/User/Register/Command/RegisterCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Jookli.Application.Features.User.Register.Command
 {
-    public class RegisterCommand
+    public record RegisterCommand : IRequest
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
