e3b1b115639b3c09a1deeb26af3a327e27b7cd65
JJablonski02 <kuba2805@icloud.com>
2023-10-10 22:13:23 +0200

    CommandBase abstract class implemented

---
+++ Jookli.Application/ServiceContracts/CommandBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.ServiceContracts
+{
+    public abstract class CommandBase
+    {
+        public Guid Id { get; set; }
+
+        protected CommandBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        protected CommandBase(Guid id)
+        {
+            Id = id;
+        }
+    }
+
+    public abstract class CommandBase<TResult> : ICommand<TResult>
+    {
+        public Guid Id { get; set; }
+        protected CommandBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        protected CommandBase(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
