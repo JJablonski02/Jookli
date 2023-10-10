207d1937ec98f7198f13b7053ccc1f6b3d5bf0a2
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:56:55 +0200

    event bus

---
+++ Jookli.Infrastructure/Configuration/EventsBus/IEventBus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration.EventsBus
+{
+    public interface IEventBus : IDisposable
+    {
+        Task Publish<T>(T @event)
+            where T : IntegrationEvent;
+
+        void Subscribe<T>(IIntegrationEventHandler<T> handler)
+            where T : IntegrationEvent;
+
+        void StartConsuming();
+    }
+}
