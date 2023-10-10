67a75bde69c844b3a8f9568b71f4f8dc360e8fbc
JJablonski02 <kuba2805@icloud.com>
2023-10-10 22:17:45 +0200

    Command pattern implemented

--- Jookli.Application/ServiceContracts/IIdentityService.cs
+++ Jookli.Application/ServiceContracts/IIdentityService.cs
@@ -5,6 +5,7 @@ namespace Jookli.Application.ServiceContracts
 {
     public interface IIdentityService
     {
-        Task ExecuteCommandAsync (IRequest request);
+        Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+        Task ExecuteCommandAsync (ICommand command);
     }
 }
