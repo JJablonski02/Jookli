ddcb04c821b5d14fd0c0940f3ff6b4039a3fe31a
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:56:15 +0200

    mediator module

---
+++ Jookli.Infrastructure/Configuration/Mediator/MediatorModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using MediatR;
+using System.Reflection;
+
+namespace Jookli.Infrastructure.Configuration.Mediator
+{
+    public class MediatorModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly);
+        }
+    }
+}
