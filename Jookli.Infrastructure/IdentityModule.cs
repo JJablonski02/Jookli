9f133d86893bcafeee140b5118fc6f1d8f18e854
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:54:44 +0200

    Refactor identitymodule

---
+++ Jookli.Infrastructure/IdentityModule.cs
@@ -0,0 +1,19 @@
+﻿using Jookli.Application.ServiceContracts;
+using Jookli.Infrastructure.Configuration.Processing;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure
+{
+    public class IdentityModule : IIdentityService
+    {
+        public async Task ExecuteCommandAsync (IRequest request)
+        {
+             await CommandsExecutor.Execute(request);
+        }
+    }
+}
