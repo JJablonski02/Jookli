255b58b94068baa5a47e03f82dcfe1490b504d8c
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:57:03 +0200

    event bus startup

---
+++ Jookli.Infrastructure/Configuration/EventsBus/EventsBusStartup.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Jookli.Infrastructure.Configuration.Processing;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration.EventsBus
+{
+    public static class EventsBusStartup
+    {
+        private static void SubscribeIntegrationEvents(ILogger logger)
+        {
+            var eventBus = UserCompositionRoot.BeginLifetimeScope().Resolve<IEventBus>();
+        }
+    }
+}
