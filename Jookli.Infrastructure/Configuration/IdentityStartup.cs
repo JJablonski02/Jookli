6ef7499132a479d5efce46c20d3e13199bacce70
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:55:03 +0200

    registering modules in identityStartup

---
+++ Jookli.Infrastructure/Configuration/IdentityStartup.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Jookli.Infrastructure.Configuration.Mediator;
+using Jookli.Infrastructure.Configuration.Processing;
+using Serilog;
+
+namespace Jookli.Infrastructure.Configuration
+{
+    public class IdentityStartup
+    {
+        private static IContainer _container;
+
+        public static void Initialize(string connectionString, ILogger logger)
+        {
+            ConfigureCompositionRoot(connectionString, logger);
+        }
+        private static void ConfigureCompositionRoot(string connectionString, ILogger logger)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterModule(new MediatorModule());
+            containerBuilder.RegisterModule(new ProcessingModule());
+
+            _container= containerBuilder.Build();
+
+            UserCompositionRoot.SetContainer(_container);
+        }
+    }
+}
