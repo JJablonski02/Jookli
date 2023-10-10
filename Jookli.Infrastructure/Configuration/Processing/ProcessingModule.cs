a77d4ff808ccdea2511bc1be423ea29211f3f316
JJablonski02 <kuba2805@icloud.com>
2023-10-10 20:55:57 +0200

    processing module

---
+++ Jookli.Infrastructure/Configuration/Processing/ProcessingModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Jookli.Application.Features.User.Register.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration.Processing
+{
+    internal class ProcessingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<RegisterCommand>();
+        }
+    }
+}
