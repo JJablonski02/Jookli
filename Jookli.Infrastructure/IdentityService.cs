dda4bda5a5bd3340fb17341e114e64535909ba4d
JJablonski02 <kuba2805@icloud.com>
2023-10-11 21:08:04 +0200

    identity service new dependencies

---
+++ Jookli.Infrastructure/IdentityService.cs
@@ -0,0 +1,23 @@
+﻿using Jookli.Application.ServiceContracts;
+using Jookli.Infrastructure.Configuration.Processing;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure
+{
+    public class IdentityService : IIdentityService
+    {
+        public async Task ExecuteCommandAsync(ICommand command)
+        {
+            await CommandsExecutor.Execute(command);
+        }
+        public async Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command)
+        {
+            return await CommandsExecutor.Execute(command);
+        }
+    }
+}
