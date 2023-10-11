e276bcaf531d3475db584231808aef2f0fca7d02
JJablonski02 <kuba2805@icloud.com>
2023-10-11 21:10:57 +0200

    Assembly class - contains reference to assembly

---
+++ Jookli.Infrastructure/Configuration/Assemblies.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Infrastructure.Configuration
+{
+    internal static class Assemblies
+    {
+        public static readonly Assembly Application = typeof(InternalCommandBase).Assembly;
+    }
+}
