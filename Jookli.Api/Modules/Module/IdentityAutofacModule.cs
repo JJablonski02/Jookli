e6e094fd79bcb9d8cc577a8d8b98ab821722ab58
JJablonski02 <kuba2805@icloud.com>
2023-10-11 21:16:26 +0200

    Identity user autofac module registered.

---
+++ Jookli.Api/Modules/Module/IdentityAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Jookli.Application.ServiceContracts;
+using Jookli.Infrastructure;
+
+namespace Jookli.Api.Modules.Module
+{
+    internal sealed class IdentityAutofacModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<IdentityService>()
+                .As<IIdentityService>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
