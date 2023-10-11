3736608a78a305094f8bf7fee8a2ba7c25bbd15e
JJablonski02 <kuba2805@icloud.com>
2023-10-11 21:16:07 +0200

    Internal command base abstract class created.

---
+++ Jookli.Application/Configuration/Command/InternalCommandBase.cs
@@ -0,0 +1,34 @@
+﻿using Jookli.Application.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Application.Configuration.Command
+{
+    public abstract class InternalCommandBase : ICommand
+    {
+        public Guid Id { get; }
+
+        protected InternalCommandBase(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+
+    public abstract class InternalCommandBase<TResult> : ICommand<TResult>
+    {
+        public Guid Id { get; }
+
+        public InternalCommandBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public InternalCommandBase(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
