b60670e319988d6f87f404a0b1a2c57ca2599418
JJablonski02 <kuba2805@icloud.com>
2023-10-12 22:30:17 +0200

    useraccescontext

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Data/UserAccessContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Jookli.UserAccess.Domain.Entities.User;
+using Jookli.UserAccess.Domain.Entities.VoiceMessage;
+using Jookli.UserAccess.Domain.Entities.Message;
+using Microsoft.Extensions.Logging;
+
+namespace Jookli.UserAccess.Infrastructure.Data
+{
+    public class UserAccessContext : DbContext
+    {
+        private readonly ILoggerFactory _logger;
+        public UserAccessContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        }
+
+        public virtual DbSet<UserEntity> User { get; set; }
+        public virtual DbSet<VoiceMessageEntity> VoiceMessage { get; set; }
+        public virtual DbSet<MessageEntity> MessageEntity { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
