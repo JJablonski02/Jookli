9d4f0bbd95e7d4ce71b179b466c3020b5bf45550
JJablonski02 <kuba2805@icloud.com>
2023-10-12 22:29:46 +0200

    useraccescontext

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Jookli.UserAccess.Infrastructure.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(UserAccessContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
