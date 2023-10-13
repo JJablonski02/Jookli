01262aacd22c3c6f887ec0871dd6690eca1fcddf
JJablonski02 <kuba2805@icloud.com>
2023-10-13 21:43:46 +0200

    Correlation Middleware build in progress

---
+++ src/API/Jookli.Api/Modules/Configuration/ExecutionContext/CorrelationMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace Jookli.Api.Modules.Configuration.ExecutionContext
+{
+    internal class CorrelationMiddleware
+    {
+        internal const string CorrelationHeaderKey = "CorrelationId";
+        private readonly RequestDelegate _next;
+
+        public CorrelationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+        }
+    }
+}
---
+++ src/API/Jookli.Api/Modules/Configuration/ExecutionContext/ExecutionContextAccessor.cs
@@ -0,0 +1,6 @@
+﻿namespace Jookli.Api.Modules.Configuration.ExecutionContext
+{
+    public class ExecutionContextAccessor
+    {
+    }
+}
