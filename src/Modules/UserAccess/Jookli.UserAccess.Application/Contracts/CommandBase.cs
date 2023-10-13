b43cea20012e12c5c6e3cf6554c801f42d92d567
JJablonski02 <kuba2805@icloud.com>
2023-10-13 21:43:07 +0200

    bug fix

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/CommandBase.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/CommandBase.cs
@@ -8,7 +8,7 @@ namespace Jookli.UserAccess.Application.Contracts
 {
     public abstract class CommandBase : ICommand
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; }
 
         protected CommandBase()
         {
@@ -23,7 +23,7 @@ namespace Jookli.UserAccess.Application.Contracts
 
     public abstract class CommandBase<TResult> : ICommand<TResult>
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; }
         protected CommandBase()
         {
             this.Id = Guid.NewGuid();
