ce2531999ada55a6aedbac12bb4fa4544d1d3629
JJablonski02 <kuba2805@icloud.com>
2023-10-14 00:23:44 +0200

    constructor adjustments in UserAccessStartup

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/UserAccessStartup.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/UserAccessStartup.cs
@@ -3,23 +3,26 @@ using Jookli.UserAccess.Infrastructure.Configuration.Mediation;
 using Jookli.UserAccess.Infrastructure.Configuration.Processing;
 using Jookli.UserAccess.Infrastructure.Configuration;
 using Serilog;
+using Jookli.BuildingBlocks.Application;
 
 namespace Jookli.UserAccess.Infrastructure.Configuration
 {
-    public class UserAccessStartup
+    public static class UserAccessStartup
     {
         private static IContainer _container;
 
-        public static void Initialize(string connectionString, ILogger logger)
+        public static void Initialize(string connectionString, ILogger logger, IExecutionContextAccessor executionContextAccessor)
         {
-            ConfigureCompositionRoot(connectionString, logger);
+            ConfigureCompositionRoot(connectionString, logger, executionContextAccessor);
         }
-        private static void ConfigureCompositionRoot(string connectionString, ILogger logger)
+        private static void ConfigureCompositionRoot(string connectionString, ILogger logger, IExecutionContextAccessor executionContextAccessor)
         {
             var containerBuilder = new ContainerBuilder();
 
             containerBuilder.RegisterModule(new MediatorModule());
             containerBuilder.RegisterModule(new ProcessingModule());
+
+            containerBuilder.RegisterInstance(executionContextAccessor);
 
             _container= containerBuilder.Build();
 
