cc1349f094986b7e518246f04bce04355a5bdd46
JJablonski02 <kuba2805@icloud.com>
2023-10-13 21:46:59 +0200

    IExectuionContextAccessor implemented in building blocks application layer

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Application/IExecutionContextAccessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Application
+{
+    public interface IExecutionContextAccessor
+    {
+        Guid UserId { get; }
+        Guid CorrelationId { get; }
+        bool IsAvailable { get; }
+    }
+}
