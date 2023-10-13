b18d9570d3bcabf83cd9d03255d2163bb7ce0247
JJablonski02 <kuba2805@icloud.com>
2023-10-13 22:00:37 +0200

    Implementing AttributeAuthHandler

---
+++ src/API/Jookli.Api/Modules/Configuration/Authorization/AttributeAuthorizationHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Jookli.Api.Modules.Configuration.Authorization
+{
+    public abstract class AttributeAuthorizationHandler<TRequirement, TAttribute>
+        : AuthorizationHandler<TRequirement>
+        where TRequirement : IAuthorizationRequirement
+        where TAttribute : Attribute
+    {
+       
+    }
+}
