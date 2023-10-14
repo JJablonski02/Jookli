b643a1b638876abe9c14de52eccba73c23b9af8a
JJablonski02 <kuba2805@icloud.com>
2023-10-15 01:14:56 +0200

    register command tests

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommand.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommand.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,25 @@ namespace Jookli.UserAccess.Application.Features.User.Register.Command
 {
     public class RegisterCommand : CommandBase
     {
+        //public RegisterCommand(
+        //    string name,
+        //    string surname,
+        //    string username,
+        //    string email,
+        //    int phoneNumber,
+        //    string password,
+        //    string confirmPassword)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Surname = surname;
+        //    Username = username;
+        //    Email = email;
+        //    PhoneNumber = phoneNumber;
+        //    Password = password;
+        //    ConfirmPassword = confirmPassword;
+
+        //}
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -18,7 +38,6 @@ namespace Jookli.UserAccess.Application.Features.User.Register.Command
         public string Email { get; set; }
         public int PhoneNumber { get; set; }
         public string Password { get; set; }
-        [Compare("Password", ErrorMessage =("Password and Confirm password must match"))]
         public string ConfirmPassword { get; set; }
     }
 }
