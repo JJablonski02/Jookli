bed00b9d5ab5f774439a1a7b3da3123db1f311c2
JJablonski02 <kuba2805@icloud.com>
2023-10-14 22:59:49 +0200

    Resource owner password validator oauth2 main class implemented

---
+++ src/API/Jookli.Api/Modules/UserAccess/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Validation;
+using Jookli.UserAccess.Application.Authentication.Authenticate;
+using Jookli.UserAccess.Application.Contracts;
+
+namespace Jookli.Api.Modules.UserAccess
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly IUserAccessModule _userAccessModule;
+
+        public ResourceOwnerPasswordValidator(IUserAccessModule userAccessModule)
+        {
+            _userAccessModule = userAccessModule;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var authenticationResult = await _userAccessModule.ExecuteCommandAsync(
+                new AuthenticateCommand(context.UserName, context.Password));
+
+            if (!authenticationResult.IsAuthenticated)
+            {
+                context.Result = new GrantValidationResult(
+                    IdentityServer4.Models.TokenRequestErrors.InvalidGrant,
+                    authenticationResult.AuthenticationError);
+
+                return;
+            }
+            context.Result = new GrantValidationResult(
+
+                authenticationResult.User.Id.ToString(),
+                "forms",
+                authenticationResult.User.Claims
+                );
+        }
+    }
+}
