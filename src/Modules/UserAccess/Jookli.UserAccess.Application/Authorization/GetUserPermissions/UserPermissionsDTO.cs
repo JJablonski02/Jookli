29652d49786f77a16e2ade1d96e4f6340cccabf7
JJablonski02 <kuba2805@icloud.com>
2023-10-14 10:35:25 +0200

    User permission dto

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Authorization/GetUserPermissions/UserPermissionsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Authorization.GetUserPermissions
+{
+    public class UserPermissionsDTO
+    {
+        public string Code { get; set; }
+    }
+}
