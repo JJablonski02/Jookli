eb29d379aac30dbd4ac7abbb7d5e710ac88539a3
JJablonski02 <kuba2805@icloud.com>
2023-10-14 10:35:51 +0200

    get user permissions query inherits queryBase

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Authorization/GetUserPermissions/GetUserPermissionsQuery.cs
@@ -0,0 +1,19 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Authorization.GetUserPermissions
+{
+    public class GetUserPermissionsQuery : QueryBase<List<UserPermissionsDTO>>
+    {
+        public GetUserPermissionsQuery(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public Guid UserId { get; }
+    }
+}
