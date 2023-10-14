d2a2b65cb7ffc176378d20767d51bb8330acf3f9
JJablonski02 <kuba2805@icloud.com>
2023-10-14 10:36:24 +0200

    QueryBase implements IQuery

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/QueryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Contracts
+{
+    public abstract class QueryBase<TResult> : IQuery<TResult>
+    {
+        public Guid Id { get; }
+
+        protected QueryBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        protected QueryBase(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
