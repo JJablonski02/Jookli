c994626fdd47e3f03cabd3be7151af0346a8a25b
JJablonski02 <kuba2805@icloud.com>
2023-10-14 22:25:26 +0200

    custom types

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/CustomClaimTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Contracts
+{
+    internal class CustomClaimTypes
+    {
+        internal const string Roles = "roles";
+        internal const string Email = "email";
+        internal const string Name = "name";
+    }
+}
