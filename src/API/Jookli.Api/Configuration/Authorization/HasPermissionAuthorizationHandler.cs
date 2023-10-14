3366f8c1540d235a4617f4115caef3a0baf61fd2
JJablonski02 <kuba2805@icloud.com>
2023-10-14 11:04:19 +0200

    HasPermission auth handler implemented

---
+++ src/API/Jookli.Api/Configuration/Authorization/HasPermissionAuthorizationHandler.cs
@@ -0,0 +1,38 @@
+﻿using Jookli.BuildingBlocks.Application;
+using Jookli.UserAccess.Application.Authorization.GetUserPermissions;
+using Jookli.UserAccess.Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Jookli.Api.Configuration.Authorization
+{
+    internal class HasPermissionAuthorizationHandler : AttributeAuthorizationHandler<HasPermissionAuthorizationRequirement, HasPermissionAttribute>
+    {
+        private readonly IExecutionContextAccessor _executionContextAccessor;
+        private readonly IUserAccessModule _userAccessModule;
+
+        public HasPermissionAuthorizationHandler(IExecutionContextAccessor executionContextAccessor, IUserAccessModule userAccessModule)
+        {
+            _executionContextAccessor = executionContextAccessor;
+            _userAccessModule = userAccessModule;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, HasPermissionAuthorizationRequirement requirement, HasPermissionAttribute attribute)
+        {
+            var permissions = await _userAccessModule.ExecuteQueryAsync(new GetUserPermissionsQuery(_executionContextAccessor.UserId));
+
+            if(!await AuthorizeAsync(attribute.Name, permissions))
+            {
+                context.Fail();
+                return;
+            }
+        }
+
+        private Task<bool> AuthorizeAsync(string permission, List<UserPermissionsDTO> permissions)
+        {
+#if !DEBUG
+            return Task.FromResult(true);
+#endif
+            return Task.FromResult(permissions.Any(x => x.Code== permission));
+        }
+    }
+}
