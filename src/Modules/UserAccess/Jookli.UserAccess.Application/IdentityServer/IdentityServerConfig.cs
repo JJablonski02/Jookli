7cec89eff98692026f86e352aeba857af11ea6b8
JJablonski02 <kuba2805@icloud.com>
2023-10-14 22:22:48 +0200

    Started implementing identity server configuration in identity module. OAuth2

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/IdentityServer/IdentityServerConfig.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.IdentityServer
+{
+    public class IdentityServerConfig
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("JookliAPI", "Jookli API")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+
+        }
+    }
+}
