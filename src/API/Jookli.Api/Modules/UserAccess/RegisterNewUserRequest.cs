c30ff29540d8833636f5ef6facdbf953323d5c99
JJablonski02 <kuba2805@icloud.com>
2023-10-15 01:15:41 +0200

    temporary class

---
+++ src/API/Jookli.Api/Modules/UserAccess/RegisterNewUserRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Jookli.Api.Modules.UserAccess
+{
+    public class RegisterNewUserRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
