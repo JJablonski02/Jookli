4ce29898ed3dcd63e737d223366166bbf8399f80
JJablonski02 <kuba2805@icloud.com>
2023-10-14 10:26:52 +0200

    Implemented Execute Query in Interface IUserAccessModule

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/IUserAccessModule.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Contracts/IUserAccessModule.cs
@@ -7,5 +7,6 @@ namespace Jookli.UserAccess.Application.Contracts
     {
         Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
         Task ExecuteCommandAsync (ICommand command);
+        Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
     }
 }
