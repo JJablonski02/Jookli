5d976c1232772e2cd5137866a02901f6b21338e3
JJablonski02 <kuba2805@icloud.com>
2023-10-14 12:38:29 +0200

    fixed swagger xml error

---
+++ src/API/Jookli.Api/Modules/UserAccess/Controllers/UserAccessController.cs
@@ -0,0 +1,27 @@
+﻿using Jookli.Api.Configuration.Authorization;
+using Jookli.UserAccess.Application.Contracts;
+using Jookli.UserAccess.Application.Features.User.Register.Command;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.UserAccess.Controllers
+{
+    [Route("userAccess/[controller]")]
+    [ApiController]
+    public class UserAccessController : ControllerBase
+    {
+        private readonly IUserAccessModule _userAccessModule;
+
+
+        [NoPermissionRequired]
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> CreateNewUser(RegisterCommand command)
+        {
+            await _userAccessModule.ExecuteCommandAsync(command);
+
+            return Ok();
+        }
+    }
+
+}
