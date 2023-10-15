aedcc7176b4118ece46a687822fac4585fac9bb6
JJablonski02 <kuba2805@icloud.com>
2023-10-15 19:54:44 +0200

    SqlConnectionFactory provides connection to db

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,52 @@
+﻿using Jookli.BuildingBlocks.Application.Data;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure
+{
+    public class SqlConnectionFactory : ISqlConnectionFactory, IDisposable
+    {
+        private readonly string _connectionString;
+        private IDbConnection _connection;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public IDbConnection CreateNewConnection()
+        {
+            var connection = new SqlConnection(this._connectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+
+        public IDbConnection GetOpenConnection()
+        {
+            if (this._connection == null || this._connection.State != ConnectionState.Open)
+            {
+                this._connection = new SqlConnection(_connectionString);
+                this._connection.Open();
+            }
+            return this._connection;
+        }
+        public void Dispose()
+        {
+            if(this._connection != null && this._connection.State== ConnectionState.Open)
+            {
+                this._connection.Dispose();
+            }
+        }
+    }
+}
