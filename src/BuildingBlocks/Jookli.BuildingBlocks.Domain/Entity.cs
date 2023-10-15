d14ef1135c225806e058d5870803a37d8f8fed3d
JJablonski02 <kuba2805@icloud.com>
2023-10-15 23:27:34 +0200

    entity - basing on domain event. Building Block

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Domain
+{
+    public abstract class Entity
+    {
+        private List<IDomainEvent> _domainEvents;
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents?.Clear();
+        }
+
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents ??= new List<IDomainEvent>();
+
+            this._domainEvents.Add(domainEvent);
+        }
+
+        protected void CheckRule(IBusinessRule rule)
+        {
+            if (rule.IsBroken())
+            {
+                throw new BusinessRuleValidationException(rule);
+            }
+        }
+    }
+}
