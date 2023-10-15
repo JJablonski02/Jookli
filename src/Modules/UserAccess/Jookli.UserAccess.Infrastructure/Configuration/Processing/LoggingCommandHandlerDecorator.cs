0b38e6dc44f1158ab07eb61d5e8c4e4afce37017
JJablonski02 <kuba2805@icloud.com>
2023-10-15 18:44:04 +0200

    logging command handler decorator implementation. RequestLogEnricher implemented

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/LoggingCommandHandlerDecorator.cs
@@ -0,0 +1,48 @@
+﻿using Jookli.BuildingBlocks.Application;
+using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Application.Contracts;
+using MediatR;
+using Serilog;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing
+{
+    internal class LoggingCommandHandlerDecorator<T> : ICommandHandler<T>
+        where T : ICommand
+    {
+
+        private readonly ILogger _logger;
+        private readonly IExecutionContextAccessor _executionContextAccessor;
+        private readonly ICommandHandler<T> _decorator;
+        public LoggingCommandHandlerDecorator(
+            ILogger logger,
+            IExecutionContextAccessor executionContextAccessor,
+            ICommandHandler<T> decorator)
+        {
+            _logger = logger;
+            _executionContextAccessor = executionContextAccessor;
+            _decorator = decorator;
+        }
+       
+
+        private class RequestLogEnricher : ILogEventEnricher
+        {
+            private readonly IExecutionContextAccessor _executionContextAccessor;
+
+            public RequestLogEnricher(IExecutionContextAccessor executionContextAccessor)
+            {
+                _executionContextAccessor = executionContextAccessor;
+            }
+
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                if (_executionContextAccessor.IsAvailable)
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("CorrelationId", new ScalarValue(_executionContextAccessor.CorrelationId)));
+                }
+            }
+        }
+    }
+}
