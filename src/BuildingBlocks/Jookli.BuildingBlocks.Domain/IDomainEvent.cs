470ddd40ce1d6d23cf1c49e516882da319ced0de
JJablonski02 <kuba2805@icloud.com>
2023-10-15 22:33:04 +0200

    IDomainEvent in buildingblocks

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Domain/IDomainEvent.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Domain
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid Id { get; }
+        DateTime OccuredOn { get; }
+    }
+}
