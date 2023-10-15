dea0006cc48e4574a3fdf2103e3649f9be932479
JJablonski02 <kuba2805@icloud.com>
2023-10-15 23:29:03 +0200

    IOutbox

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Application/Outbox/IOutbox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Application.Outbox
+{
+    public interface IOutbox
+    {
+        void Add(OutboxMessage message);
+        Task Save();
+    }
+}
