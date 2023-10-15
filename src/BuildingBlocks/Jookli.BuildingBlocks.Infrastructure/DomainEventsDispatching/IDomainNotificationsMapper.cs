88498fc8b306f7cc377ea58f47a36133da8e0b2f
JJablonski02 <kuba2805@icloud.com>
2023-10-15 23:24:44 +0200

    IDomainNotificationsMapper in building blocks implemented

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/DomainEventsDispatching/IDomainNotificationsMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure.DomainEventsDispatching
+{
+    public interface IDomainNotificationsMapper
+    {
+        string GetName(Type type);
+
+        Type GetType(string name);
+    }
+}
