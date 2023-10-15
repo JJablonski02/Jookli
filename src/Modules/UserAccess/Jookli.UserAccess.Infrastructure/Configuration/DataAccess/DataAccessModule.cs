2abfc9b757c14f90c998c22dc8bb64bb038ad564
JJablonski02 <kuba2805@icloud.com>
2023-10-15 21:32:34 +0200

    DataAccessModule registered db

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/DataAccess/DataAccessModule.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/DataAccess/DataAccessModule.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.BuildingBlocks.Infrastructure;
+using Jookli.UserAccess.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Logging;
 
 namespace Jookli.UserAccess.Infrastructure.Configuration.DataAccess
 {
@@ -18,6 +19,33 @@ namespace Jookli.UserAccess.Infrastructure.Configuration.DataAccess
             _loggerFactory = loggerFactory;
         }
 
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SqlConnectionFactory>()
+                .As<ISqlConnectionFactory>()
+                .WithParameter("connectionString", _connectionString)
+                .InstancePerLifetimeScope();
 
+            builder
+                .Register(c =>
+                {
+                    var dbContextOptionsBuilder = new DbContextOptionsBuilder<UserAccessContext>();
+                    dbContextOptionsBuilder.UseSqlServer(_connectionString);
+
+                    dbContextOptionsBuilder
+                    .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>();
+
+                    return new UserAccessContext(dbContextOptionsBuilder.Options, _loggerFactory);
+                })
+                .AsSelf().As<DbContext>().InstancePerLifetimeScope();
+
+            var infrastructureAssembly = typeof(UserAccessContext).Assembly;
+
+            builder.RegisterAssemblyTypes(infrastructureAssembly)
+                .Where(type => type.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .FindConstructorsWith(new AllConstructorFinder());
+        }
     }
 }
