bd09a017dd84c296d2d368c68ef267433db48f2d
JJablonski02 <kuba2805@icloud.com>
2023-10-16 00:14:59 +0200

    AllPropertiesContractResolver inherits DefaultContractResolver implemented.

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Serialization/AllPropertiesContractResolver.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure.Serialization
+{
+    public class AllPropertiesContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var properties = type.GetProperties(
+                System.Reflection.BindingFlags.Public |
+                System.Reflection.BindingFlags.NonPublic |
+                System.Reflection.BindingFlags.Instance
+                ).Select(c => this.CreateProperty(c, memberSerialization))
+                .ToList();
+
+            properties.ForEach(p =>
+            {
+                p.Writable = true;
+                p.Readable = true;
+            });
+
+            return properties;
+        }
+    }
+}
