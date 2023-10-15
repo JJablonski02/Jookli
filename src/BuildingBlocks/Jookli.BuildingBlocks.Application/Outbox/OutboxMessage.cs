c36c0bfce9ea1b037955aa4b3e53229a801295c8
JJablonski02 <kuba2805@icloud.com>
2023-10-15 23:28:55 +0200

    Outbox message blueprint

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Application/Outbox/OutboxMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Application.Outbox
+{
+    public class OutboxMessage
+    {
+        public Guid Id { get; set; }
+        public DateTime OccuredOn { get; set; }
+        public string Type { get; set; }
+        public string Data { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+
+        public OutboxMessage(Guid id, DateTime occuredOn, string type, string data)
+        {
+            this.Id = id;
+            this.OccuredOn = occuredOn;
+            this.Type = type;
+            this.Data = data;
+        }
+
+        private OutboxMessage()
+        {
+        }
+    }
+}
