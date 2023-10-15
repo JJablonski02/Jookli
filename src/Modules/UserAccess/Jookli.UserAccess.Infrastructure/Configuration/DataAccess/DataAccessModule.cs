40ad8b77f82a3333d680bc97b45e09de01faa816
JJablonski02 <kuba2805@icloud.com>
2023-10-15 19:26:54 +0200

    DataAccessModule implemeting

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/DataAccess/DataAccessModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.DataAccess
+{
+    internal class DataAccessModule : Autofac.Module
+    {
+        private readonly string _connectionString;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public DataAccessModule(string connectionString, ILoggerFactory loggerFactory)
+        {
+            _connectionString = connectionString;
+            _loggerFactory = loggerFactory;
+        }
+
+
+    }
+}
