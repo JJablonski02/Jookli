57e48f5362bf3554570838fbbc65118887e89b63
JJablonski02 <kuba2805@icloud.com>
2023-10-15 23:26:28 +0200

    DomainEventsAccessor created. Implements IDomainEventsAccessor

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/DomainEventsDispatching/DomainEventsAccessor.cs
@@ -0,0 +1,35 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure.DomainEventsDispatching
+{
+    internal class DomainEventsAccessor : IDomainEventsAccessor
+    {
+        private readonly DbContext _jookliDbContext;
+
+        public DomainEventsAccessor(DbContext jookliDbContext)
+        {
+            _jookliDbContext = jookliDbContext;
+        }
+
+        public void ClearAllDomainEvents()
+        {
+            var domainEntities = this._jookliDbContext.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any()).ToList();
+
+            domainEntities
+                .ForEach(entity => entity.Entity.ClearDomainEvents());
+        }
+
+        public IReadOnlyCollection<IDomainEvent> GetAllDomainEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
