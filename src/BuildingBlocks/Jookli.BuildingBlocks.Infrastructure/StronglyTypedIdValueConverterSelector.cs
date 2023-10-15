b87b4a77686663d33d5b21df4dcae2dc21fec9ed
JJablonski02 <kuba2805@icloud.com>
2023-10-15 21:33:49 +0200

    Converter selector

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/StronglyTypedIdValueConverterSelector.cs
@@ -0,0 +1,47 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure
+{
+    public class StronglyTypedIdValueConverterSelector : ValueConverterSelector
+    {
+        private readonly ConcurrentDictionary<(Type ModelClrType, Type ProviderType), ValueConverterInfo> _converters
+            = new ConcurrentDictionary<(Type ModelClrType, Type ProviderClrType), ValueConverterInfo>();
+
+        public StronglyTypedIdValueConverterSelector(ValueConverterSelectorDependencies dependencies) : base(dependencies) { }
+
+        public override IEnumerable<ValueConverterInfo> Select(Type modelClrType, Type providerClrType = null)
+        {
+            var baseConverters = base.Select(modelClrType, providerClrType);
+
+            foreach (var converter in baseConverters)
+            {
+                yield return converter;
+            }
+
+            var underlyingModelType = UnwrapNullableType(modelClrType);
+            var underlyingProviderType = UnwrapNullableType(providerClrType);
+
+            if(underlyingProviderType is null || underlyingProviderType == typeof(Guid))
+            {
+                var isTypedIdValue = typeof(TypeIdValueBase).IsAssignableFrom(underlyingModelType);
+            }
+        }
+
+        private static Type UnwrapNullableType(Type type)
+        {
+            if(type is null)
+            {
+                return null;
+            }
+
+            return Nullable.GetUnderlyingType(type) ?? type;
+        }
+    }
+}
