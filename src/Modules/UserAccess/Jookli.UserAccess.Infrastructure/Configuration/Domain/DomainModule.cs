f4152fac1ce9e24ad00b2ed6f8e88c2645853e14
JJablonski02 <kuba2805@icloud.com>
2023-10-16 22:22:33 +0200

    Domain module created

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Domain/DomainModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Jookli.UserAccess.Application.Common;
+using Jookli.UserAccess.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Domain
+{
+    internal class DomainModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UsersCounter>()
+                .As<IUsersCounter>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
