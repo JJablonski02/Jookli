27edad963bb502360e84849f10280fce34f37dd4
JJablonski02 <kuba2805@icloud.com>
2023-10-16 22:20:55 +0200

    BuildingBlocks domainEventBase implemented

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Domain/DomainEventBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Domain
+{
+    public class DomainEventBase : IDomainEvent
+    {
+        public Guid Id { get; }
+
+        public DateTime OccuredOn { get; }
+
+        public DomainEventBase()
+        {
+            this.Id = Guid.NewGuid();
+            this.OccuredOn = DateTime.UtcNow;
+        }
+    }
+}
