3484cd22e4ee321b6a7e250e8bc00bf6921f0d00
JJablonski02 <kuba2805@icloud.com>
2023-10-16 22:21:40 +0200

    Created Notification for new user registered

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Notification/NewUserRegisteredNotification.cs
@@ -0,0 +1,16 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Register.Notification
+{
+    public class NewUserRegisteredNotification : DomainNotificationBase<NewUserRegisteredDomainEvent>
+    {
+        public NewUserRegisteredNotification(NewUserRegisteredDomainEvent domainEvent, Guid id) : base(domainEvent, id) { }
+    }
+}
