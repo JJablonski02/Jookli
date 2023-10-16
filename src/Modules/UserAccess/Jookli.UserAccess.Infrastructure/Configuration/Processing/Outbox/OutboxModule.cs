97775617fdd6ec3cdc17b322cdc7b0307c24eb69
JJablonski02 <kuba2805@icloud.com>
2023-10-16 21:04:48 +0200

    OtboxModule creating

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/Outbox/OutboxModule.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/Outbox/OutboxModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using Jookli.BuildingBlocks.Infrastructure;
+using Jookli.UserAccess.Infrastructure.Outbox;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,16 @@ namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.Outbox
 {
     internal class OutboxModule : Module
     {
+        private readonly BiDictionary<string, Type> _domainNotificationMap;
+
+        public OutboxModule(BiDictionary<string, Type> domainNotificationMap)
+        {
+            _domainNotificationMap = domainNotificationMap;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<OutboxAccessor>
+        }
     }
 }
