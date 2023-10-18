555593d7421a974ff0780c5c07dc72993667ac50
JJablonski02 <kuba2805@icloud.com>
2023-10-18 20:32:35 +0200

    UnitOfWork

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.DomainEventsDispatching;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure
+{
+    public class UnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        private readonly IDomainEventsDispatcher _domainEventsDispatcher;
+
+        public UnitOfWork(DbContext dbContext, IDomainEventsDispatcher domainEventsDispatcher)
+        {
+            _dbContext = dbContext;
+            _domainEventsDispatcher = domainEventsDispatcher;
+        }
+
+        public async Task<int> CommitAsync(CancellationToken cancellationToken = default, Guid? internalCommandId = null)
+        {
+            await this._domainEventsDispatcher.DispatchEventsAsync();
+
+            return await this._dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
