068ede95e8e138ea666e3394a84beb5924594167
JJablonski02 <kuba2805@icloud.com>
2023-10-18 18:02:57 +0200

    RequestLog enricher nested

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/LoggingCommandHandlerWithResultDecorator.cs
@@ -0,0 +1,52 @@
+﻿using Jookli.BuildingBlocks.Application;
+using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Application.Contracts;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing
+{
+    internal class LoggingCommandHandlerWithResultDecorator<T, TResult> : ICommandHandler<T, TResult> where T : ICommand<TResult>
+    {
+        private readonly ILogger _logger;
+        private readonly IExecutionContextAccessor _executionContextAccessor;
+        private readonly ICommandHandler<T, TResult> _decorator;
+
+        public LoggingCommandHandlerWithResultDecorator(ILogger logger, IExecutionContextAccessor executionContextAccessor, ICommandHandler<T, TResult> decorator)
+        {
+            _logger = logger;
+            _executionContextAccessor = executionContextAccessor;
+            _decorator = decorator;
+        }
+
+        public Task<TResult> Handle(T request, CancellationToken cancellationToken)
+        {
+
+        }
+
+        private class RequestLogEnricher : ILogEventEnricher
+        {
+            private readonly IExecutionContextAccessor _executionContextAccessor;
+            public RequestLogEnricher(IExecutionContextAccessor executionContextAccessor)
+            {
+                _executionContextAccessor = executionContextAccessor;
+            }
+
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                if (_executionContextAccessor.IsAvailable)
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("CorrelationId", new ScalarValue(_executionContextAccessor.CorrelationId)));
+                }
+            }
+        }
+
+    }
+}
