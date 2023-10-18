14bdb0793d3efe46a59059f228164ddfca810a98
JJablonski02 <kuba2805@icloud.com>
2023-10-18 20:01:06 +0200

    BuildingBlocks IUnitOfWork

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync(
+            CancellationToken cancellationToken = default,
+            Guid? internalCommandId = null);
+    }
+}
