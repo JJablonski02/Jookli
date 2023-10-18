a8a25a6db8dbd8516acff1669d89bb05f7153c57
JJablonski02 <kuba2805@icloud.com>
2023-10-18 20:32:41 +0200

    ICommandsScheduler

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Configuration/Command/ICommandsScheduler.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Configuration.Command
+{
+    public interface ICommandsScheduler
+    {
+        Task EnqueueAsync(ICommand command);
+        Task EnqueueAsync<T>(ICommand<T> command);
+    }
+}
