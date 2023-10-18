422949d2409c3034bdc697feb4c01715bc661fc7
JJablonski02 <kuba2805@icloud.com>
2023-10-18 18:06:28 +0200

    CommandLogEnricher nested

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/LoggingCommandHandlerWithResultDecorator.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/LoggingCommandHandlerWithResultDecorator.cs
@@ -28,7 +28,22 @@ namespace Jookli.UserAccess.Infrastructure.Configuration.Processing
 
         public Task<TResult> Handle(T request, CancellationToken cancellationToken)
         {
+           
+        }
 
+        private class CommandLogEnricher : ILogEventEnricher
+        {
+            private readonly ICommand<TResult> _command;
+
+            public CommandLogEnricher(ICommand<TResult> command)
+            {
+                _command = command;
+            }
+
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                logEvent.AddOrUpdateProperty(new LogEventProperty("Context", new ScalarValue($"Command:{_command.Id.ToString()}")));
+            } 
         }
 
         private class RequestLogEnricher : ILogEventEnricher
