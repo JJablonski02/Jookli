fa8f8a9cad46e5a9f7d9af9f51baff4269c2790f
JJablonski02 <kuba2805@icloud.com>
2023-10-21 21:27:39 +0200

    UserEntity modified

--- src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/User/UserEntity.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/User/UserEntity.cs
@@ -1,5 +1,6 @@
 ﻿using Jookli.UserAccess.Domain.Entities.Message;
 using Jookli.UserAccess.Domain.Entities.VoiceMessage;
+using Jookli.UserAccess.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace Jookli.UserAccess.Domain.Entities.User
@@ -9,12 +10,12 @@ namespace Jookli.UserAccess.Domain.Entities.User
         [Key]
         public Guid UserID { get; set; }
         [StringLength(50)]
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int PhoneNumber { get; set; } 
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public int PhoneNumber { get; set; } 
         public DateTime DateOfLastLogin { get; set; }
         public bool Premium { get; set; }
         public bool IsMicrophoneAllowed { get; set; }
