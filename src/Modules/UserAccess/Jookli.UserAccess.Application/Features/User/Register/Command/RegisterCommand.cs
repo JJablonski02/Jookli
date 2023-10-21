959e971c41485d6c6c6de558d76b8872c7668458
JJablonski02 <kuba2805@icloud.com>
2023-10-21 21:27:06 +0200

    registerCommand new fields

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommand.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Jookli.UserAccess.Application.Contracts;
+using Jookli.UserAccess.Domain.Enums;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -12,32 +13,11 @@ namespace Jookli.UserAccess.Application.Features.User.Register.Command
 {
     public class RegisterCommand : CommandBase
     {
-        //public RegisterCommand(
-        //    string name,
-        //    string surname,
-        //    string username,
-        //    string email,
-        //    int phoneNumber,
-        //    string password,
-        //    string confirmPassword)
-        //{
-        //    Id = id;
-        //    Name = name;
-        //    Surname = surname;
-        //    Username = username;
-        //    Email = email;
-        //    PhoneNumber = phoneNumber;
-        //    Password = password;
-        //    ConfirmPassword = confirmPassword;
-
-        //}
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Username { get; set; }
+        public Guid UserId { get; set; }
         public string Email { get; set; }
-        public int PhoneNumber { get; set; }
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
     }
 }
