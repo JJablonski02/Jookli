f37b54c5abc984c0573dee499292d5f5d78b30a3
JJablonski02 <kuba2805@icloud.com>
2023-10-21 22:29:39 +0200

    UserEntityException

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Application/Exceptions/UserEntityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Application.Exceptions
+{
+    public class UserEntityException : Exception
+    {
+        public UserEntityException(string message) : base(message)
+        {
+        }
+    }
+}
