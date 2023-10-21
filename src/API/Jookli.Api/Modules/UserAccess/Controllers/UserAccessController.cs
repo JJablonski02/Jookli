f4cd0bc89795ef2e7de2f61009cef79111931003
JJablonski02 <kuba2805@icloud.com>
2023-10-21 22:28:23 +0200

    RemoveUser request

--- src/API/Jookli.Api/Modules/UserAccess/Controllers/UserAccessController.cs
+++ src/API/Jookli.Api/Modules/UserAccess/Controllers/UserAccessController.cs
@@ -1,12 +1,13 @@
 ﻿using Jookli.Api.Configuration.Authorization;
 using Jookli.UserAccess.Application.Contracts;
 using Jookli.UserAccess.Application.Features.User.Register.Command;
+using Jookli.UserAccess.Application.Features.User.Remove.Command;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jookli.Api.Modules.UserAccess.Controllers
 {
-    [Route("api/userAccess/[controller]")]
+    [Route("api/userAccess")]
     [ApiController]
     public class UserAccessController : ControllerBase
     {
@@ -21,11 +22,21 @@ namespace Jookli.Api.Modules.UserAccess.Controllers
         [NoPermissionRequired]
         [AllowAnonymous]
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("RegisterUser")]
         public async Task<IActionResult> RegisterNewUserRequest(RegisterCommand request)
         {
             await _userAccessModule.ExecuteCommandAsync(request);
                 
+            return Ok();
+        }
+
+        [NoPermissionRequired]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("RemoveUser")]
+        public async Task<IActionResult> RemoveUser(RemoveUserCommand command)
+        {
+            await _userAccessModule.ExecuteCommandAsync(command);
 
             return Ok();
         }
