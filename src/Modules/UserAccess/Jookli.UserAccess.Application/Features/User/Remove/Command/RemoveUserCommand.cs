55e19958659c8a75397c0d6d9c3b68855cbe6ffe
JJablonski02 <kuba2805@icloud.com>
2023-10-21 22:29:26 +0200

    RemoveUserCommand

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Remove/Command/RemoveUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Remove.Command
+{
+    public sealed class RemoveUserCommand : CommandBase<Unit>
+    {
+        public RemoveUserCommand(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public Guid UserId { get; set; }
+    }
+}
