08a529cbaba9d5d72a85b8d506443c00726f336b
JJablonski02 <kuba2805@icloud.com>
2023-10-22 09:59:38 +0200

    ResetPasswordCommand

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ResetPassword/Command/ResetPasswordCommand.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ResetPassword.Command
+{
+    public class ResetPasswordCommand : CommandBase
+    {
+        public ResetPasswordCommand(Guid userId, string token, string password, string replyPassword)
+        {
+            UserId = userId;
+            Token = token;
+            Password = password;
+            ReplyPassword = replyPassword;
+        }
+
+        public Guid UserId { get; set; }
+        public string Token { get; set; }
+        public string OldPassword { get; set; }
+        public string Password { get; set; }
+        public string ReplyPassword { get; set; }
+    }
+}
