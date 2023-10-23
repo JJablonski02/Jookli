eecd9e8fed48c54ecdec6014f7d91988555f8075
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:09:50 +0200

    UnitOfWorkCommandHandlerDecorator in BB infrastructure created

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/DomainEventsDispatching/UnitOfWorkCommandHandlerDecorator.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Jookli.BuildingBlocks.Infrastructure.DomainEventsDispatching
+{
+    public class UnitOfWorkCommandHandlerDecorator<T> : IRequestHandler<T, Unit> where T : IRequest
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRequestHandler<T, Unit> _decorator;
+
+        public UnitOfWorkCommandHandlerDecorator(IUnitOfWork unitOfWork, IRequestHandler<T, Unit> decorator)
+        {
+            _unitOfWork = unitOfWork;
+            _decorator = decorator;
+        }
+
+        public async Task<Unit> Handle(T request, CancellationToken cancellationToken)
+        {
+            await this._decorator.Handle(request, cancellationToken);
+
+            await this._unitOfWork.CommitAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
