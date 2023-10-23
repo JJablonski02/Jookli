fac6412a319f61a1a786efd4c8776977d7b5e2ae
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:12:59 +0200

    UserRegistrationId Inherits TypeIdValueBase created.

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Domain/UserRegistrations/UserRegistrationId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Domain.UserRegistrations
+{
+    public class UserRegistrationId : TypeIdValueBase
+    {
+        public UserRegistrationId(Guid value) : base(value)
+        {
+        }
+    }
+}
