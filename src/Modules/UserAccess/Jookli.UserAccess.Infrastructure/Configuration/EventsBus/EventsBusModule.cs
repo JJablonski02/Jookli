0488bff2c799d150aca3af60337f1d7521535594
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:06:07 +0200

    EventsBusModule created

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/EventsBus/EventsBusModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Jookli.BuildingBlocks.EventBus;
+using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.EventsBus
+{
+    internal class EventsBusModule : Autofac.Module
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public EventsBusModule(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if(_eventsBus != null)
+            {
+                builder.RegisterInstance(_eventsBus).SingleInstance();
+            }
+            else
+            {
+                builder.RegisterType<InMemoryEventBus>()
+                    .As<IEventsBus>()
+                    .SingleInstance();
+            }
+        }
+    }
+}
