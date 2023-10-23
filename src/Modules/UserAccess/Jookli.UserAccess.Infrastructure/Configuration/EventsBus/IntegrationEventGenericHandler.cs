e5bb7af06a58fe0e7d39fd5e305349a883e23e89
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:02:58 +0200

    IntegrationEventGenericHandler added

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/EventsBus/IntegrationEventGenericHandler.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Dapper;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.BuildingBlocks.Infrastructure.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.EventsBus
+{
+    internal class IntegrationEventGenericHandler<T> : IIntegrationEventHandler<T> where T : IntegrationEvent
+    {
+        public async Task Handle(T @event)
+        {
+            using(var scope = UserAccessCompositionRoot.BeginLifetimeScope())
+            {
+                using (var connection = scope.Resolve<ISqlConnectionFactory>().GetOpenConnection())
+                {
+
+                    string type = @event.GetType().FullName;
+                    var data = JsonConvert.SerializeObject(@event, new JsonSerializerSettings
+                    {
+                        ContractResolver = new AllPropertiesContractResolver()
+                    });
+
+                    var sql = "INSERT INTO [User].[InboxMessages] (Id, OccuredOn, Type, Data) " +
+                        "VALUES (@Id, @OccuredOn, @Type, @Data";
+
+                    await connection.ExecuteScalarAsync(sql, new
+                    {
+                        @event.Id,
+                        @event.OccuredOn,
+                        type,
+                        data
+                    });
+                }
+            }
+        }
+    }
+}
