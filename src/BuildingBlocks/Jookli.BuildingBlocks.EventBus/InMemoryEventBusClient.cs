ae280fb6ebaead53c8be56e0370064c997181468
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:11:31 +0200

    InMemoryEventBusClient created. Implements IEventsBus

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.EventBus/InMemoryEventBusClient.cs
@@ -0,0 +1,33 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.EventBus
+{
+    internal class InMemoryEventBusClient : IEventsBus
+    {
+        private readonly ILogger _logger;
+        public void Dispose()
+        {
+        }
+
+        public async Task Publish<T>(T @event) where T : IntegrationEvent
+        {
+            _logger.Information("Publishing {Event}", @event.GetType().FullName);
+            await InMemoryEventBus.Instance.Publish(@event);
+        }
+
+        public void StartConsuming()
+        {
+        }
+
+        public void Subscribe<T>(IIntegrationEventHandler<T> handler) where T : IntegrationEvent
+        {
+            InMemoryEventBus.Instance.Subscribe(handler);
+        }
+    }
+}
