1fd9025204f97063ff12d0c20d8c19cf404b1010
JJablonski02 <kuba2805@icloud.com>
2023-10-23 22:10:12 +0200

    DomainEventsDispatcherNotificationHandlerDecorator BB infrastructure created

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/DomainEventsDispatching/DomainEventsDispatcherNotificationHandlerDecorator.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure.DomainEventsDispatching
+{
+    public class DomainEventsDispatcherNotificationHandlerDecorator<T> : INotificationHandler<T> where T : INotification
+    {
+        private readonly INotificationHandler<T> _decorator;
+        private readonly IDomainEventsDispatcher _domainEventsDispatcher;
+
+        public DomainEventsDispatcherNotificationHandlerDecorator(INotificationHandler<T> decorator, IDomainEventsDispatcher domainEventsDispatcher)
+        {
+            _decorator = decorator;
+            _domainEventsDispatcher = domainEventsDispatcher;
+        }
+
+        public async Task Handle(T notification, CancellationToken cancellationToken)
+        {
+            await this._decorator.Handle(notification, cancellationToken);
+
+            await this._domainEventsDispatcher.DispatchEventsAsync();
+        }
+    }
+}
