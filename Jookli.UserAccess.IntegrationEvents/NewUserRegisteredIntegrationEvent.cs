c9c72e1951d0a54abc195397f2bf6300718be4c2
JJablonski02 <kuba2805@icloud.com>
2023-10-24 22:25:21 +0200

    IntegrationEvent. UserRegistered.

---
+++ Jookli.UserAccess.IntegrationEvents/NewUserRegisteredIntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.IntegrationEvents
+{
+    public class NewUserRegisteredIntegrationEvent : IntegrationEvent
+    {
+        public Guid UserId { get; }
+        public string Email { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public NewUserRegisteredIntegrationEvent(Guid id, DateTime occurredOn, Guid userId, string email, string firstName, string lastName)
+            : base(id, occurredOn)
+        {
+            UserId = userId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
