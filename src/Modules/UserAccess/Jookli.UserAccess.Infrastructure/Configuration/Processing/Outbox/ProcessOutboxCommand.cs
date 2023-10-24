9b760bcbb0eaf67370633fd4380d017f85512456
JJablonski02 <kuba2805@icloud.com>
2023-10-24 21:06:39 +0200

    ProcessOutboxCommand. ProcessoutboxJob inherits

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/Outbox/ProcessOutboxCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.Outbox
+{
+    public class ProcessOutboxCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
