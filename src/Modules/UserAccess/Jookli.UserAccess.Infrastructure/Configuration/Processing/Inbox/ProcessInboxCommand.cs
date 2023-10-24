fa3e7293ed5e5bb69a49e3125803d7b00d406fec
JJablonski02 <kuba2805@icloud.com>
2023-10-24 21:07:38 +0200

    ProcessInboxCommand implements IrecurringCommand, inherits CommandBase. ProcessInboxJob inheriter

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/Inbox/ProcessInboxCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.Inbox
+{
+    public class ProcessInboxCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
