fad07e367413ae047881a714d988e8bfb3a78f07
JJablonski02 <kuba2805@icloud.com>
2023-10-24 21:07:03 +0200

    ProcessInboxJob implements IJob. Executor.

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/Inbox/ProcessInboxJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.Inbox
+{
+    [DisallowConcurrentExecution]
+    public class ProcessInboxJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await CommandsExecutor.Execute(new ProcessInboxCommand());
+        }
+    }
+}
