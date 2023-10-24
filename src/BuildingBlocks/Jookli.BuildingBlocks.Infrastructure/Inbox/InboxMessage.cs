54b26e32cb042c888c0b33f6011b3a2063579220
JJablonski02 <kuba2805@icloud.com>
2023-10-24 20:35:20 +0200

    InboxMessage BB

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure.Inbox
+{
+    public class InboxMessage
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Data { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+
+        public InboxMessage(Guid id, string type, string data, DateTime? processedDate)
+        {
+            Id = id;
+            Type = type;
+            Data = data;
+            ProcessedDate = processedDate;
+        }
+
+        private InboxMessage() { }
+    }
+}
