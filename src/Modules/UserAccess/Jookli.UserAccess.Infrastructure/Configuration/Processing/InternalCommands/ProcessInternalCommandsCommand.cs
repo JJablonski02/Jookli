e5249b935cb9cbe86e2e9a84f6a15f949a37d9ab
JJablonski02 <kuba2805@icloud.com>
2023-10-24 22:26:47 +0200

    InternalCommands process

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Processing/InternalCommands/ProcessInternalCommandsCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.InternalCommands
+{
+    internal class ProcessInternalCommandsCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
