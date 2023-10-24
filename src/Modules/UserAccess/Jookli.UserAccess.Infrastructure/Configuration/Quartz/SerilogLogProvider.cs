092711c6519445af7e9daf319cadd965bcf4d9a2
JJablonski02 <kuba2805@icloud.com>
2023-10-24 21:05:18 +0200

    SerilogLogProvider

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Quartz/SerilogLogProvider.cs
@@ -0,0 +1,60 @@
+﻿using Quartz.Logging;
+using Serilog;
+using Logger = Quartz.Logging.Logger;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Quartz
+{
+    internal class SerilogLogProvider : ILogProvider
+    {
+        private readonly ILogger _logger;
+
+        public SerilogLogProvider(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Logger GetLogger(string name)
+        {
+            return (level, func, exception, parameters) =>
+            {
+
+                if (func == null)
+                {
+                    return true;
+                }
+                switch (level)
+                {
+                    case LogLevel.Debug:
+                    case LogLevel.Trace:
+                        _logger.Debug(exception, func(), parameters);
+                        break;
+                    case LogLevel.Info:
+                        _logger.Information(exception, func(), parameters);
+                        break;
+                    case LogLevel.Warn:
+                        _logger.Warning(exception, func(), parameters);
+                        break;
+                    case LogLevel.Error:
+                        _logger.Error(exception, func(), parameters);
+                        break;
+                    case LogLevel.Fatal:
+                        _logger.Fatal(exception, func(), parameters);
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            };
+        }
+
+        public IDisposable OpenMappedContext(string key, object value, bool destructure = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable OpenNestedContext(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
