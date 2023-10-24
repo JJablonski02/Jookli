e297df67a858ea1860e5bee12899d0e3ef3c51b5
JJablonski02 <kuba2805@icloud.com>
2023-10-24 22:26:03 +0200

    PublishEventHandler for new user registration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Notification/NewUserRegisteredPublishEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Register.Notification
+{
+    internal class NewUserRegisteredPublishEventHandler : INotificationHandler<NewUserRegisteredNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public NewUserRegisteredPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(NewUserRegisteredNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new NewUserRegisteredIntegrationEvent(
+                notification.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.UserID,
+                notification.DomainEvent.Email,
+                notification.DomainEvent.FirstName,
+                notification.DomainEvent.LastName));;
+        }
+    }
+}
