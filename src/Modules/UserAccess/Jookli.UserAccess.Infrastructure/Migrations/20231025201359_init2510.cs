c5d1250639e708c16f2b9eb984acbc677ff031dc
JJablonski02 <kuba2805@icloud.com>
2023-10-25 22:58:42 +0200

    migration to db new data

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Migrations/20231025201359_init2510.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jookli.UserAccess.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2510 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.RenameTable(
+                name: "OutboxMessages",
+                newName: "OutboxMessages",
+                newSchema: "user");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "OutboxMessages",
+                schema: "user",
+                newName: "OutboxMessages");
+        }
+    }
+}
