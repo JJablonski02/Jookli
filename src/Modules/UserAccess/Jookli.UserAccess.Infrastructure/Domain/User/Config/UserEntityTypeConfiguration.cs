4cfaa9274e8b5d9bf4b694df02ced8ce29fde6d0
JJablonski02 <kuba2805@icloud.com>
2023-10-30 21:36:13 +0100

    UserEntityTypeConfiguration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/User/Config/UserEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Jookli.UserAccess.Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.User.Config
+{
+    internal class UserEntityTypeConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("UserAccess_User");
+
+            builder.HasKey(x => x.UserID);
+            builder.HasIndex(x => x.UserID);
+        }
+    }
+}
