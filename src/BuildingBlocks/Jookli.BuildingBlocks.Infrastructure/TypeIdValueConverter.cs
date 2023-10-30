c996883291f19e0665c87a0b460bb4e2b5108f36
JJablonski02 <kuba2805@icloud.com>
2023-10-30 23:04:13 +0100

    TypeIdvalueConverter BB

---
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/TypeIdValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.BuildingBlocks.Infrastructure
+{
+    public class TypedIdValueConverter<TTypedIdValue> : ValueConverter<TTypedIdValue, Guid>
+        where TTypedIdValue : TypeIdValueBase
+    {
+        public TypedIdValueConverter(ConverterMappingHints mappingHints = null)
+            : base(id => id.Value, value => Create(value), mappingHints)
+        {
+        }
+
+        private static TTypedIdValue Create(Guid id) => Activator.CreateInstance(typeof(TTypedIdValue), id) as TTypedIdValue;
+    }
+}
