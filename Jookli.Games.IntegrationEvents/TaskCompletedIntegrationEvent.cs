6e797cd009239040bf15016d5a0bebfecb32acbb
JJablonski02 <kuba2805@icloud.com>
2023-11-01 23:20:16 +0100

    TaskCompletedIntegrationEvent

---
+++ Jookli.Games.IntegrationEvents/TaskCompletedIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+
+namespace Jookli.Games.IntegrationEvents
+{
+    public class TaskCompletedIntegrationEvent : IntegrationEvent
+    {
+        public Guid UserId { get; }
+        public Guid TaskId { get; }
+        public double Score { get; }
+         
+        public TaskCompletedIntegrationEvent(Guid id, DateTime occuredOn, Guid userId, Guid taskId, double score) : base(id, occuredOn)
+        {
+            UserId = userId;
+            TaskId = taskId;
+            Score = score;
+        }
+    }
+}
