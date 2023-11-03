080d91b4259ff2de1d1d159cb1a72888737e30c4
JJablonski02 <kuba2805@icloud.com>
2023-11-03 10:49:41 +0100

    Games.Application Mediatr main command/query

---
+++ src/Modules/Games/Jookli.Games.Application/Contracts/IGamesModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Contracts
+{
+    public interface IGamesModule
+    {
+        Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+        Task ExecuteCommandAsync(ICommand command);
+        Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+    }
+}
