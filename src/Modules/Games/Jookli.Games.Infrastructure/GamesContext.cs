7a6f7d9acd54657914aa2ef70aa2e2f6a2bdb5ac
JJablonski02 <kuba2805@icloud.com>
2023-11-03 10:49:11 +0100

    Games Infrastructure. Context, Module created

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
@@ -0,0 +1,10 @@
+﻿using Jookli.Games.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jookli.Games.Infrastructure
+{
+    public class GamesContext : DbContext
+    {
+        public DbSet<GamesEntity> Games { get; set; }
+    }
+}
---
+++ src/Modules/Games/Jookli.Games.Infrastructure/GamesModule.cs
@@ -0,0 +1,27 @@
+﻿using Jookli.Games.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure
+{
+    internal class GamesModule : IGamesModule
+    {
+        public Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ExecuteCommandAsync(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
