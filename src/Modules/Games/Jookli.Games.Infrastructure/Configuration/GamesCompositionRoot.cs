6e119238e41b9b29f95237e34de11b54f4684dd6
JJablonski02 <kuba2805@icloud.com>
2023-11-03 18:36:28 +0100

    GamesCompositionRoot

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/GamesCompositionRoot.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace Jookli.Games.Infrastructure.Configuration
+{
+    internal class GamesCompositionRoot
+    {
+        private static IContainer _container;
+
+        internal static void SetContainer(IContainer container)
+        {
+            _container = container;
+        }
+
+        internal static ILifetimeScope BeginLifetimeScope()
+        {
+            return _container.BeginLifetimeScope();
+        }
+    }
+}
