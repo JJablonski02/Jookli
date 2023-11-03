bc24b6489f94c7497679394d45d6839322514e41
JJablonski02 <kuba2805@icloud.com>
2023-11-03 18:36:36 +0100

    CommandsExecutor

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Processing/CommandsExecutor.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Jookli.Games.Application.Contracts;
+using MediatR;
+
+namespace Jookli.Games.Infrastructure.Configuration.Processing
+{
+    public class CommandsExecutor
+    {
+        internal static async Task Execute(ICommand command)
+        {
+            using (var scope = GamesCompositionRoot.BeginLifetimeScope())
+            {
+                var mediator = scope.Resolve<IMediator>();
+                await mediator.Send(command);
+            }
+        }
+        internal static async Task<TResult> Execute<TResult>(ICommand<TResult> command)
+        {
+            using (var scope = GamesCompositionRoot.BeginLifetimeScope())
+            {
+                var mediator = scope.Resolve<IMediator>();
+                return await mediator.Send(command);
+            }
+        }
+    }
+}
