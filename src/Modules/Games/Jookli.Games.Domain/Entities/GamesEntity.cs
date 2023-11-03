5e64d4983aacc597e3c0f93e1ee95adb74fa7eba
JJablonski02 <kuba2805@icloud.com>
2023-11-03 10:50:10 +0100

    GamesEntity

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/GamesEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Jookli.Games.Domain.Entities
+{
+    public class GamesEntity
+    {
+        public Guid GameId { get; set; }
+        public string FullName { get; set; }
+        public int Counter { get; set; }    
+    }
+}
