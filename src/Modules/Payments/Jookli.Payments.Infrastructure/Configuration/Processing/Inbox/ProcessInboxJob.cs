741a77e9ea935c4c47dd8ad6205d0abae49c4024
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:59:38 +0100

    ProcessInboxJob

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Processing/Inbox/ProcessInboxJob.cs
@@ -0,0 +1,16 @@
+﻿using Jookli.Payments.Infrastructure.Configuration.Processing;
+using Jookli.Payments.Infrastructure.Configuration.Processing.Inbox;
+using Quartz;
+
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Processing.Inbox
+{
+    [DisallowConcurrentExecution]
+    public class ProcessInboxJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await CommandsExecutor.Execute(new ProcessInboxCommand());
+        }
+    }
+}
