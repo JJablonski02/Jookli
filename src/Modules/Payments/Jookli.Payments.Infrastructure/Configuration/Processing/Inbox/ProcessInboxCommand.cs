9a06b826c59a4f31ba8b5d0489d248842cdefdca
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:59:31 +0100

    ProcessInboxCommand

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Processing/Inbox/ProcessInboxCommand.cs
@@ -0,0 +1,8 @@
+﻿using Jookli.Payments.Application.Contracts;
+
+namespace Jookli.Payments.Infrastructure.Configuration.Processing.Inbox
+{
+    public class ProcessInboxCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
