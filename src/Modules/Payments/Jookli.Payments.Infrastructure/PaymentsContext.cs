e7a33d70addeb9ccb50dc8e2c83c6f6108906ab8
JJablonski02 <kuba2805@icloud.com>
2023-11-04 23:02:56 +0100

    PaymentsContext

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/PaymentsContext.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Jookli.Payments.Infrastructure
+{
+    public class PaymentsContext : DbContext
+    {
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<InternalCommand> InternalCommands { get; set; }
+        private readonly ILoggerFactory _logger;
+        public PaymentsContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        }
+    }
+}
