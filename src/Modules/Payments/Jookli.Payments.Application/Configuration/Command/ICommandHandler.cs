1ccdc0a1e938c2d9058b02ef6defcb638d5dfcc1
JJablonski02 <kuba2805@icloud.com>
2023-11-04 20:09:35 +0100

    ICommandHandler

---
+++ src/Modules/Payments/Jookli.Payments.Application/Configuration/Command/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Payments.Application.Contracts;
+using MediatR;
+
+namespace Jookli.Payments.Application.Configuration.Command
+{
+    public interface ICommandHandler<in TCommand> : 
+        IRequestHandler<TCommand> where TCommand : ICommand
+    {
+    }
+
+    public interface ICommandHandler<in TCommand, TResult> : 
+        IRequestHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+    {
+    }
+}
