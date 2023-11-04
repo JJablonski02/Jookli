77de0b53b8f9ab6bb62b1f0c3c374fa9f52020ca
JJablonski02 <kuba2805@icloud.com>
2023-11-04 20:24:36 +0100

    IQueryHandler

---
+++ src/Modules/Payments/Jookli.Payments.Application/Configuration/Query/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Payments.Application.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Configuration.Query
+{
+    public interface IQueryHandler<in TQuery, TResult> : 
+        IRequestHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+    }
+}
