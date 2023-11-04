493e7d3585ebd06289dafa5d639b7f10b0b5524c
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:57:50 +0100

    IStripeService

---
+++ src/Modules/Payments/Jookli.Payments.Application/Services/IStripeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Services
+{
+    public interface IStripeService
+    {
+    }
+}
