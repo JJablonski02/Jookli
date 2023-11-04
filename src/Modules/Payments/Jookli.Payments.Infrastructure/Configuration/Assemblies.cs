84c29c86578a09fdcc72ea5d5d10476a9a5c7f14
JJablonski02 <kuba2805@icloud.com>
2023-11-04 23:02:41 +0100

    Assemblies

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Assemblies.cs
@@ -0,0 +1,12 @@
+﻿using Jookli.Payments.Application.Configuration.Command;
+using System.Reflection;
+
+
+namespace Jookli.Payments.Infrastructure.Configuration
+{
+    internal static class Assemblies
+    {
+        public static readonly Assembly Application = typeof(InternalCommandBase).Assembly;
+
+    }
+}
