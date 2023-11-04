b373f4b52d1b72eb128ecb30ceb6d233a4776dd7
JJablonski02 <kuba2805@icloud.com>
2023-11-04 23:02:17 +0100

    QuartzModule

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Quartz/QuartzModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Quartz;
+
+namespace Jookli.Payments.Infrastructure.Configuration.Quartz
+{
+    public class QuartzModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => typeof(IJob).IsAssignableFrom(t))
+                .InstancePerDependency();
+        }
+    }
+}
