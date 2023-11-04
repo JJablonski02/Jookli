df32ea4102ba1c6e46948849606bea92323b0253
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:59:05 +0100

    OutboxAccessor

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Outbox/OutboxAccessor.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+
+namespace Jookli.Payments.Infrastructure.Configuration.Outbox
+{
+    public class OutboxAccessor : IOutbox
+    {
+        private readonly PaymentsContext _paymentsContext;
+
+        internal OutboxAccessor(PaymentsContext paymentContext)
+        {
+            _paymentsContext = paymentContext;
+        }
+
+        public void Add(OutboxMessage message)
+        {
+            _paymentsContext.OutboxMessages.Add(message);
+        }
+
+        public Task Save()
+        {
+            return Task.CompletedTask; // Save is done automatically using EF Core Change Tracking mechanism during SaveChanges.
+        }
+    }
+}
