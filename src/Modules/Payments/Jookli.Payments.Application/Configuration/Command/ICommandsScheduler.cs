363dca53098191862e3f62db8fee11b99ba4bdb4
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:57:40 +0100

    ICommandsScheduler

---
+++ src/Modules/Payments/Jookli.Payments.Application/Configuration/Command/ICommandsScheduler.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Payments.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Configuration.Command
+{
+    public interface ICommandsScheduler
+    {
+        Task EnqueueAsync(ICommand command);
+        Task EnqueueAsync<T>(ICommand<T> command);
+    }
+}
