9344a1cbabffd19e7350997abe8d699110de87e6
JJablonski02 <kuba2805@icloud.com>
2023-11-04 20:00:36 +0100

    Payment UserEntity

--- src/Modules/Payments/Jookli.Payments.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Jookli.Payments.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Modules/Payments/Jookli.Payments.Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using Jookli.BuildingBlocks.Domain;
+
+namespace Jookli.Payments.Domain.Entities.User
+{
+    public class UserEntity : Entity
+    {
+        public Guid UserId { get; set; }
+        public string? Email { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
