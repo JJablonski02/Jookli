00119e350ae7d776ba5f3d12c4eeac848539d796
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:58:07 +0100

    paymentsmodule

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/DataAccess/PaymentsModule.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.BuildingBlocks.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Logging;
+
+namespace Jookli.Payments.Infrastructure.Configuration.DataAccess
+{
+    internal class PaymentsModule : Autofac.Module
+    {
+        private readonly string _connectionString;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public PaymentsModule(string connectionString, ILoggerFactory loggerFactory)
+        {
+            _connectionString = connectionString;
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SqlConnectionFactory>()
+                .As<ISqlConnectionFactory>()
+                .WithParameter("connectionString", _connectionString)
+                .InstancePerLifetimeScope();
+
+            builder
+                .Register(c =>
+                {
+                    var dbContextOptionsBuilder = new DbContextOptionsBuilder<PaymentsContext>();
+                    dbContextOptionsBuilder.UseSqlServer(_connectionString);
+
+                    dbContextOptionsBuilder
+                    .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>();
+
+                    return new PaymentsContext(dbContextOptionsBuilder.Options, _loggerFactory);
+                })
+                .AsSelf().As<DbContext>().InstancePerLifetimeScope();
+
+            var infrastructureAssembly = typeof(PaymentsContext).Assembly;
+
+            builder.RegisterAssemblyTypes(infrastructureAssembly)
+                .Where(type => type.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope()
+                .FindConstructorsWith(new AllConstructorFinder());
+        }
+    }
+}
