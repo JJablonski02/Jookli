425c74fef818b15c6ac741d802be3c408a7b4e7d
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:59:45 +0100

    CommandsScheduler

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Processing/InternalCommands/CommandsScheduler.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.BuildingBlocks.Infrastructure.Serialization;
+using Jookli.Payments.Application.Configuration.Command;
+using Jookli.Payments.Application.Contracts;
+using Newtonsoft.Json;
+
+namespace Jookli.Payments.Infrastructure.Configuration.Processing.InternalCommands
+{
+    public class CommandsScheduler : ICommandsScheduler
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public CommandsScheduler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task EnqueueAsync(ICommand command)
+        {
+            var connection = this._sqlConnectionFactory.GetOpenConnection();
+
+            const string sqlInsert = "INSERT INTO [users].[InternalCommands] ([Id], [EnqueueDate] , [Type], [Data]) VALUES " +
+                                     "(@Id, @EnqueueDate, @Type, @Data)";
+
+            await connection.ExecuteAsync(sqlInsert, new
+            {
+                command.Id,
+                EnqueueDate = DateTime.UtcNow,
+                Type = command.GetType().FullName,
+                Data = JsonConvert.SerializeObject(command, new JsonSerializerSettings
+                {
+                    ContractResolver = new AllPropertiesContractResolver()
+                })
+            }); 
+        }
+
+        public async Task EnqueueAsync<T>(ICommand<T> command)
+        {
+            var connection = this._sqlConnectionFactory.GetOpenConnection();
+
+            const string sqlInsert = "INSERT INTO [users].[InternalCommands] ([Id], [EnqueueDate] , [Type], [Data]) VALUES " +
+                                     "(@Id, @EnqueueDate, @Type, @Data)";
+
+            await connection.ExecuteAsync(sqlInsert, new
+            {
+                command.Id,
+                Enqueue = DateTime.UtcNow,
+                Type = command.GetType().FullName,
+                Data = JsonConvert.SerializeObject(command, new JsonSerializerSettings 
+                { 
+                    ContractResolver = new AllPropertiesContractResolver()
+                })
+            });
+        }
+    }
+}
