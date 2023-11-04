062cca5508cca3ccb9330ea6a2c406cc601c909b
JJablonski02 <kuba2805@icloud.com>
2023-11-04 20:24:52 +0100

    InternalCommandBase

--- src/Modules/Payments/Jookli.Payments.Application/Configuration/Command/InternalCommandBase.cs
+++ src/Modules/Payments/Jookli.Payments.Application/Configuration/Command/InternalCommandBase.cs
@@ -1,9 +1,5 @@
 ﻿using Jookli.Payments.Application.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Jookli.Payments.Application.Configuration.Command
 {
