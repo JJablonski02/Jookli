76463a0e241261cc9d7751c42f18d39435791d11
JJablonski02 <kuba2805@icloud.com>
2023-11-04 22:59:54 +0100

    ProcessInternalCommandsCommand

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/Processing/InternalCommands/ProcessInternalCommandsCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.Payments.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Infrastructure.Configuration.Processing.InternalCommands
+{
+    internal class ProcessInternalCommandsCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
