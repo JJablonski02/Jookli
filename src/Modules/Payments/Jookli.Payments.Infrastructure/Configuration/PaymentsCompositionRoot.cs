606e9be62f1b5ee64674c9729c2cb0e7e006e08e
JJablonski02 <kuba2805@icloud.com>
2023-11-04 23:02:50 +0100

    PaymentsCompositionRoot

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/PaymentsCompositionRoot.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Infrastructure.Configuration
+{
+    internal static class PaymentsCompositionRoot
+    {
+        private static IContainer _container;
+
+        internal static void SetContainer(IContainer container)
+        {
+            _container = container;
+        }
+
+        internal static ILifetimeScope BeginLifetimeScope()
+        {
+            return _container.BeginLifetimeScope();
+        }
+    }
+}
