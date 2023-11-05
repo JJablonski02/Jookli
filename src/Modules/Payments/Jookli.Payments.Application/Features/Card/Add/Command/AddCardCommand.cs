c42b9fefe2f45b7245e7820a2eb3a04a40dcdde2
JJablonski02 <kuba2805@icloud.com>
2023-11-05 20:52:23 +0100

    AddCardCommand

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/Card/Add/Command/AddCardCommand.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Payments.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Features.Card.Add.Command
+{
+    public class AddCardCommand : CommandBase<Guid>
+    {
+        public AddCardCommand() { }
+
+    }
+}
