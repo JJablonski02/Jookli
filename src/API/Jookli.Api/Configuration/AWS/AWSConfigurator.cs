818f9a7925cb4e4faea2e5596ecac1644ee483a7
JJablonski02 <kuba2805@icloud.com>
2023-11-05 19:03:15 +0100

    Correct AWSConfiguration. Reads data from AWS. All credentials hidden.

---
+++ src/API/Jookli.Api/Configuration/AWS/AWSConfigurator.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleSystemsManagement.Model;
+using Amazon.SimpleSystemsManagement;
+using Jookli.Api.Configuration.Authorization;
+using Amazon;
+
+namespace Jookli.Api.Configuration.AWS
+{
+    public static class AWSConfigurator
+    {
+        public async static Task<IConfiguration> ConfigureBuilderAsync(IConfigurationBuilder builder)
+        {
+            var client = new AmazonSimpleSystemsManagementClient(RegionEndpoint.EUNorth1);
+
+            var request = new GetParameterRequest()
+            {
+                Name = $"/development/JookliApi/ConnectionString"
+            };
+
+            var value = await client.GetParameterAsync(request: request);
+
+            builder.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                {"JookliConnectionString", value.Parameter.Value}
+            });
+
+            var configuration = builder.Build();
+
+            return configuration;
+        }
+    }
+}
