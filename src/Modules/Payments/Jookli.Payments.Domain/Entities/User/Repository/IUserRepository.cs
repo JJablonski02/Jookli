3c328ae23856dcc1b43f4867f343afedff469c9a
JJablonski02 <kuba2805@icloud.com>
2023-11-07 17:15:53 +0100

    IUserRepository

---
+++ src/Modules/Payments/Jookli.Payments.Domain/Entities/User/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Domain.Entities.User.Repository
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(UserEntity userEntity, CancellationToken cancellationToken = default);
+    }
+}
