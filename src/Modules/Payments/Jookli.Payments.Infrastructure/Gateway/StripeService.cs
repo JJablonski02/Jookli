30fc964903f4c88268ceb4fa35223d32241daba1
JJablonski02 <kuba2805@icloud.com>
2023-11-07 22:27:12 +0100

    Stripe Service

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Gateway/StripeService.cs
@@ -0,0 +1,36 @@
+﻿using Jookli.Payments.Application.Services;
+using Stripe;
+using Stripe.Checkout;
+
+namespace Jookli.Payments.Infrastructure.Gateway
+{
+    internal sealed class StripeService : IStripeService
+    {
+        public async Task AddCardAsync()
+        {
+            StripeConfiguration.ApiKey = "MyKey";
+
+            var options = new TokenCreateOptions
+            {
+                Card = new TokenCardOptions
+                {
+                    Number = "4111111111111111",
+                    ExpMonth = 12,
+                    ExpYear = 2025,
+                    Cvc = "123"
+                }
+            };
+
+            var service = new TokenService();
+            Token stripeToken = service.Create(options);
+
+            var options = new AccountUpdateOptions
+            {
+                ExternalAccount = stripeToken.Id
+            };
+
+            var service = new AccountService();
+            Account updatedAccount = service.Update("ID profilu użytkownika w Stripe", options);
+        }
+    }
+}
