c81a3b832578c912fcb71bfaecec8201930c0874
JJablonski02 <kuba2805@icloud.com>
2023-11-07 17:16:24 +0100

    StripeService

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Service/StripeService.cs
@@ -0,0 +1,12 @@
+﻿using Jookli.Payments.Application.Services;
+using System;
+using System.Collections.Generic;
+
+
+namespace Jookli.Payments.Infrastructure.Service
+{
+    internal sealed class StripeService : IStripeService
+    {
+        public async Task AddCardAsync()
+    }
+}
