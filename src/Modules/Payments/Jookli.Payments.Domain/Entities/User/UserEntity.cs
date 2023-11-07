4d8bf63d103eb332b6ebbab4259564f73bfdf049
JJablonski02 <kuba2805@icloud.com>
2023-11-07 17:16:11 +0100

    UserEntity more properites

--- src/Modules/Payments/Jookli.Payments.Domain/Entities/User/UserEntity.cs
+++ src/Modules/Payments/Jookli.Payments.Domain/Entities/User/UserEntity.cs
@@ -1,11 +1,13 @@
 ﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.Payments.Domain.Entities.Card;
 
 namespace Jookli.Payments.Domain.Entities.User
 {
     public class UserEntity : Entity
     {
         public Guid UserId { get; set; }
-        public string? Email { get; set; }
-        public bool IsDeleted { get; set; }
+        public string Email { get; set; }
+        public Guid CardId { get; set; }
+        public CardEntity Card { get; set; }
     }
 }
