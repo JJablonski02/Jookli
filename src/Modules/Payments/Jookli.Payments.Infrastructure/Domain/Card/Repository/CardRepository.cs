3a1aa24a9a6f9906a261446507314ac85e725816
JJablonski02 <kuba2805@icloud.com>
2023-11-07 17:16:19 +0100

    Card Repository

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/Card/Repository/CardRepository.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.Payments.Domain.Entities.Card;
+using Jookli.Payments.Domain.Entities.Card.RepositoryContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Infrastructure.Domain.Card.Repository
+{
+    internal sealed class CardRepository : ICardRepository
+    {
+        private readonly PaymentsContext _paymentsContext;
+
+        public CardRepository(PaymentsContext paymentsContext)
+        {
+            _paymentsContext = paymentsContext;
+        }
+
+        public async Task AddAsync(CardEntity cardEntity, CancellationToken cancellation = default)
+        {
+            await _paymentsContext.AddAsync(cardEntity, cancellation);
+        }
+    }
+}
