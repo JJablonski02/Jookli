2e7c9fd8c265bd0da21fba88dcbcaa6dba2e985e
JJablonski02 <kuba2805@icloud.com>
2023-11-07 17:14:48 +0100

    RemoveCardCommand/Handler

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/Card/Remove/Command/RemoveCardCommand.cs
@@ -0,0 +1,19 @@
+﻿using Jookli.Payments.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Features.Card.Remove.Command
+{
+    public class RemoveCardCommand : CommandBase
+    {
+        public RemoveCardCommand()
+        {
+
+        }
+
+        public Guid UserId { get; set; }
+    }
+}
