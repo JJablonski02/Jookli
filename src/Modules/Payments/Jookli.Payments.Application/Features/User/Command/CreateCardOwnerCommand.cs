6b19e3393dfcbe9de07a0297c32ea7312c1253eb
JJablonski02 <kuba2805@icloud.com>
2023-11-08 18:44:38 +0100

    Payment CreateCardOwnerCommand

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/User/Command/CreateCardOwnerCommand.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.Payments.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Features.User.Command
+{
+    public class CreateCardOwnerCommand : InternalCommandBase
+    {
+        internal Guid UserId { get; set; }
+        internal string Email { get; set; }
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+
+        public CreateCardOwnerCommand(Guid Id, Guid userId, string email, string firstName, string lastName)
+        { 
+            UserId = userId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
