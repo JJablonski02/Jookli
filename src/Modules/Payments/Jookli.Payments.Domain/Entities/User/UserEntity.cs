02dabc446279bd62644874766092dcbe78441476
JJablonski02 <kuba2805@icloud.com>
2023-11-08 18:44:48 +0100

    UserEntity adjustments

--- src/Modules/Payments/Jookli.Payments.Domain/Entities/User/UserEntity.cs
+++ src/Modules/Payments/Jookli.Payments.Domain/Entities/User/UserEntity.cs
@@ -7,7 +7,9 @@ namespace Jookli.Payments.Domain.Entities.User
     {
         public Guid UserId { get; set; }
         public string Email { get; set; }
-        public Guid CardId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid CardSecret { get; set; }
         public CardEntity Card { get; set; }
     }
 }
