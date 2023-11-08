4766f308b6592b494cd951665f918773bc6b4104
JJablonski02 <kuba2805@icloud.com>
2023-11-08 19:05:22 +0100

    UserRepository

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/User/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.Payments.Domain.Entities.User;
+using Jookli.Payments.Domain.Entities.User.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Infrastructure.Domain.User.Repository
+{
+    internal sealed class UserRepository : IUserRepository
+    {
+        private readonly PaymentsContext _paymentsContext;
+
+        public UserRepository(PaymentsContext paymentsContext)
+        {
+            _paymentsContext = paymentsContext;
+        }
+
+        public async Task AddAsync(UserEntity userEntity, CancellationToken cancellationToken = default)
+        {
+            await _paymentsContext.User.AddAsync(userEntity, cancellationToken);
+        }
+    }
+}
