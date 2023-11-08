dcb109c52a1dad0662f3d03b6d484a897b1d2205
JJablonski02 <kuba2805@icloud.com>
2023-11-08 22:14:36 +0100

    AWS Secret to Stripe stored

--- src/API/Jookli.Api/Configuration/AWS/AWSConfigurator.cs
+++ src/API/Jookli.Api/Configuration/AWS/AWSConfigurator.cs
@@ -2,6 +2,9 @@
 using Amazon.SimpleSystemsManagement.Model;
 using Amazon.SimpleSystemsManagement;
 using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Newtonsoft.Json;
 
 namespace Jookli.Api.Configuration.AWS
 {
@@ -16,12 +19,45 @@ namespace Jookli.Api.Configuration.AWS
                 Name = $"/development/JookliApi/ConnectionString"
             };
 
-            var value = await client.GetParameterAsync(request: request);
+            try
+            {
+                var value = await client.GetParameterAsync(request: request);
 
-            builder.AddInMemoryCollection(new Dictionary<string, string>
+                builder.AddInMemoryCollection(new Dictionary<string, string>
             {
                 {"JookliConnectionString", value.Parameter.Value}
             });
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            var secret = "development/stripe/secret";
+
+            IAmazonSecretsManager secretClient = new AmazonSecretsManagerClient(RegionEndpoint.EUNorth1);
+
+            GetSecretValueRequest secretRequest = new GetSecretValueRequest
+            {
+                SecretId = secret,
+                VersionStage = "AWSCURRENT"
+            };
+
+            GetSecretValueResponse secretResponse;
+
+            try
+            {
+                secretResponse = await secretClient.GetSecretValueAsync(secretRequest);
+                builder.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "StripeSecret" , secretResponse.SecretString }
+                });
+            }
+            catch (Exception ex)
+            {
+
+            }
 
             var configuration = builder.Build();
 
