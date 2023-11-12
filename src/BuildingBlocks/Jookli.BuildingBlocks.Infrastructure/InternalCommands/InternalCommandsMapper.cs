4fc272a7da54bf8e083fa7160f0108558f4b8d25
JJablonski02 <kuba2805@icloud.com>
2023-11-12 22:28:16 +0100

    mapper ctor bug fixed

--- src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/InternalCommands/InternalCommandsMapper.cs
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/InternalCommands/InternalCommandsMapper.cs
@@ -9,6 +9,12 @@ namespace Jookli.BuildingBlocks.Infrastructure.InternalCommands
     public class InternalCommandsMapper : IInternalCommandsMapper
     {
         private readonly BiDictionary<string, Type> _internalCommandsMap;
+
+        public InternalCommandsMapper(BiDictionary<string, Type> internalCommandsMap)
+        {
+            _internalCommandsMap = internalCommandsMap;
+        }
+
         public string GetName(Type type)
         {
             return _internalCommandsMap.TryGetBySecond(type, out var name) ? name : null;
