4a4239c5af311ed3c79581520fa1e1843a795390
JJablonski02 <kuba2805@icloud.com>
2023-11-12 22:04:24 +0100

    InternalCommandConfiguration

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/InternalCommands/Config/InternalCommandEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.InternalCommands.Config
+{
+    internal class InternalCommandEntityTypeConfiguration : IEntityTypeConfiguration<InternalCommand>
+    {
+        public void Configure(EntityTypeBuilder<InternalCommand> builder)
+        {
+            builder.ToTable("Payments_InternalCommands");
+
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Id).ValueGeneratedNever();
+        }
+    }
+}
