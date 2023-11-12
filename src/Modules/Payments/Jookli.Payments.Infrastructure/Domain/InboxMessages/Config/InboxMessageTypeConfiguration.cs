1d98d041729e1274af36b60d859adb2771d9dded
JJablonski02 <kuba2805@icloud.com>
2023-11-12 22:04:12 +0100

    InboxTypeConfiguration

---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/InboxMessages/Config/InboxMessageTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.Inbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.InboxMessages.Config
+{
+    internal class InboxMessageTypeConfiguration : IEntityTypeConfiguration<InboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<InboxMessage> builder)
+        {
+            builder.ToTable("Payments_InboxMessage");
+
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
