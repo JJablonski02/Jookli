f49b3feb38adf77bac8f5ed57682b5f52412cd84
JJablonski02 <kuba2805@icloud.com>
2023-11-13 22:12:54 +0100

    InboxMessage modified

--- src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
@@ -9,16 +9,17 @@ namespace Jookli.BuildingBlocks.Infrastructure.Inbox
     public class InboxMessage
     {
         public Guid Id { get; set; }
+        public DateTime OccurredOn { get; set; }
         public string Type { get; set; }
         public string Data { get; set; }
         public DateTime? ProcessedDate { get; set; }
 
-        public InboxMessage(Guid id, string type, string data, DateTime? processedDate)
+        public InboxMessage(DateTime occurredOn, string type, string data)
         {
-            Id = id;
-            Type = type;
-            Data = data;
-            ProcessedDate = processedDate;
+            this.Id = Guid.NewGuid();
+            this.OccurredOn = occurredOn;
+            this.Type = type;
+            this.Data = data;
         }
 
         private InboxMessage() { }
