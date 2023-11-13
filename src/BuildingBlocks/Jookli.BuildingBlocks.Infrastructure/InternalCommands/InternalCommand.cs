d6991b44cfba541417abb1d66d8390a41a42507c
JJablonski02 <kuba2805@icloud.com>
2023-11-13 22:13:09 +0100

    InternalCommand

--- src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/InternalCommands/InternalCommand.cs
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/InternalCommands/InternalCommand.cs
@@ -11,6 +11,8 @@ namespace Jookli.BuildingBlocks.Infrastructure.InternalCommands
         public Guid Id { get; set; }
         public string Type { get; set; }
         public string Data { get; set; }
+        public string? Error { get; set; }
+        public DateTime EnqueueDate { get; set; }
         public DateTime? ProcessedDate { get; set; }
     }
 }
