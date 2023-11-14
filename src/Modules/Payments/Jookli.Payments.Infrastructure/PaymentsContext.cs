93ba1db79a5ddf56e0a008b81b1a4208f98c5cbc
JJablonski02 <kuba2805@icloud.com>
2023-11-14 23:52:25 +0100

    Payments dbcontext

--- src/Modules/Payments/Jookli.Payments.Infrastructure/PaymentsContext.cs
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/PaymentsContext.cs
@@ -1,15 +1,18 @@
 ﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Jookli.BuildingBlocks.Infrastructure.Inbox;
 using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
 using Jookli.Payments.Domain.Entities.Card;
 using Jookli.Payments.Domain.Entities.User;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Jookli.Payments.Infrastructure
 {
     public class PaymentsContext : DbContext
     {
         public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<InboxMessage> InboxMessage { get; set; }
         public DbSet<InternalCommand> InternalCommands { get; set; }
         public DbSet<CardEntity> Cards { get; set; }
         public DbSet<UserEntity> User { get; set; }
@@ -18,6 +21,11 @@ namespace Jookli.Payments.Infrastructure
         public PaymentsContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
         {
             _logger = loggerFactory;
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
