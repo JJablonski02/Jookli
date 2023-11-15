0f0835c562d508f192b2372fce42cb8535382e07
JJablonski02 <kuba2805@icloud.com>
2023-11-15 22:50:03 +0100

    migration fixed name bug

--- src/Modules/Payments/Jookli.Payments.Infrastructure/Migrations/20231115180921_PaymentsFixedBugs.Designer.cs
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Migrations/20231115180921_PaymentsFixedBugs.Designer.cs
@@ -4,6 +4,7 @@ using Jookli.Payments.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jookli.Payments.Infrastructure.Migrations
 {
     [DbContext(typeof(PaymentsContext))]
-    partial class PaymentsContextModelSnapshot : ModelSnapshot
+    [Migration("20231115180921_PaymentsFixedBugs")]
+    partial class PaymentsFixedBugs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Migrations/20231115180921_PaymentsFixedBugs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jookli.Payments.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentsFixedBugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OccurredOn",
+                table: "Payments_InboxMessage",
+                newName: "OccuredOn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OccuredOn",
+                table: "Payments_InboxMessage",
+                newName: "OccurredOn");
+        }
+    }
+}
