4f7f06d9bb4a0ee06877e81ec1e02d1e8c5191df
JJablonski02 <kuba2805@icloud.com>
2023-11-15 22:51:05 +0100

    occured inbox

--- src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
+++ src/BuildingBlocks/Jookli.BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
@@ -9,15 +9,15 @@ namespace Jookli.BuildingBlocks.Infrastructure.Inbox
     public class InboxMessage
     {
         public Guid Id { get; set; }
-        public DateTime OccurredOn { get; set; }
+        public DateTime OccuredOn { get; set; }
         public string Type { get; set; }
         public string Data { get; set; }
         public DateTime? ProcessedDate { get; set; }
 
-        public InboxMessage(DateTime occurredOn, string type, string data)
+        public InboxMessage(DateTime occuredOn, string type, string data)
         {
             this.Id = Guid.NewGuid();
-            this.OccurredOn = occurredOn;
+            this.OccuredOn = occuredOn;
             this.Type = type;
             this.Data = data;
         }
