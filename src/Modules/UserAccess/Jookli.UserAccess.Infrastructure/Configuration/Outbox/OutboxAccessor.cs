674f07827c3d88cf6cd9608ea89db00dc0276b04
JJablonski02 <kuba2805@icloud.com>
2023-11-17 23:11:33 +0100

    outbox accessor

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Configuration/Outbox/OutboxAccessor.cs
@@ -0,0 +1,28 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Configuration.Outbox
+{
+    public class OutboxAccessor : IOutbox
+    {
+        private readonly UserAccessContext _userAccessContext;
+        public OutboxAccessor(UserAccessContext userAccessContext)
+        {
+            _userAccessContext = userAccessContext;
+        }
+
+        public void Add(OutboxMessage message)
+        {
+            _userAccessContext.OutboxMessages.Add(message);
+        }
+
+        public Task Save()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
