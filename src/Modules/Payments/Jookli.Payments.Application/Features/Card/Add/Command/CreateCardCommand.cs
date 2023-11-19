f0c08fcd4bcd40ab91bb13614c1b2ccf73f30fc0
JJablonski02 <kuba2805@icloud.com>
2023-11-19 22:47:50 +0100

    CreateCardCommand

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/Card/Add/Command/CreateCardCommand.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.Payments.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Features.Card.Add.Command
+{
+    public class CreateCardCommand : CommandBase
+    {
+        public CreateCardCommand(string cardId, string expiryDate, string cardNumber, string cVV)
+        {
+            CardId = cardId;
+            ExpiryDate = expiryDate;
+            CardNumber = cardNumber;
+            CVV = cVV;
+        }
+
+        public Guid UserId { get; set; }
+        public string CardId { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+    }
+}
