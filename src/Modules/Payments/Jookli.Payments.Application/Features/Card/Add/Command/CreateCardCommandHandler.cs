5f2755930242be5a49705726e62817f2e8422af4
JJablonski02 <kuba2805@icloud.com>
2023-11-19 22:47:57 +0100

    CreateCardCommandHandler

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/Card/Add/Command/CreateCardCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.Payments.Application.Configuration.Command;
+using Jookli.Payments.Domain.Entities.Card.RepositoryContract;
+using MediatR;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Jookli.Payments.Application.Features.Card.Add.Command
+{
+    internal class CreateCardCommandHandler : ICommandHandler<CreateCardCommand>
+    {
+        private readonly ICardRepository _cardRepository;
+        public async Task<Unit> Handle(CreateCardCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _cardRepository.GetNumberAsync(command.CardNumber, cancellationToken);
+
+            if(result != null)
+            {
+                throw new ArgumentException($"User with card number: {command.CardNumber} exists");
+            }
+
+
+            return Unit.Value;
+        }
+    }
+}
