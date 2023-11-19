7a701a03af2bf9c537960b2743ce5e6eb264a0f8
JJablonski02 <kuba2805@icloud.com>
2023-11-19 22:47:26 +0100

    UserEntity

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.User
+{
+    public class UserEntity
+    {
+        public Guid UserId { get; set; } 
+    }
+}
