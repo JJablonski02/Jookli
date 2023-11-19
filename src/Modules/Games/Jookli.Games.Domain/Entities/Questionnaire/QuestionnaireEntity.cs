2304e04700f8f9479ea91c401f1099a359c09f02
JJablonski02 <kuba2805@icloud.com>
2023-11-19 22:47:19 +0100

    QuestionnaireEntity

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/Questionnaire/QuestionnaireEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.Questionnaire
+{
+    internal class QuestionnaireEntity
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Company { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
