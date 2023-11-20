21eb0194c1940eec0df6f3ef110516d5af23c35e
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:15:32 +0100

    Commands

---
+++ src/Modules/Games/Jookli.Games.Application/Configuration/Command/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.Games.Application.Contracts;
+using MediatR;
+
+namespace Jookli.Games.Application.Configuration.Command
+{
+    public interface ICommandHandler<in TCommand> :
+        IRequestHandler<TCommand> 
+        where TCommand : ICommand
+    {
+    }
+
+    public interface ICommandHandler<in TCommand, TResult> :
+        IRequestHandler<TCommand, TResult>
+        where TCommand : ICommand<TResult>
+    {
+    }
+}
