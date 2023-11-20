2a1556a4f080fba0d48af8b5d166412d9a7d1d98
JJablonski02 <kuba2805@icloud.com>
2023-11-20 23:24:27 +0100

    profile created notification

---
+++ src/Modules/Games/Jookli.Games.Application/Features/Profile/Notification/ProfileCreatedNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.Games.Domain.Entities.Profile.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Features.Profile.Notification
+{
+    internal class ProfileCreatedNotification : DomainNotificationBase<ProfileCreatedDomainEvent>
+    {
+        public ProfileCreatedNotification(ProfileCreatedDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
