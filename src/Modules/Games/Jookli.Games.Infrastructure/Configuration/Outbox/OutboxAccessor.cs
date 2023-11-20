5fdc65a6f3d558fb85fa738ec94151c0ca1fa0fa
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:16:55 +0100

    Outbox Games

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Outbox/OutboxAccessor.cs
@@ -0,0 +1,28 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration.Outbox
+{
+    public class OutboxAccessor : IOutbox
+    {
+        private readonly GamesContext _gamesContext;
+        public OutboxAccessor(GamesContext userAccessContext)
+        {
+            _gamesContext = userAccessContext;
+        }
+
+        public void Add(OutboxMessage message)
+        {
+            _gamesContext.OutboxMessages.Add(message);
+        }
+
+        public Task Save()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Outbox/OutboxMessageGlobalTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration.Outbox
+{
+    internal class OutboxMessageGlobalTypeConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.ToTable("OutboxMessagesGlobal");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+        }
+    }
+}
