eb73ceb10fd5467bc825e9a144b8848432277048
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:18:22 +0100

    Fluent validation

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Processing/ValidationCommandHandlerDecorator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Jookli.BuildingBlocks.Application.Exceptions;
+using Jookli.Games.Application.Configuration.Command;
+using Jookli.Games.Application.Contracts;
+using MediatR;
+
+namespace Jookli.Games.Infrastructure.Configuration.Processing
+{
+    public class ValidationCommandHandlerDecorator<T> : ICommandHandler<T> where T : ICommand
+    {
+        private readonly IList<IValidator<T>> _validators;
+        private readonly ICommandHandler<T> _decorator;
+
+        public ValidationCommandHandlerDecorator(IList<IValidator<T>> validators, ICommandHandler<T> decorator)
+        {
+            _validators = validators;
+            _decorator = decorator;
+        }
+
+        public Task<Unit> Handle(T command, CancellationToken cancellationToken)
+        {
+            var errors = _validators.Select(v => v.Validate(command))
+                .SelectMany(result => result.Errors).Where(er => er != null)
+                .ToList();
+
+            if (errors.Any())
+            {
+                throw new InvalidCommandException(errors.Select(er => er.ErrorMessage).ToList());
+            }
+
+            return _decorator.Handle(command, cancellationToken);
+        }
+    }
+}
