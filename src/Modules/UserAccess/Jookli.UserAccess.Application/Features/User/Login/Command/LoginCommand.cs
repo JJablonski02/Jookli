ce1b1a22980bfb4e55b302992777c584520cd344
JJablonski02 <kuba2805@icloud.com>
2023-11-20 19:28:40 +0100

    Login Command

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Login/Command/LoginCommand.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Login/Command/LoginCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Jookli.UserAccess.Application.Features.User.Login.Command
 {
-    public class LoginCommand
+    public class LoginCommand : CommandBase
     {
-        public string UserName { get; }
-        public string Password { get; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
