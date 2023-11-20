849d25c5c0c72c5dfa4a7974b00f52e1dbda69b7
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:17:03 +0100

    Processing Inbox

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Processing/Inbox/ProcessInboxCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.Games.Application.Configuration.Command;
+using MediatR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration.Processing.Inbox
+{
+    internal class ProcessInboxCommandHandler : ICommandHandler<ProcessInboxCommand>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public ProcessInboxCommandHandler(IMediator mediator, ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _mediator = mediator;
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task<Unit> Handle(ProcessInboxCommand command, CancellationToken cancellationToken)
+        {
+            var connection = this._sqlConnectionFactory.GetOpenConnection();
+            string sql = "SELECT " +
+                         $"Id AS '{nameof(InboxMessageDto.Id)}', " +
+                         $"Type AS '{nameof(InboxMessageDto.Type)}', " +
+                         $"Data AS '{nameof(InboxMessageDto.Data)}' " +
+                         "FROM dbo.Games_InboxMessage AS InboxMessage " +
+                         "WHERE InboxMessage.ProcessedDate IS NULL " +
+                         "ORDER BY InboxMessage.OccurredOn";
+
+
+            var messagess = await connection.QueryAsync<InboxMessageDto>(sql);
+
+            var messages = new List<InboxMessageDto>();
+
+            const string sqlUpdateProcessedDate = "UPDATE dbo.Games_InboxMessage " +
+                                                  "SET ProcessedDate = @Date " +
+                                                  "WHERE Id = @Id";
+
+            foreach (var message in messages)
+            {
+                var messageAssembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .SingleOrDefault(assembly => message.Type.Contains(assembly.GetName().Name));
+
+                Type type = messageAssembly.GetType(message.Type);
+                var request = JsonConvert.DeserializeObject(message.Data, type);
+
+                try
+                {
+                    await _mediator.Publish((INotification)request, cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+                await connection.ExecuteAsync(sqlUpdateProcessedDate, new
+                {
+                    Date = DateTime.UtcNow,
+                    message.Id
+                });
+            }
+
+            return Unit.Value;
+        }
+    }
+}
