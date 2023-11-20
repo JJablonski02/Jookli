f096aca9fe5fae849081802d5850cc17c2b5bbf7
JJablonski02 <kuba2805@icloud.com>
2023-11-20 23:25:35 +0100

    profile entity

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/Profile/ProfileEntity.cs
@@ -0,0 +1,20 @@
+﻿using Jookli.Games.Domain.Entities.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.Profile
+{
+    public class ProfileEntity
+    {
+        public Guid profileId { get; set; }
+        public string profileName { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+        public bool isDeleted { get; set; }
+        public ICollection<GameEntity> games { get; set; }
+
+    }
+}
