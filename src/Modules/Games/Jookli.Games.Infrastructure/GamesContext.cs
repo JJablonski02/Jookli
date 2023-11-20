7ece64b0bad14b7e0d32c183adf841101dc2fc28
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:18:54 +0100

    cleanup

--- src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
+++ src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
@@ -1,10 +1,24 @@
-﻿using Jookli.Games.Domain.Entities.Game;
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Jookli.BuildingBlocks.Infrastructure.Inbox;
+using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Jookli.Games.Domain.Entities.Game;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Jookli.Games.Infrastructure
 {
     public class GamesContext : DbContext
     {
+        public GamesContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        }
+
+
         public DbSet<GameEntity> Games { get; set; }
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<InternalCommand> InternalCommands { get; set; }
+        public DbSet<InboxMessage> InboxMessage { get; set; }
+        private readonly ILoggerFactory _logger;
     }
 }
