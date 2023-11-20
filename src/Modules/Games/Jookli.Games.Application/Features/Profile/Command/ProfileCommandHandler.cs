411d9dbef12da68915a94a3bfe224dec7aa15c9a
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:29:19 +0100

    profile command

---
+++ src/Modules/Games/Jookli.Games.Application/Features/Profile/Command/ProfileCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.Games.Application.Configuration.Command;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Features.Profile.Command
+{
+    internal class ProfileCommandHandler : ICommandHandler<ProfileCommand>
+    {
+        public Task<Unit> Handle(ProfileCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
