2f5c43b81a06449085f04925b0a48482bfbfb13b
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:29:25 +0100

    create user command

---
+++ src/Modules/Games/Jookli.Games.Application/Features/User/Command/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.Games.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Features.User.Command
+{
+    internal class CreateUserCommand : CommandBase
+    {
+    }
+}
---
+++ src/Modules/Games/Jookli.Games.Application/Features/User/Command/CreateUserCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.Games.Application.Configuration.Command;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Features.User.Command
+{
+    internal class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
+    {
+        public Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
