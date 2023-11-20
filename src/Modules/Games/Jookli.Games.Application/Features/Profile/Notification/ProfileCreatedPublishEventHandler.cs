f283ef5ff8378ba3273e0ca93081d2700142228f
JJablonski02 <kuba2805@icloud.com>
2023-11-20 23:25:16 +0100

    profile created publishment

---
+++ src/Modules/Games/Jookli.Games.Application/Features/Profile/Notification/ProfileCreatedPublishEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.Games.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Application.Features.Profile.Notification
+{
+    internal class ProfileCreatedPublishEventHandler : INotificationHandler<ProfileCreatedNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+        public ProfileCreatedPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(ProfileCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new ProfileCreatedIntegrationEvent(
+                notification.DomainEvent.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.ProfileId
+                ));
+        }
+    }
+}
