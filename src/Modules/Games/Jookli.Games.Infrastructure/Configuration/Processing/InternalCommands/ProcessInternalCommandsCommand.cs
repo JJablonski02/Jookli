827b344053029ff6b4cc8490e1a1b30826511928
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:17:11 +0100

    Processing Internal Commands

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Processing/InternalCommands/ProcessInternalCommandsCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.Games.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration.Processing.InternalCommands
+{
+    internal class ProcessInternalCommandsCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
