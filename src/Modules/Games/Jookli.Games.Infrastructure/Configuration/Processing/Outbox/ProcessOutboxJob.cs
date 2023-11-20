206c5cb7600a95e1ff9b152b2c036c24601a807f
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:17:18 +0100

    Processing Outbox

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Processing/Outbox/ProcessOutboxJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration.Processing.Outbox
+{
+    [DisallowConcurrentExecution]
+    public class ProcessOutboxJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await CommandsExecutor.Execute(new ProcessOutboxCommand());
+        }
+    }
+}
