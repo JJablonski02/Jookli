afaa2059d4d29214abc12f9ddbe5d818289ddd53
JJablonski02 <kuba2805@icloud.com>
2023-11-20 19:28:48 +0100

    Login Command Handler

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Login/Command/LoginCommandHandler.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Login/Command/LoginCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Jookli.BuildingBlocks.Application.Data;
+using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.User;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,23 @@ using System.Threading.Tasks;
 
 namespace Jookli.UserAccess.Application.Features.User.Login.Command
 {
-    internal class LoginCommandHandler
+    public class LoginCommandHandler : ICommandHandler<LoginCommand>
     {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public LoginCommandHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public Task<Unit> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var connection = _sqlConnectionFactory.GetOpenConnection();
+            //in progress
+            //dapper
+            //validation
+            //result
+            return null;
+        }
     }
 }
