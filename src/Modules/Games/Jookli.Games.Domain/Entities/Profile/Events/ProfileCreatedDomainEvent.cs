1ec63325ee4e7beb7f1986a12bce40273aad65e3
JJablonski02 <kuba2805@icloud.com>
2023-11-20 23:25:24 +0100

    profile created domain event

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/Profile/Events/ProfileCreatedDomainEvent.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.Profile.Events
+{
+    public class ProfileCreatedDomainEvent : DomainEventBase
+    {
+        public ProfileCreatedDomainEvent(Guid profileId, string profileName)
+        {
+            ProfileId = profileId;
+            ProfileName = profileName; 
+        }
+
+        public Guid ProfileId { get; set; }
+        public string ProfileName { get; set; }
+    }
+}
