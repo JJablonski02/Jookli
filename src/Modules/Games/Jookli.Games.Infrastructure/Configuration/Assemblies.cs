3f8c814f917c2c196a1de6ef65e91cbd9d1ba27d
JJablonski02 <kuba2805@icloud.com>
2023-11-20 20:18:48 +0100

    assemblies

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Configuration/Assemblies.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Games.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Configuration
+{
+    internal static class Assemblies
+    {
+        public static readonly Assembly Application = typeof(InternalCommandBase).Assembly;
+    }
+}
