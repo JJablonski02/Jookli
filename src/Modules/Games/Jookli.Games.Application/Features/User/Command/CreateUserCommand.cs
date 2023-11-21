64a432fc807d4c06fbbd226536b07abad34632da
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:44:11 +0100

    create user command

--- src/Modules/Games/Jookli.Games.Application/Features/User/Command/CreateUserCommand.cs
+++ src/Modules/Games/Jookli.Games.Application/Features/User/Command/CreateUserCommand.cs
@@ -9,5 +9,16 @@ namespace Jookli.Games.Application.Features.User.Command
 {
     internal class CreateUserCommand : CommandBase
     {
+        public CreateUserCommand(Guid id, Guid userId, string email, string firstName, string lastName)
+        {
+            UserId = userId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
