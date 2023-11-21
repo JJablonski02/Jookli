cf8baada4f966c60e89e124dcade88b70eaa5969
JJablonski02 <kuba2805@icloud.com>
2023-11-21 22:53:53 +0100

    migration created and db updated

--- src/Modules/Games/Jookli.Games.Infrastructure/Migrations/20231121213449_ProfilesMigration.Designer.cs
+++ src/Modules/Games/Jookli.Games.Infrastructure/Migrations/20231121213449_ProfilesMigration.Designer.cs
@@ -4,6 +4,7 @@ using Jookli.Games.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jookli.Games.Infrastructure.Migrations
 {
     [DbContext(typeof(GamesContext))]
-    partial class GamesContextModelSnapshot : ModelSnapshot
+    [Migration("20231121213449_ProfilesMigration")]
+    partial class ProfilesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Migrations/20231121213449_ProfilesMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jookli.Games.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfilesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProfileEntityProfileId",
+                table: "Games_Game",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Games_Profile",
+                columns: table => new
+                {
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_Profile", x => x.ProfileId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Game_ProfileEntityProfileId",
+                table: "Games_Game",
+                column: "ProfileEntityProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Game_Games_Profile_ProfileEntityProfileId",
+                table: "Games_Game",
+                column: "ProfileEntityProfileId",
+                principalTable: "Games_Profile",
+                principalColumn: "ProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Game_Games_Profile_ProfileEntityProfileId",
+                table: "Games_Game");
+
+            migrationBuilder.DropTable(
+                name: "Games_Profile");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_Game_ProfileEntityProfileId",
+                table: "Games_Game");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileEntityProfileId",
+                table: "Games_Game");
+        }
+    }
+}
