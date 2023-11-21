547e0366de8b0fe55e13b632ee6113a442816304
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:43:35 +0100

    create user domain evnet

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/User/Events/CreateUserDomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.User.Events
+{
+    public class CreateUserDomainEvent : DomainEventBase
+    {
+        public CreateUserDomainEvent(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; }
+    }
+}
