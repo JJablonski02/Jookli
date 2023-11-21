aea3ac315a1417ee5e1f0a69785019e51fa5b742
JJablonski02 <kuba2805@icloud.com>
2023-11-21 22:53:05 +0100

    profile repository

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Domain/Profile/Repository/ProfileRepository.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.Games.Domain.Entities.Profile;
+using Jookli.Games.Domain.Entities.Profile.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Domain.Profile.Repository
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        private readonly IProfileRepository _profileRepository;
+        public ProfileRepository(IProfileRepository profileRepository)
+        {
+            _profileRepository = profileRepository;
+        }
+
+        public async Task AddAsync(ProfileEntity profileEntity, CancellationToken cancellationToken)
+        {
+            await _profileRepository.AddAsync(profileEntity, cancellationToken);
+        }
+    }
+}
