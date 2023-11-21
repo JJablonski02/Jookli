992d72a6b7f801280e16d931bd8a34dc78bffae4
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:42:58 +0100

    game entity config

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Domain/Games/Config/GameTypeConfig.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.Games.Domain.Entities.Game;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Domain.Games.Config
+{
+    internal sealed class GameTypeConfig : IEntityTypeConfiguration<GameEntity>
+    {
+        public void Configure(EntityTypeBuilder<GameEntity> builder)
+        {
+            builder.ToTable("Games_Game");
+
+            builder.HasKey(x => x.GameId);
+        }
+    }
+}
