bc0e912991941c59c3187c40d93faf58dfc73792
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:43:14 +0100

    games user entity

--- src/Modules/Games/Jookli.Games.Domain/Entities/User/UserEntity.cs
+++ src/Modules/Games/Jookli.Games.Domain/Entities/User/UserEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace Jookli.Games.Domain.Entities.User
 {
-    public class UserEntity
+    public class UserEntity : Entity
     {
         public Guid UserId { get; set; } 
         public bool IsDeleted { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
