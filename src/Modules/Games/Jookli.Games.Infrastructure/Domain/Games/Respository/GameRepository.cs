09bfba6b81906569d7641d03049ecc413ed4850c
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:42:49 +0100

    game repository

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Domain/Games/Respository/GameRepository.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.Games.Domain.Entities.Game;
+using Jookli.Games.Domain.Entities.Game.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Domain.Games.Respository
+{
+    internal class GameRepository : IGameRepository
+    {
+        private readonly GamesContext _gamesContext;
+        public GameRepository(GamesContext gamesContext)
+        {
+            _gamesContext = gamesContext;
+        }
+
+        public async Task AddAsync(GameEntity gameEntity, CancellationToken cancellationToken)
+        {
+            await _gamesContext.AddAsync(gameEntity, cancellationToken);
+        }
+    }
+}
