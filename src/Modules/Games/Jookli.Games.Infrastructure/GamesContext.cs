731c1360fa7514700045ff4021fd65d4a952f49d
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:42:05 +0100

    games context adjustments

--- src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
+++ src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
@@ -4,21 +4,26 @@ using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
 using Jookli.Games.Domain.Entities.Game;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Jookli.Games.Infrastructure
 {
     public class GamesContext : DbContext
     {
-        public GamesContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
-        {
-            _logger = loggerFactory;
-        }
-
-
         public DbSet<GameEntity> Games { get; set; }
         public DbSet<OutboxMessage> OutboxMessages { get; set; }
         public DbSet<InternalCommand> InternalCommands { get; set; }
         public DbSet<InboxMessage> InboxMessage { get; set; }
         private readonly ILoggerFactory _logger;
+        public GamesContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
