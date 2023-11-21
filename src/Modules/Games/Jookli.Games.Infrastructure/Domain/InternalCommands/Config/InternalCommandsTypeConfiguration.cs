46b98c17a9b82c08bb46d4918d85f8e57107da5b
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:42:27 +0100

    internal commands config

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Domain/InternalCommands/Config/InternalCommandsTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Domain.InternalCommands.Config
+{
+    internal sealed class InternalCommandsTypeConfiguration : IEntityTypeConfiguration<InternalCommand>
+    {
+        public void Configure(EntityTypeBuilder<InternalCommand> builder)
+        {
+            builder.ToTable("Games_InternalCommands");
+
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Id).ValueGeneratedNever();
+        }
+    }
+}
