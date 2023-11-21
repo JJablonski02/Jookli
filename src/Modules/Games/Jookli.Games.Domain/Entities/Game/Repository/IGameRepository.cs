c79f414ce527b07b914f6fc06352db39d981673f
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:43:45 +0100

    game repository

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/Game/Repository/IGameRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.Game.Repository
+{
+    public interface IGameRepository
+    {
+        Task AddAsync(GameEntity gameEntity, CancellationToken cancellationToken);
+    }
+}
