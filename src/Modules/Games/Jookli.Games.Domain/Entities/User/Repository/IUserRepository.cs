7217ff7e31d983a63a5f45b1bd6b0660aefcee3f
JJablonski02 <kuba2805@icloud.com>
2023-11-21 21:43:26 +0100

    user repository

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/User/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.User.Repository
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(UserEntity userEntity, CancellationToken cancellationToken);
+        Task GetById(Guid Id, CancellationToken cancellationToken);
+    }
+}
