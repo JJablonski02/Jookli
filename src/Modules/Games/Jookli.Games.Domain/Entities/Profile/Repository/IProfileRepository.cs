930e9393b147cc092b875641094bc2604d180b51
JJablonski02 <kuba2805@icloud.com>
2023-11-21 22:52:38 +0100

    profile type configuration

---
+++ src/Modules/Games/Jookli.Games.Domain/Entities/Profile/Repository/IProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Domain.Entities.Profile.Repository
+{
+    public interface IProfileRepository
+    {
+        Task AddAsync(ProfileEntity profileEntity, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Domain/Profile/Config/ProfileTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.Games.Domain.Entities.Profile;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Games.Infrastructure.Domain.Profile.Config
+{
+    internal sealed class ProfileTypeConfiguration : IEntityTypeConfiguration<ProfileEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProfileEntity> builder)
+        {
+            builder.ToTable("Games_Profile");
+
+            builder.HasKey(x => x.ProfileId);
+        }
+    }
+}
