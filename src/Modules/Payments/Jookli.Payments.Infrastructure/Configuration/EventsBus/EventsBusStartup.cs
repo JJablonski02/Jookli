78fe7459443f4cf71eb267deed04e13e5823402e
JJablonski02 <kuba2805@icloud.com>
2023-11-23 22:16:49 +0100

    GamesProfile created integration event in payments

--- src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/EventsBus/EventsBusStartup.cs
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Configuration/EventsBus/EventsBusStartup.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.Games.IntegrationEvents;
 using Jookli.UserAccess.IntegrationEvents;
 using Serilog;
 
@@ -17,6 +18,7 @@ namespace Jookli.Payments.Infrastructure.Configuration.EventsBus
             var eventBus = PaymentsCompositionRoot.BeginLifetimeScope().Resolve<IEventsBus>();
 
             SubscribeToIntegrationEvent<NewUserRegisteredIntegrationEvent>(eventBus, logger);
+            SubscribeToIntegrationEvent<ProfileCreatedIntegrationEvent>(eventBus, logger);
         }
 
         private static void SubscribeToIntegrationEvent<T>(IEventsBus eventBus, ILogger logger) where T : IntegrationEvent
