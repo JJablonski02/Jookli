ea01067e9af2fd20b44f77ad10e35c19e268ed32
JJablonski02 <kuba2805@icloud.com>
2023-11-25 01:55:01 +0100

    games context new Objects

--- src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
+++ src/Modules/Games/Jookli.Games.Infrastructure/GamesContext.cs
@@ -2,6 +2,8 @@
 using Jookli.BuildingBlocks.Infrastructure.Inbox;
 using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
 using Jookli.Games.Domain.Entities.Game;
+using Jookli.Games.Domain.Entities.Profile;
+using Jookli.Games.Domain.Entities.User;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
@@ -14,6 +16,9 @@ namespace Jookli.Games.Infrastructure
         public DbSet<OutboxMessage> OutboxMessages { get; set; }
         public DbSet<InternalCommand> InternalCommands { get; set; }
         public DbSet<InboxMessage> InboxMessage { get; set; }
+        public DbSet<UserEntity> Users {get; set;}    
+        public DbSet<ProfileEntity> Profile { get; set;}    
+
         private readonly ILoggerFactory _logger;
         public GamesContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
         {
