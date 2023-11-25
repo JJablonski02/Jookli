14a5facf8f19920edc2b24b6f1d04d5ead91fa3c
JJablonski02 <kuba2805@icloud.com>
2023-11-25 01:54:23 +0100

    create user command in payments

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/User/Command/CreateUserCommand.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.Payments.Application.Configuration.Command;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Payments.Application.Features.User.Command
+{
+    internal sealed class CreateUserCommand : InternalCommandBase
+    {
+        public CreateUserCommand(Guid Id, Guid userId, string? email, string? firstName, string? lastName) : base(Id)
+        {
+            UserId = userId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Guid UserId { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
