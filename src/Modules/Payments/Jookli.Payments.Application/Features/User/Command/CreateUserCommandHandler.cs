a2e4f1522874ad5b4433e791cec5209ad0079f55
JJablonski02 <kuba2805@icloud.com>
2023-11-25 01:54:09 +0100

    CreateUser command handler in payments

---
+++ src/Modules/Payments/Jookli.Payments.Application/Features/User/Command/CreateUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Jookli.Payments.Application.Configuration.Command;
+using Jookli.Payments.Domain.Entities.User;
+using Jookli.Payments.Domain.Entities.User.Repository;
+using MediatR;
+namespace Jookli.Payments.Application.Features.User.Command
+{
+    internal class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        public CreateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+        {
+            if(await _userRepository.ExistAsync(command.UserId, cancellationToken))
+            {
+                return Unit.Value;
+            }
+
+            var user = new UserEntity()
+            {
+                UserId = command.UserId,
+                FirstName= command.FirstName,
+                LastName= command.LastName,
+                Email= command.Email,
+            };
+
+            await _userRepository.AddAsync(user, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
