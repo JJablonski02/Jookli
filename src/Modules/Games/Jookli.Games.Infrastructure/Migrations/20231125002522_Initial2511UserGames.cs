0021814d262ac3bf57f0eb177608b37147682829
JJablonski02 <kuba2805@icloud.com>
2023-11-25 01:54:47 +0100

    two new migrations - ACTUAL

---
+++ src/Modules/Games/Jookli.Games.Infrastructure/Migrations/20231125002522_Initial2511UserGames.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jookli.Games.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2511UserGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games_InboxMessage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OccuredOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_InboxMessage", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games_InternalCommands",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Error = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EnqueueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_InternalCommands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games_OutboxMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OccuredOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games_Profile",
+                columns: table => new
+                {
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_Profile", x => x.ProfileId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games_User",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games_Game",
+                columns: table => new
+                {
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Counter = table.Column<int>(type: "int", nullable: false),
+                    GameOwner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileEntityProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games_Game", x => x.GameId);
+                    table.ForeignKey(
+                        name: "FK_Games_Game_Games_Profile_ProfileEntityProfileId",
+                        column: x => x.ProfileEntityProfileId,
+                        principalTable: "Games_Profile",
+                        principalColumn: "ProfileId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Game_ProfileEntityProfileId",
+                table: "Games_Game",
+                column: "ProfileEntityProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_InboxMessage_Id",
+                table: "Games_InboxMessage",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_User_UserId",
+                table: "Games_User",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games_Game");
+
+            migrationBuilder.DropTable(
+                name: "Games_InboxMessage");
+
+            migrationBuilder.DropTable(
+                name: "Games_InternalCommands");
+
+            migrationBuilder.DropTable(
+                name: "Games_OutboxMessages");
+
+            migrationBuilder.DropTable(
+                name: "Games_User");
+
+            migrationBuilder.DropTable(
+                name: "Games_Profile");
+        }
+    }
+}
