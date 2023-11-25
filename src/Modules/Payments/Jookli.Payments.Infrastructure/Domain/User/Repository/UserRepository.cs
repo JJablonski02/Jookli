7610293c55e808c127fb4cfefeb0dc1553b41d9a
JJablonski02 <kuba2805@icloud.com>
2023-11-25 01:53:32 +0100

    userRepository Exists query

--- src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/User/Repository/UserRepository.cs
+++ src/Modules/Payments/Jookli.Payments.Infrastructure/Domain/User/Repository/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Jookli.Payments.Domain.Entities.User;
 using Jookli.Payments.Domain.Entities.User.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,11 @@ namespace Jookli.Payments.Infrastructure.Domain.User.Repository
         public async Task AddAsync(UserEntity userEntity, CancellationToken cancellationToken = default)
         {
             await _paymentsContext.User.AddAsync(userEntity, cancellationToken);
+        }
+
+        public async Task<bool> ExistAsync(Guid userId, CancellationToken cancellationToken = default)
+        {
+            return await _paymentsContext.User.AnyAsync(x => x.UserId == userId, cancellationToken);
         }
     }
 }
