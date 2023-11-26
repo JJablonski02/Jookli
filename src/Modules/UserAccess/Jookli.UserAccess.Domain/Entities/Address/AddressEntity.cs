87722c629cee3bbeb5564b89f9cdc730043b8cea
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:20:42 +0100

    address entity

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Address/AddressEntity.cs
@@ -0,0 +1,22 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Address
+{
+    public class AddressEntity : Entity
+    {
+        public Guid AddressId { get; set; }
+        public UserEntity User { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
