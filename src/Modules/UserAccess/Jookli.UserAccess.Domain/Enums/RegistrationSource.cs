1a08a42e693842829b1ff5262881380e9701fa21
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:19:33 +0100

    registration source enum

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Enums/RegistrationSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Enums
+{
+    public enum RegistrationSource
+    {
+        Internal = 0,
+        Facebook = 1,
+        Apple = 2,
+        Google = 3,
+    }
+}
