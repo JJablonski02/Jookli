badfcb8311ee18e1919e1c48e6709ea3ed446b45
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:19:21 +0100

    account status enum

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Enums/AccountStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Enums
+{
+    public enum AccountStatus
+    {
+        NotConfirmed = 0,
+        Confirmed = 1,
+        WaitingForConfirmation = 2,
+        ConfirmationExpired = 3,
+    }
+}
