6cbbb2e659d6cd63f7b39d5778f3290bbc98d19c
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:19:57 +0100

    location entity

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Location/LocationEntity.cs
@@ -0,0 +1,20 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Location
+{
+    public class LocationEntity : Entity
+    {
+        public Guid LocationId { get; set; }
+        public UserEntity User { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string LocationName { get; set; }
+    }
+}
