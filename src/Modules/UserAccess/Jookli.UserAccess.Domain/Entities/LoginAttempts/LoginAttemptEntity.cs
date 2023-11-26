23a2badd67b35bb53fe19f348e0127c653586406
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:19:47 +0100

    login attempt entity

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/LoginAttempts/LoginAttemptEntity.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.LoginAttempts
+{
+    public class LoginAttemptEntity : Entity
+    {
+        public Guid LoginAttemptId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime SuccessfullAuthorizationDate { get; set; }
+        public DateTime BadAuthorizationDate { get; set; }
+        public string IpAddress { get; set; }
+        public UserEntity User { get; set; }
+    }
+}
