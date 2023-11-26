e5135b1885ce8df993ad2eaa94a55a532629abd2
JJablonski02 <kuba2805@icloud.com>
2023-11-26 22:18:49 +0100

    User details entity

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/UserDetails/UserDetailsEntity.cs
@@ -0,0 +1,30 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.UserDetails
+{
+    public class UserDetailsEntity : Entity
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AreaCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public Gender Gender { get; set; } 
+        public Gender BaseInfoGender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string BaseInfoCountry { get; set; }
+        public string Education { get; set; }
+        public string Specialization { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public string Legacy { get; set; }
+        public string PoliticalViews { get; set; }
+        public string CurrentRelationShip { get; set; }
+        public string Interesets { get; set; }
+    }
+}
