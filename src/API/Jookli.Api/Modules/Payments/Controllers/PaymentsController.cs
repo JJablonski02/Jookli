1bf2ca4331910f23d72e3c952698615a2d67914f
JJablonski02 <kuba2805@icloud.com>
2023-11-27 23:54:56 +0100

    payments controller created

---
+++ src/API/Jookli.Api/Modules/Payments/Controllers/PaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.Payments.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.Payments.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController
+    {
+        private readonly IPaymentsModule _paymentsModule;
+
+        public PaymentsController(IPaymentsModule paymentsModule)
+        {
+            _paymentsModule = paymentsModule;
+        }
+    }
+}
