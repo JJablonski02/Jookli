842aa68a3f72c48b7161a9534eb08c1874a20774
JJablonski02 <kuba2805@icloud.com>
2023-11-28 00:11:21 +0100

    location configuration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/Location/Config/LocationEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.UserAccess.Domain.Entities.Location;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.Location.Config
+{
+    internal sealed class LocationEntityTypeConfiguration : IEntityTypeConfiguration<LocationEntity>
+    {
+        public void Configure(EntityTypeBuilder<LocationEntity> builder)
+        {
+            builder.ToTable("UserAccess_Location");
+
+            builder.HasKey(x => x.LocationId);
+
+            builder.HasOne(x => x.User)
+                .WithOne(user => user.Location)
+                .HasForeignKey<LocationEntity>(e => e.UserId);
+        }
+    }
+}
