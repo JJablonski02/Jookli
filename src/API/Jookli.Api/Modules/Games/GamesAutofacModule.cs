23254a7d16857d2a25d736a853b3035fc0a06a88
JJablonski02 <kuba2805@icloud.com>
2023-11-27 23:54:46 +0100

    games autofac module registered

---
+++ src/API/Jookli.Api/Modules/Games/GamesAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Jookli.Games.Application.Contracts;
+using Jookli.Games.Infrastructure;
+
+namespace Jookli.Api.Modules.Games
+{
+    public class GamesAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<GamesModule>()
+                .As<IGamesModule>().
+                InstancePerLifetimeScope();
+        }
+    }
+}
