1b65f399096fcad181fea781e31883b5efc6a5d7
JJablonski02 <kuba2805@icloud.com>
2023-11-27 23:55:07 +0100

    payments autofac module created

---
+++ src/API/Jookli.Api/Modules/Payments/PaymentsAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Jookli.Payments.Application.Contracts;
+using Jookli.Payments.Infrastructure;
+
+namespace Jookli.Api.Modules.Payments
+{
+    public class PaymentsAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PaymentsModule>()
+                .As<IPaymentsModule>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
