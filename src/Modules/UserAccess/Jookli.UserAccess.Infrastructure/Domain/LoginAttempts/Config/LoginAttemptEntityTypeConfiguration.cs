9962f05e554c1b7767b2f429b01c6e29cf055199
JJablonski02 <kuba2805@icloud.com>
2023-11-28 00:11:44 +0100

    Login attempts configuration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/LoginAttempts/Config/LoginAttemptEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.UserAccess.Domain.Entities.LoginAttempts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.LoginAttempts.Config
+{
+    internal sealed class LoginAttemptEntityTypeConfiguration : IEntityTypeConfiguration<LoginAttemptEntity>
+    {
+        public void Configure(EntityTypeBuilder<LoginAttemptEntity> builder)
+        {
+            builder.ToTable("UserAccess_LoginAttempts");
+
+            builder.HasKey(t => t.LoginAttemptId);
+
+            builder.HasOne(user => user.User)
+                .WithMany(o => o.LoginAttempts)
+                .HasForeignKey(x => x.UserId);
+        }
+    }
+}
