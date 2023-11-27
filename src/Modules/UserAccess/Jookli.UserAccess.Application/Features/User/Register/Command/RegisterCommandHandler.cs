3c5203abcbb4698f84ee78e0a2f4215e4a3dfdd1
JJablonski02 <kuba2805@icloud.com>
2023-11-28 00:14:10 +0100

    New logic of registration new user. Implemented new entites.

--- src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommandHandler.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Command/RegisterCommandHandler.cs
@@ -1,8 +1,13 @@
 ﻿using Jookli.UserAccess.Application.Authentication;
 using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.Address;
+using Jookli.UserAccess.Domain.Entities.Location;
+using Jookli.UserAccess.Domain.Entities.LoginAttempts;
 using Jookli.UserAccess.Domain.Entities.User;
 using Jookli.UserAccess.Domain.Entities.User.Events;
 using Jookli.UserAccess.Domain.Entities.User.RepositoryContract;
+using Jookli.UserAccess.Domain.Entities.UserDetails;
+using Jookli.UserAccess.Domain.Enums;
 using MediatR;
 
 namespace Jookli.UserAccess.Application.Features.User.Register.Command
@@ -18,7 +23,6 @@ namespace Jookli.UserAccess.Application.Features.User.Register.Command
 
         public async Task<Unit> Handle(RegisterCommand command, CancellationToken cancellationToken)
         {
-            var password = PasswordManager.HashPassword(command.Password);
             var userEmail = await _userRepository.GetByUserEmailAsync(command.Email, cancellationToken);
 
             if (userEmail is not null)
@@ -26,21 +30,42 @@ namespace Jookli.UserAccess.Application.Features.User.Register.Command
                 throw new ArgumentException($"User with email: {command.Email} exists");
             }
 
+            if(command.Password != command.ConfirmPassword)
+            {
+                throw new ArgumentException("Password and Confirm Password do not match");
+            }
+
+            var password = PasswordManager.HashPassword(command.Password);
+
             var user = new UserEntity()
             {
-                UserID = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
                 Email = command.Email,
-                Password = command.Password,
-                FirstName = command.FirstName,
-                LastName = command.LastName,
-                Gender = command.Gender,
-                CreationDate = command.CreationDate,
-                DateOfLastActivity = DateTime.UtcNow
+                Password = password,
+                CreationDate = DateTime.UtcNow,
+                AccountStatus = Domain.Enums.AccountStatus.WaitingForConfirmation,
+                RegistrationSource = Domain.Enums.RegistrationSource.Internal,
+                PushNotifications = command.PushNotifications,
+                IsDeleted = false,
+                IsAccountBlocked = false,
+                UserDetails = new UserDetailsEntity()
+                {
+                    UserDetailsId = Guid.NewGuid(),
+                    FirstName = command.FirstName,
+                    LastName = command.LastName,
+                    Gender = command.Gender,
+                },
+                DateOfLastActivity = DateTime.Now,
+                DateOfLastActivityUtc= DateTime.UtcNow
             };
 
-            user.AddDomainEvent(new NewUserRegisteredDomainEvent(user.UserID, user.Email, user.Password, user.FirstName, user.LastName, user.Gender, user.CreationDate, user.DateOfLastActivity));
+            user.AddDomainEvent(
+                new NewUserRegisteredDomainEvent(
+                    user.UserId, user.Email, user.Password, user.CreationDate, user.AccountStatus, user.RegistrationSource,
+                    user.PushNotifications, user.IsDeleted, user.IsAccountBlocked, user.UserDetails.UserId, user.UserDetails.FirstName,
+                    user.UserDetails.LastName, user.UserDetails.Gender, user.DateOfLastActivity, user.DateOfLastActivityUtc));
 
-            await _userRepository.AddUserAsync(user, cancellationToken);
+            await _userRepository.AddAsync(user, cancellationToken);
 
             return Unit.Value;
         }
