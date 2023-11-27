be22c22bafa8454a183a3db42b03ec4fdd7444ad
JJablonski02 <kuba2805@icloud.com>
2023-11-28 00:12:06 +0100

    user entity details configuration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/UserDetails/Config/UserDetailsEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.UserAccess.Domain.Entities.UserDetails;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.UserDetails.Config
+{
+    internal sealed class UserDetailsEntityTypeConfiguration : IEntityTypeConfiguration<UserDetailsEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserDetailsEntity> builder)
+        {
+            builder.ToTable("UserAccess_UserDetails");
+
+            builder.HasKey(x => x.UserDetailsId);
+            builder.HasOne(x => x.User)
+                .WithOne(x => x.UserDetails)
+                .HasForeignKey<UserDetailsEntity>(t => t.UserId);
+        }
+    }
+}
