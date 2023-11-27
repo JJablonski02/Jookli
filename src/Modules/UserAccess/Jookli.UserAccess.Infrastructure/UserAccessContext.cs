0f678ddc08ccee6cc607eeb08e2fc34dab5225ae
JJablonski02 <kuba2805@icloud.com>
2023-11-28 00:10:54 +0100

    User access context new entites added

--- src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/UserAccessContext.cs
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/UserAccessContext.cs
@@ -6,12 +6,22 @@ using Jookli.BuildingBlocks.Application.Outbox;
 using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
 using System.Reflection;
 using Jookli.BuildingBlocks.Infrastructure.Inbox;
+using Jookli.UserAccess.Domain.Entities.Address;
+using Jookli.UserAccess.Domain.Entities.Location;
+using Jookli.UserAccess.Domain.Entities.UserDetails;
+using Jookli.UserAccess.Domain.Entities.LoginAttempts;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure.Internal;
 
 namespace Jookli.UserAccess.Infrastructure
 {
     public class UserAccessContext : DbContext
     {
         public DbSet<UserEntity> User { get; set; }
+        public DbSet<UserDetailsEntity> UserDetails { get; set; }
+        public DbSet<AddressEntity> Address { get; set; }
+        public DbSet<LocationEntity> Location { get; set; }
+        public DbSet<LocationServicesEntity> LocationServices { get; set; }
+        public DbSet<LoginAttemptEntity> LoginAttempts { get; set; }
         public DbSet<OutboxMessage> OutboxMessages { get; set; }
         public DbSet<InternalCommand> InternalCommands { get; set; }
         public DbSet<InboxMessage> InboxMessage { get; set; }
@@ -24,7 +34,6 @@ namespace Jookli.UserAccess.Infrastructure
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasDefaultSchema("UserAccess");
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(modelBuilder);
         }
