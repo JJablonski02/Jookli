753a99123b94266340f7b8db83f11f6b1264f739
JJablonski02 <kuba2805@icloud.com>
2023-11-30 23:19:20 +0100

    I user details repository

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/UserDetails/Repository/IUserDetailsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.UserDetails.Repository
+{
+    public interface IUserDetailsRepository
+    {
+        Task AddAsync(UserDetailsEntity userDetailsEntity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(UserDetailsEntity userDetailsEntity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid userDetailsId, CancellationToken cancellationToken = default);
+        Task<UserDetailsEntity> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    }
+}
