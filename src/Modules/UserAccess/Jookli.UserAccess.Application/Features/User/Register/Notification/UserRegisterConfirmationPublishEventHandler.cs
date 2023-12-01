d9db341322a1c6a7b6f16c609b04a06e6a8c3c86
JJablonski02 <kuba2805@icloud.com>
2023-12-01 23:39:36 +0100

    user register confirmation notification published

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Notification/UserRegisterConfirmationPublishEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Register.Notification
+{
+    public class UserRegisterConfirmationPublishEventHandler : INotificationHandler<UserRegisterConfirmationNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public UserRegisterConfirmationPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(UserRegisterConfirmationNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new SendEmailConfirmationIntegrationEvent(notification.Id, notification.DomainEvent.OccuredOn));
+        }
+    }
+}
