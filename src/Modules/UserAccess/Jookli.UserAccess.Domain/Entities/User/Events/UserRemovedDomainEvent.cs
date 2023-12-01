95dcf196161fc8934d26107786206ca214164b46
JJablonski02 <kuba2805@icloud.com>
2023-12-01 22:57:48 +0100

    domain event

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/User/Events/UserRemovedDomainEvent.cs
@@ -0,0 +1,19 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.User.Events
+{
+    public class UserRemovedDomainEvent : DomainEventBase
+    {
+        public UserRemovedDomainEvent(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public Guid UserId { get; }
+    }
+}
