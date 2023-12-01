a3d5a8f3978bfef6eae24407fc9ae1656335af30
JJablonski02 <kuba2805@icloud.com>
2023-12-01 22:57:30 +0100

    removed notification publisher

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Remove/Notification/UserRemovedPublishEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Remove.Notification
+{
+    public class UserRemovedPublishEventHandler : INotificationHandler<UserRemovedDomainEvent>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public UserRemovedPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(UserRemovedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new UserRemovedIntegrationEvent(
+                notification.Id,
+                notification.OccuredOn,
+                notification.UserId));
+        }
+    }
+}
