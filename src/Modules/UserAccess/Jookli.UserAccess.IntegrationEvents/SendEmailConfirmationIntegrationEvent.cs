fd21970dae419b3fa243001604e580164abcb61e
JJablonski02 <kuba2805@icloud.com>
2023-12-01 23:39:02 +0100

    send email confirmation integration event

---
+++ src/Modules/UserAccess/Jookli.UserAccess.IntegrationEvents/SendEmailConfirmationIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.IntegrationEvents
+{
+    public class SendEmailConfirmationIntegrationEvent : IntegrationEvent
+    {
+        public SendEmailConfirmationIntegrationEvent(Guid id, DateTime occuredOn) : base(id, occuredOn)
+        {
+        }
+    }
+}
