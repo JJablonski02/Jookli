173d3e8d421c7897a1116b162eda28c19d530d3a
JJablonski02 <kuba2805@icloud.com>
2023-12-01 22:57:42 +0100

    removed integration event

---
+++ src/Modules/UserAccess/Jookli.UserAccess.IntegrationEvents/UserRemovedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.IntegrationEvents
+{
+    public class UserRemovedIntegrationEvent : IntegrationEvent
+    {
+        public Guid UserId { get; }
+        public UserRemovedIntegrationEvent(Guid id, DateTime occuredOn, Guid userId) : base(id, occuredOn)
+        {
+            UserId = userId;
+        }
+    }
+}
