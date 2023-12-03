274b1e444b9f081b07afedefbc5b8967f1aa5ede
JJablonski02 <kuba2805@icloud.com>
2023-12-03 23:03:43 +0100

    tapjoy controller

---
+++ src/API/Jookli.Api/Modules/Games/Controllers/TapjoyController.cs
@@ -0,0 +1,32 @@
+﻿using Jookli.Api.Configuration.Authorization;
+using Jookli.Games.Application.Contracts;
+using Jookli.Games.Application.Features.Games.Tapjoy.Read;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.Games.Controllers
+{
+    [Route("api/games/tapjoy")]
+    [ApiController]
+    public class TapjoyController : ControllerBase
+    {
+        private readonly IGamesModule _gamesModule;
+
+        public TapjoyController(IGamesModule gamesModule)
+        {
+            _gamesModule = gamesModule;
+        }
+
+        [NoPermissionRequired]
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetDetails")]
+
+        public async Task<IActionResult> GetDetails(ReadUserDetailsCommand readUserDetailsCommand)
+        {
+           var result = await _gamesModule.ExecuteQueryAsync(readUserDetailsCommand);
+
+            return Ok();
+        }
+    }
+}
