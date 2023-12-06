dede08de0203cf759742f495d9d3cdd9461a7e74
JJablonski02 <kuba2805@icloud.com>
2023-12-06 21:04:45 +0100

    user device information added to entity

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/UsersDevice/Config/UsersDeviceEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.UserAccess.Domain.Entities.UsersDevice;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.UsersDevice.Config
+{
+    public class UsersDeviceEntityTypeConfiguration : IEntityTypeConfiguration<UsersDeviceEntity>
+    {
+        public void Configure(EntityTypeBuilder<UsersDeviceEntity> builder)
+        {
+            builder.ToTable("UserAccess_UsersDevice");
+
+            builder.HasKey(x => x.UsersDeviceId);
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.UsersDevice)
+                .HasForeignKey(x => x.UserId);
+        }
+    }
+}
