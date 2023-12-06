3b568157780d3298b8f022f8829a5c93d0374a1a
JJablonski02 <kuba2805@icloud.com>
2023-12-06 21:03:39 +0100

    token created domain event

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Token/Events/TokenCreatedDomainEvent.cs
@@ -0,0 +1,21 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Token.Events
+{
+    public class TokenCreatedDomainEvent : DomainEventBase
+    {
+        public TokenCreatedDomainEvent(Guid userId, TokenPurpose tokenPurpose)
+        {
+            UserId = userId;
+            TokenPurpose = tokenPurpose;
+        }
+        public Guid UserId { get; set; }
+        public TokenPurpose TokenPurpose { get; set; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Token/Repository/ITokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Token.Repository
+{
+    public interface ITokenRepository
+    {
+        Task AddAsync(TokenEntity tokenEntity, CancellationToken cancellationToken);
+    }
+}
