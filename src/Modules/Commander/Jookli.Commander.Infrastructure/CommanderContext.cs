bfdcd09a1d6d0105cdaf6dc67890cbd570f8d1ba
JJablonski02 <kuba2805@icloud.com>
2023-12-06 22:22:43 +0100

    context and module

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/CommanderContext.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Jookli.BuildingBlocks.Infrastructure.Inbox;
+using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Jookli.Commander.Domain.Entites.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Jookli.Commander.Infrastructure
+{
+    public class CommanderContext : DbContext
+    {
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<InternalCommand> InternalCommands { get; set; }
+        public DbSet<InboxMessage> InboxMessage { get; set; }
+        public DbSet<UserEntity> Users {get; set;}       
+
+        private readonly ILoggerFactory _logger;
+        public CommanderContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        } 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/CommanderModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Jookli.Commander.Application.Contracts;
+using Jookli.Commander.Infrastructure.Configuration;
+using Jookli.Commander.Infrastructure.Configuration.Processing;
+using MediatR;
+
+namespace Jookli.Commander.Infrastructure
+{
+    public class CommanderModule : ICommanderModule
+    {
+        public async Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command)
+        {
+            return await CommandsExecutor.Execute(command);
+        }
+
+        public async Task ExecuteCommandAsync(ICommand command)
+        {
+            await CommandsExecutor.Execute(command);
+        }
+
+        public async Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
+        {
+            using (var scope = CommanderCompositionRoot.BeginLifetimeScope())
+            {
+                var mediator = scope.Resolve<IMediator>();
+                return await mediator.Send(query);
+            }
+        }
+    }
+}
