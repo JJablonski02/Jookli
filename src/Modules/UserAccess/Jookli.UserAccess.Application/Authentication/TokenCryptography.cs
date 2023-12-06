226557e8a565dca80b0d974577147645d5e307b9
JJablonski02 <kuba2805@icloud.com>
2023-12-06 21:00:27 +0100

    Token cryptography sha256

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Authentication/TokenCryptography.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Authentication
+{
+    public static class TokenCryptography
+    {
+        public static string sha256(string input)
+        {
+            var crypt = SHA256.Create();
+
+            byte[] inputByes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = crypt.ComputeHash(inputByes);
+            var result = hashBytes.ToString();
+
+            return result;
+        }
+    }
+}
