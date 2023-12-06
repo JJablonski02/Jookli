a975ec5670a3c0ae41f2d0266fa857d6e3ab18a6
JJablonski02 <kuba2805@icloud.com>
2023-12-06 14:07:50 +0100

    UsersDevice entity added - contains information about users device

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/UsersDevice/UsersDeviceEntity.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.UsersDevice
+{
+    public class UsersDeviceEntity : Entity
+    {
+        public Guid UsersDeviceId { get; set; }
+        public Guid UserId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string OS { get; set; }
+        public string OsVersion { get; set; }
+        public string IpAddress { get; set; }
+        public string Platform { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public UserEntity User { get; set; }
+    }
+}
