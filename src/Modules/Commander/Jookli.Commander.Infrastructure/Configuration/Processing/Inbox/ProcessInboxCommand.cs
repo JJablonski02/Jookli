a3b091c5636b598f11c81256797302d1ca707fc0
JJablonski02 <kuba2805@icloud.com>
2023-12-06 22:20:06 +0100

    Commander inbox

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Inbox/ProcessInboxCommand.cs
@@ -0,0 +1,9 @@
+﻿using Jookli.Commander.Application.Contracts;
+
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Inbox
+{
+    public class ProcessInboxCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Inbox/ProcessInboxJob.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Inbox
+{
+    [DisallowConcurrentExecution]
+    public class ProcessInboxJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await CommandsExecutor.Execute(new ProcessInboxCommand());
+        }
+    }
+}
