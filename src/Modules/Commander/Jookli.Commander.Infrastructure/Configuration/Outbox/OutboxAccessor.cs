1622ab88a868edaa6257a88be188b9b61b220678
JJablonski02 <kuba2805@icloud.com>
2023-12-06 22:22:00 +0100

    outbox

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Outbox/OutboxAccessor.cs
@@ -0,0 +1,23 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Outbox
+{
+    public class OutboxAccessor : IOutbox
+    {
+        private readonly CommanderContext _gamesContext;
+        public OutboxAccessor(CommanderContext userAccessContext)
+        {
+            _gamesContext = userAccessContext;
+        }
+
+        public void Add(OutboxMessage message)
+        {
+            _gamesContext.OutboxMessages.Add(message);
+        }
+
+        public Task Save()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
