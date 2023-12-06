627fcd0db39f13981dfe1e184a1d31991101be3d
JJablonski02 <kuba2805@icloud.com>
2023-12-06 21:04:17 +0100

    enum token purpose

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Enums/TokenPurpose.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Enums
+{
+    public enum TokenPurpose
+    {
+        ConfirmAccount = 0,
+        RemindPassword = 1,
+    }
+}
