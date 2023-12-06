7d55a8307eb61bcbe81c62bc6837c053828bab0a
JJablonski02 <kuba2805@icloud.com>
2023-12-06 20:58:44 +0100

    Token entity type configuration

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Domain/Token/Config/TokenEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.UserAccess.Domain.Entities.Token;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Infrastructure.Domain.Token.Config
+{
+    internal sealed class TokenEntityTypeConfiguration : IEntityTypeConfiguration<TokenEntity>
+    {
+        public void Configure(EntityTypeBuilder<TokenEntity> builder)
+        {
+            builder.ToTable("UserAccess_Tokens");
+
+            builder.HasKey(x => x.TokenId);
+
+            builder.HasOne(x => x.User)
+                .WithMany(y => y.Token)
+                .HasForeignKey(x => x.UserId);
+        }
+    }
+}
