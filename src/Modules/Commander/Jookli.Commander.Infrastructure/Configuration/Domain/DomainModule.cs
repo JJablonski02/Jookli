a65a7c83307eb191f41c4d1c3c8b6283b333a2a4
JJablonski02 <kuba2805@icloud.com>
2023-12-06 22:21:37 +0100

    data access

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Domain/DomainModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Domain
+{
+    internal class DomainModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
