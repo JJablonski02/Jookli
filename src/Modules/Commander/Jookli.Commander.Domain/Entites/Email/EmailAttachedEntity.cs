5beca1d98a389b3e3d527f0c330325bc5a5f4cac
JJablonski02 <kuba2805@icloud.com>
2023-12-08 22:49:04 +0100

    Email entity

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/Email/EmailAttachedEntity.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.Email
+{
+    public class EmailAttachedEntity : Entity
+    {
+        public Guid EmailAttachedId { get; set; }
+        public Guid EmailEntityId { get; set; }
+        public EmailEntity Email { get; set; }
+        public string FilePath { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/Email/EmailEntity.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.Commander.Domain.Entites.EmailAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.Email
+{
+    public class EmailEntity : Entity
+    {
+        public Guid EmailId { get; set; }
+        public Guid EmailAccountId { get; set; }
+        public EmailAccountEntity EmailAccount { get; set; }
+        public string? EmailName { get; set; }
+        public string? Subject { get; set; }
+        public string? Content { get; set; }
+        public string? Recipement { get; set; }
+        public string? Error { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+        public ICollection<EmailAttachedEntity> EmailAttacheds { get; set; }
+    }
+}
