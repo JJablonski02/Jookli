38b9fee6a95a03e3fef496b30be00fa429da375d
JJablonski02 <kuba2805@icloud.com>
2023-12-08 22:48:55 +0100

    confirmation event handler

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/SendConfirmation/Incoming/SendEmailConfirmationEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using Jookli.Commander.Application.Features.Email.SendConfirmation.Command;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.Email.SendConfirmation.Incoming
+{
+    internal class SendEmailConfirmationEventHandler : INotificationHandler<SendEmailConfirmationIntegrationEvent>
+    {
+        private readonly ICommandsScheduler _commandsScheulder;
+
+        public SendEmailConfirmationEventHandler(ICommandsScheduler commandsScheduler)
+        {
+            _commandsScheulder = commandsScheduler;
+        }
+        public async Task Handle(SendEmailConfirmationIntegrationEvent notification, CancellationToken cancellationToken)
+        {
+            await _commandsScheulder.EnqueueAsync(new SendConfirmationCommand(
+                notification.Id,
+                notification.UserId,
+                notification.Email,
+                notification.CallbackUrl
+                ));
+        }
+    }
+}
