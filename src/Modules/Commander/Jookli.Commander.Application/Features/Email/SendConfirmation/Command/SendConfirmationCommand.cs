11b64c26129babbf61a6c933a703079eb2af62f6
JJablonski02 <kuba2805@icloud.com>
2023-12-08 22:48:44 +0100

    Send confirmation command

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/SendConfirmation/Command/SendConfirmationCommand.cs
@@ -0,0 +1,23 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Jookli.Commander.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.Email.SendConfirmation.Command
+{
+    internal class SendConfirmationCommand : InternalCommandBase
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }   
+        public string CallbackUrl { get; set; }
+        public SendConfirmationCommand(Guid id, Guid userId, string email, string callbackUrl) : base(id)
+        {
+            UserId = userId;
+            Email = email;
+            CallbackUrl = callbackUrl;
+        }
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/SendConfirmation/Command/SendConfirmationCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿
+using Jookli.Commander.Application.Configuration.Command;
+using MediatR;
+
+namespace Jookli.Commander.Application.Features.Email.SendConfirmation.Command
+{
+    internal class SendConfirmationCommandHandler : ICommandHandler<SendConfirmationCommand>
+    {
+        public Task<Unit> Handle(SendConfirmationCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
