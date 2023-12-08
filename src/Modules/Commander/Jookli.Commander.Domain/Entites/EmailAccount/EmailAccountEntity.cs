6effe0b33ce6c18da83c5fa1c05f888a6c8ada21
JJablonski02 <kuba2805@icloud.com>
2023-12-08 22:49:24 +0100

    email account entity

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/EmailAccount/EmailAccountEntity.cs
@@ -0,0 +1,30 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.Commander.Domain.Entites.EmailTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.EmailAccount
+{
+    public class EmailAccountEntity : Entity
+    {
+        public Guid EmailAccountId { get; set; }
+        public string? Name { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Pop3Server { get; set; }
+        public string? Pop3Port { get; set; }
+        public string? Pop3Login { get; set; }
+        public string? Pop3Password { get; set; }
+        public string? SmtpServer { get; set; }
+        public int? SmtpPort { get; set; }
+        public string? SmtpLogin { get; set; }
+        public string? SmtpPassword { get; set; }
+        public string? ImapServer { get; set; }
+        public int? ImapPort { get; set; }
+        public string? ImapLogin { get; set; }
+        public string? ImapPassword { get; set; }
+        public ICollection<EmailTemplateEntity> EmailTemplates { get; set; }
+    }
+}
