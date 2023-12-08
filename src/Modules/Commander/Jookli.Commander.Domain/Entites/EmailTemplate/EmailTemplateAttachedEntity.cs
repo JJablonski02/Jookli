8aea4e8b8ee72373d92a2823d452a77000fc1bf2
JJablonski02 <kuba2805@icloud.com>
2023-12-08 22:49:36 +0100

    email template entity

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/EmailTemplate/EmailTemplateAttachedEntity.cs
@@ -0,0 +1,12 @@
+﻿using Jookli.BuildingBlocks.Domain;
+
+namespace Jookli.Commander.Domain.Entites.EmailTemplate
+{
+    public class EmailTemplateAttachedEntity : Entity
+    {
+        public Guid EmailTemplateAttachedId { get; set; }
+        public Guid EmailTemplateId { get; set; }
+        public EmailTemplateEntity EmailTemplate { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/EmailTemplate/EmailTemplateEntity.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.Commander.Domain.Entites.EmailAccount;
+using Jookli.Commander.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.EmailTemplate
+{
+    public class EmailTemplateEntity
+    {
+        public Guid EmailTemplateId { get; set; }
+        public Guid EmailAccountId { get; set; }
+        public EmailAccountEntity EmailAccount { get; set; }
+        public string EmailName { get; set; }
+        public string Content { get; set; }
+        public string Subject { get; set; }
+        public string SenderName { get; set; }
+        public bool Active { get; set; }
+        public EmailTemplateType EmailTemplate { get; set; }
+        public ICollection<EmailTemplateAttachedEntity> EmailTemplateAttacheds { get; set; }
+    }
+}
