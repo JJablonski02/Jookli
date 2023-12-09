7ef81b630aafc091fc6b4591cc6d9481e80cb8cc
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:20:56 +0100

    Process email command

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Emails/ProcessEmailCommand.cs
@@ -0,0 +1,13 @@
+﻿using Jookli.Commander.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Emails
+{
+    public class ProcessEmailCommand : CommandBase, IRecurringCommand
+    {
+    }
+}
