423beb075203a755245de7543ef1b601a74bb383
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:20:37 +0100

    Process Email Job

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Emails/ProcessEmailJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Emails
+{
+    [DisallowConcurrentExecution]
+    public class ProcessEmailJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await CommandsExecutor.Execute(new ProcessEmailCommand());
+        }
+    }
+}
