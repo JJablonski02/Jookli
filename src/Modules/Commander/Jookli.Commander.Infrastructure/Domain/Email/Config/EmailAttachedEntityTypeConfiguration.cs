ef277e08207f1f84975080401df3bd6c86fbfca1
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:21:57 +0100

    Email attached entity type configuration

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Domain/Email/Config/EmailAttachedEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.Commander.Domain.Entites.Email;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Domain.Email.Config
+{
+    internal sealed class EmailAttachedEntityTypeConfiguration : IEntityTypeConfiguration<EmailAttachedEntity>
+    {
+        public void Configure(EntityTypeBuilder<EmailAttachedEntity> builder)
+        {
+            builder.ToTable("Commander_EmailAttached");
+            builder.HasKey(e => e.EmailAttachedId);
+            builder.HasIndex(e => e.EmailAttachedId);
+
+            builder.HasOne(e => e.Email)
+                .WithMany(e => e.EmailAttacheds)
+                .HasForeignKey(e => e.EmailEntityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
