7f630854fac911438a236888d54e9f261dd0c00d
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:23:13 +0100

    send email confirmation publish event handler

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Notification/SendEmailConfirmationPublishEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.Register.Notification
+{
+    public class SendEmailConfirmationPublishEventHandler : INotificationHandler<SendEmailConfirmationNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public SendEmailConfirmationPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(SendEmailConfirmationNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new SendEmailConfirmationIntegrationEvent(
+                notification.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.UserId,
+                notification.DomainEvent.Email,
+                notification.DomainEvent.Url));
+        }
+    }
+}
