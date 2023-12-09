0021614548af3d85e44251ac515efc7db9757acf
JJablonski02 <kuba2805@icloud.com>
2023-12-09 23:06:50 +0100

    Email template repository

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/EmailTemplate/Repository/IEmailTemplateRepository.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Commander.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.EmailTemplate.Repository
+{
+    public interface IEmailTemplateRepository
+    {
+        Task AddAsync(EmailTemplateEntity emailTemplate, CancellationToken cancellationToken = default);
+        Task<EmailTemplateEntity?> GetFullyTemplateByTemplateTypeAsync(EmailTemplateType emailTemplateType, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Domain/EmailTemplate/EmailTemplateRepository.cs
@@ -0,0 +1,32 @@
+﻿using Jookli.Commander.Domain.Entites.EmailTemplate;
+using Jookli.Commander.Domain.Entites.EmailTemplate.Repository;
+using Jookli.Commander.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Domain.EmailTemplate
+{
+    public class EmailTemplateRepository : IEmailTemplateRepository
+    {
+        private readonly CommanderContext _commanderContext;
+
+        public EmailTemplateRepository(CommanderContext commanderContext)
+        {
+            _commanderContext = commanderContext;
+        }
+
+        public async Task AddAsync(EmailTemplateEntity emailTemplate, CancellationToken cancellationToken)
+        {
+            await _commanderContext.EmailTemplate.AddAsync(emailTemplate, cancellationToken);
+        }
+
+        public async Task<EmailTemplateEntity?> GetFullyTemplateByTemplateTypeAsync(EmailTemplateType emailTemplateType, CancellationToken cancellationToken)
+        {
+            return await _commanderContext.EmailTemplate.Where(x => x.EmailTemplate == emailTemplateType).Include(x => x.EmailTemplateAttacheds).FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
