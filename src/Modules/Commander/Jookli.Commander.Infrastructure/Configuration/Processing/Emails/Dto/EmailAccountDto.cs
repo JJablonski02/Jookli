605eecae84c855537dc4ae2461581c3657f3bfb8
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:21:03 +0100

    Email account dto

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Emails/Dto/EmailAccountDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Emails.Dto
+{
+    public class EmailAccountDto
+    {
+        public Guid EmailAccountId { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string SmtpLogin { get; set; }
+        public string SmtpPassword { get; set; }
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
