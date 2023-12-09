aa893337195a7ef82b9c1b028815105a50ef4655
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:20:24 +0100

    Email Template Type

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Enums/EmailTemplateType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Enums
+{
+    public enum EmailTemplateType
+    {
+        ConfirmAccount = 0,
+        RecoverPassword = 1,
+        SendQuestion = 5,
+    }
+}
