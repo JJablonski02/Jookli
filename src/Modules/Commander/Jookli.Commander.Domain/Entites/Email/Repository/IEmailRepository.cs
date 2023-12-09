11e598a3234e013bf961a6ca6dd38f6197812b15
JJablonski02 <kuba2805@icloud.com>
2023-12-09 23:06:36 +0100

    Email repository

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/Email/Repository/IEmailRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.Email.Repository
+{
+    public interface IEmailRepository
+    {
+        Task AddAsync(EmailEntity email, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Domain/Email/EmailRepository.cs
@@ -0,0 +1,20 @@
+﻿using Jookli.Commander.Domain.Entites.Email;
+using Jookli.Commander.Domain.Entites.Email.Repository;
+
+namespace Jookli.Commander.Infrastructure.Domain.Email
+{
+    public class EmailRepository : IEmailRepository
+    {
+        public readonly CommanderContext _commanderContext;
+
+        public EmailRepository(CommanderContext commanderContext)
+        {
+            _commanderContext = commanderContext;
+        }
+
+        public async Task AddAsync(EmailEntity email, CancellationToken cancellationToken = default)
+        {
+            await _commanderContext.Email.AddAsync(email, cancellationToken);
+        }
+    }
+}
