7af4aa7eb53246684016b35bd980d7456a8d73a2
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:22:57 +0100

    Send email confirmation notification

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/Register/Notification/SendEmailConfirmationNotification.cs
@@ -0,0 +1,12 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+
+namespace Jookli.UserAccess.Application.Features.User.Register.Notification
+{
+    public class SendEmailConfirmationNotification : DomainNotificationBase<SendEmailConfirmationDomainEvent>
+    {
+        public SendEmailConfirmationNotification(SendEmailConfirmationDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
