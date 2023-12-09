e6442dd535ba9e48a0ebc583559e16a152b5087b
JJablonski02 <kuba2805@icloud.com>
2023-12-09 18:21:10 +0100

    email attached dto

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Emails/Dto/EmailAttachedDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Emails.Dto
+{
+    public class EmailAttachedDto
+    {
+        public Guid EmailAttachedId { get; set; }
+        public Guid EmailId { get; set; }
+        public string FilePath { get; set; }
+        public string? Name { get; set; }
+    }
+}
