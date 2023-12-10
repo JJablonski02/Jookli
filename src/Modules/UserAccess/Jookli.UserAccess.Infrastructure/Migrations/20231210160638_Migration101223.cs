b537ce9c489c0bf69e9e70879660aef4ab648476
JJablonski02 <kuba2805@icloud.com>
2023-12-10 22:00:40 +0100

    user access migraiton

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Infrastructure/Migrations/20231210160638_Migration101223.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jookli.UserAccess.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration101223 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAccess_LocationServices_UserAccess_Location_LocationId",
+                table: "UserAccess_LocationServices");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "UserAccess_UserDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "LocationId",
+                table: "UserAccess_LocationServices",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserAccess_LocationServices_LocationId",
+                table: "UserAccess_LocationServices",
+                newName: "IX_UserAccess_LocationServices_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialization",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PoliticalViews",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlaceOfResidence",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Legacy",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Interesets",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Education",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateOfBirth",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentRelationShip",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BaseInfoCountry",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserAccess_UserDetails",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "UserAccess_Tokens",
+                columns: table => new
+                {
+                    TokenId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TokenValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsValid = table.Column<bool>(type: "bit", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Metadata = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccess_Tokens", x => x.TokenId);
+                    table.ForeignKey(
+                        name: "FK_UserAccess_Tokens_UserAccess_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserAccess_User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAccess_UsersDevice",
+                columns: table => new
+                {
+                    UsersDeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OS = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OsVersion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccess_UsersDevice", x => x.UsersDeviceId);
+                    table.ForeignKey(
+                        name: "FK_UserAccess_UsersDevice_UserAccess_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserAccess_User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccess_Tokens_UserId",
+                table: "UserAccess_Tokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccess_UsersDevice_UserId",
+                table: "UserAccess_UsersDevice",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAccess_LocationServices_UserAccess_Location_UserId",
+                table: "UserAccess_LocationServices",
+                column: "UserId",
+                principalTable: "UserAccess_Location",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAccess_LocationServices_UserAccess_Location_UserId",
+                table: "UserAccess_LocationServices");
+
+            migrationBuilder.DropTable(
+                name: "UserAccess_Tokens");
+
+            migrationBuilder.DropTable(
+                name: "UserAccess_UsersDevice");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserAccess_UserDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "UserAccess_LocationServices",
+                newName: "LocationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserAccess_LocationServices_UserId",
+                table: "UserAccess_LocationServices",
+                newName: "IX_UserAccess_LocationServices_LocationId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialization",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PoliticalViews",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlaceOfResidence",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "UserAccess_UserDetails",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Legacy",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Interesets",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Education",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "UserAccess_UserDetails",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentRelationShip",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BaseInfoCountry",
+                table: "UserAccess_UserDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "UserAccess_UserDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAccess_LocationServices_UserAccess_Location_LocationId",
+                table: "UserAccess_LocationServices",
+                column: "LocationId",
+                principalTable: "UserAccess_Location",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
