ed9e16cf2dd41b219cfd3223bed9ce772e6e485a
JJablonski02 <kuba2805@icloud.com>
2023-12-17 16:03:20 +0100

    email sender interface

---
+++ src/Modules/Commander/Jookli.Commander.Infrastructure/Configuration/Processing/Emails/IEmailSender.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.Commander.Infrastructure.Configuration.Processing.Emails.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Infrastructure.Configuration.Processing.Emails
+{
+    public interface IEmailSender
+    {
+        string DefaultSenderAddress { get; }
+
+        string DefaultSenderName { get; }
+
+        void Send(Action<EmailMessageConfigurationBuilder> mailMessageBuild);
+
+        Task SendAsync(Action<EmailMessageConfigurationBuilder> mailMessageBuild);
+
+        void Send(MailMessage mailMessage);
+
+        Task SendAsync(MailMessage mailMessage);
+
+        void Send(string from, string to, string subject, string body, bool isBodyHtml);
+
+        Task SendAsync(string from, string to, string subject, string body, bool isBodyHtml);
+    }
+}
