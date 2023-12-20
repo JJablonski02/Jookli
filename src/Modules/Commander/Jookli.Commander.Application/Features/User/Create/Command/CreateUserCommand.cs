d19056619e9278055abd0ffc915876d5d9b3054c
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:39:25 +0100

    create user command adjustments

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/User/Create/Command/CreateUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.User.Create.Command
+{
+    public class CreateUserCommand : InternalCommandBase
+    {
+        public CreateUserCommand(Guid id, Guid userId, string email, string firstName, string lastName, bool isDeleted) : base(id)
+        {
+            UserId = userId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            IsDeleted = isDeleted;
+        }
+        public Guid UserId { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
