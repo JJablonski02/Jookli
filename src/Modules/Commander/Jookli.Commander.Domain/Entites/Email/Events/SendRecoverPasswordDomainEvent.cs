8c44b26d7cde40d8729f7ea7567e7334b500889c
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:40:11 +0100

    send recover password domain event

---
+++ src/Modules/Commander/Jookli.Commander.Domain/Entites/Email/Events/SendRecoverPasswordDomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Domain.Entites.Email.Events
+{
+    public class SendRecoverPasswordDomainEvent : DomainEventBase
+    {
+        public Guid EmailId { get; set; }
+        public SendRecoverPasswordDomainEvent(Guid emailId)
+        {
+            EmailId = emailId;
+        }
+    }
+}
