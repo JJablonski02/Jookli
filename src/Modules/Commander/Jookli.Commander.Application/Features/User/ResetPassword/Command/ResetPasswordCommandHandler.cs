0e47c569948abe34069ff97984f2b4795bd68299
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:39:47 +0100

    reset password command handler

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/User/ResetPassword/Command/ResetPasswordCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using Jookli.Commander.Domain.Entites.User.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.User.ResetPassword.Command
+{
+    internal class ResetPasswordCommandHandler : ICommandHandler<ResetPasswordCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ResetPasswordCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(ResetPasswordCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(command.UserId, cancellationToken);
+
+            user.Password = command.Password;
+
+            return Unit.Value;
+        }
+    }
+}
