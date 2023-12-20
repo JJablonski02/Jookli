76ff8280f772da882fbe003c73131a7130635dbe
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:39:56 +0100

    reset password event handler

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/User/ResetPassword/Incoming/ResetPasswordEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using Jookli.Commander.Application.Features.User.ResetPassword.Command;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.User.ResetPassword.Incoming
+{
+    internal class ResetPasswordEventHandler : INotificationHandler<UserPasswordChangedIntegrationEvent>
+    {
+        private readonly ICommandsScheduler _commandsScheduler;
+        public ResetPasswordEventHandler(ICommandsScheduler commandsScheduler)
+        {
+            _commandsScheduler = commandsScheduler;
+        }
+        public async Task Handle(UserPasswordChangedIntegrationEvent notification, CancellationToken cancellationToken)
+        {
+            await _commandsScheduler.EnqueueAsync(new ResetPasswordCommand(notification.Id, notification.UserId, notification.Password));
+        }
+    }
+}
