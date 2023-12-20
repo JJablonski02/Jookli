3886c85a7ff1f489a15fb373a1d123a40c37a209
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:38:35 +0100

    send recover password command

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/SendRecoverPassword/Command/SendRecoverPasswordCommand.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+
+namespace Jookli.Commander.Application.Features.Email.SendRecoverPassword.Command
+{
+    public class SendRecoverPasswordCommand : InternalCommandBase
+    {
+        public SendRecoverPasswordCommand(Guid id, Guid userId, string callbackUrl) : base(id)
+        {
+            UserId = userId;
+            CallbackUrl = callbackUrl;
+        }
+        public Guid UserId { get; set; }
+        public string CallbackUrl { get; set; }
+    }
+}
