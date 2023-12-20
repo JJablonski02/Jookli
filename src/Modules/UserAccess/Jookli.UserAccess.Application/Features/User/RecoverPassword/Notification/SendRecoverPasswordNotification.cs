f4aa246f22754e090fad6dfd31e5c96d1f2651df
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:41:30 +0100

    send recover notification in useraccess

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/RecoverPassword/Notification/SendRecoverPasswordNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.RecoverPassword.Notification
+{
+    public class SendRecoverPasswordNotification : DomainNotificationBase<SendRecoverPasswordDomainEvent>
+    {
+        public SendRecoverPasswordNotification(SendRecoverPasswordDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/RecoverPassword/Notification/SendRecoverPasswordPublishEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.RecoverPassword.Notification
+{
+    public class SendRecoverPasswordPublishEventHandler : INotificationHandler<SendRecoverPasswordNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public SendRecoverPasswordPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(SendRecoverPasswordNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new UserRecoverPasswordIntegrationEvent(
+                notification.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.UserId,
+                notification.DomainEvent.CallbackUrl));
+        }
+    }
+}
