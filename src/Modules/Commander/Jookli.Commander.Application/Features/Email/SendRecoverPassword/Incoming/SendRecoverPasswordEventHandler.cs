c1224c9fbe5e0aae6390410842fa5117d63086c9
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:38:56 +0100

    send recover password event handler

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/SendRecoverPassword/Incoming/SendRecoverPasswordEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using Jookli.Commander.Application.Features.Email.SendRecoverPassword.Command;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.Email.SendRecoverPassword.Incoming
+{
+    internal class RecoverPasswordEventHandler : INotificationHandler<UserRecoverPasswordIntegrationEvent>
+    {
+        private readonly ICommandsScheduler _commandsScheduler;
+
+        public RecoverPasswordEventHandler(ICommandsScheduler commandsScheduler)
+        {
+            _commandsScheduler = commandsScheduler;
+        }
+
+        public async Task Handle(UserRecoverPasswordIntegrationEvent notification, CancellationToken cancellationToken)
+        {
+            await _commandsScheduler.EnqueueAsync(new SendRecoverPasswordCommand(
+                notification.Id, notification.UserId,
+                notification.CallbackUrl));
+        }
+    }
+}
