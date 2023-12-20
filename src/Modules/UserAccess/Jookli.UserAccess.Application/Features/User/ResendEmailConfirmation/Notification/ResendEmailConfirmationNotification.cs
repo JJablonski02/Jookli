e229cba0285bc9f84fdc7d9deae4627d15ad04b8
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:42:39 +0100

    resend email confirmation notification

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ResendEmailConfirmation/Notification/ResendEmailConfirmationNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ResendEmailConfirmation.Notification
+{
+    public class ResendEmailConfirmationNotification : DomainNotificationBase<ResendEmailConfirmationDomainEvent>
+    {
+        public ResendEmailConfirmationNotification(ResendEmailConfirmationDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ResendEmailConfirmation/Notification/ResendEmailConfirmationPublishEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ResendEmailConfirmation.Notification
+{
+    public class ResendEmailConfirmationPublishEventHandler : INotificationHandler<ResendEmailConfirmationNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public ResendEmailConfirmationPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+
+        public async Task Handle(ResendEmailConfirmationNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new SendEmailConfirmationIntegrationEvent(
+                notification.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.UserId,
+                notification.DomainEvent.Email,
+                notification.DomainEvent.CallbackUrl,
+                notification.DomainEvent.FirstName,
+                notification.DomainEvent.LastName));
+        }
+    }
+}
