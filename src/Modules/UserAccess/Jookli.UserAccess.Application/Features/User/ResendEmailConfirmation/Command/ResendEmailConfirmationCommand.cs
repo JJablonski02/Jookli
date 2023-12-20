1b9ee05a5691428f79c386f3f0d190fb9633a141
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:42:27 +0100

    resend email confirmation

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ResendEmailConfirmation/Command/ResendEmailConfirmationCommand.cs
@@ -0,0 +1,14 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ResendEmailConfirmation.Command
+{
+    public class ResendEmailConfirmationCommand : CommandBase
+    {
+        public string Email { get; set; }   
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ResendEmailConfirmation/Command/ResendEmailConfirmationCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Jookli.UserAccess.Application.Authentication;
+using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.Token;
+using Jookli.UserAccess.Domain.Entities.Token.Events;
+using Jookli.UserAccess.Domain.Entities.Token.Repository;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using Jookli.UserAccess.Domain.Entities.User.RepositoryContract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ResendEmailConfirmation.Command
+{
+    public class ResendEmailConfirmationCommandHandler : ICommandHandler<ResendEmailConfirmationCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenRepository _tokenRepository;
+
+        public ResendEmailConfirmationCommandHandler(IUserRepository userRepository, ITokenRepository tokenRepository)
+        {
+            _userRepository = userRepository;
+            _tokenRepository = tokenRepository;
+        }
+
+        public async Task<Unit> Handle(ResendEmailConfirmationCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUserEmailAsync(command.Email);
+
+            if(user == null)
+            {
+                throw new ArgumentException($"User with {command.Email} does not exist");
+            }
+
+            var token = new TokenEntity()
+            {
+                TokenId = Guid.NewGuid(),
+                UserId = user.UserId,
+                CreationDate = DateTime.UtcNow,
+                ExpirationDate = DateTime.UtcNow.AddHours(24),
+                Metadata = "Resent user confirmation token via email.",
+                IsValid = true,
+                TokenValue = TokenCryptography.sha256(Guid.NewGuid().ToString())
+            };
+
+            token.AddDomainEvent(new ResendEmailConfirmationDomainEvent(user.UserId, command.Email, token.TokenValue, user.FirstName, user.LastName));
+
+            await _tokenRepository.AddAsync(token, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
