3e07f0f41183951814a46c36685d1dd76af5a8d1
JJablonski02 <kuba2805@icloud.com>
2023-12-20 11:41:18 +0100

    send recover in useraccess

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/RecoverPassword/Command/SendRecoverPasswordCommand.cs
@@ -0,0 +1,14 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.RecoverPassword.Command
+{
+    public class SendRecoverPasswordCommand : CommandBase
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/RecoverPassword/Command/SendRecoverPasswordCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using Jookli.UserAccess.Application.Authentication;
+using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.Token;
+using Jookli.UserAccess.Domain.Entities.Token.Repository;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using Jookli.UserAccess.Domain.Entities.User.RepositoryContract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.RecoverPassword.Command
+{
+    internal class SendRecoverPasswordCommandHandler : ICommandHandler<SendRecoverPasswordCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenRepository _tokenRepository;
+        public SendRecoverPasswordCommandHandler(IUserRepository userRepository, ITokenRepository tokenRepository)
+        {
+            _userRepository = userRepository;
+            _tokenRepository = tokenRepository;
+        }
+
+        public async Task<Unit> Handle(SendRecoverPasswordCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUserEmailAsync(command.Email);
+
+            if (user == null)
+            {
+                throw new ArgumentException($"User with {command.Email} does not exist");
+            }
+
+            var token = new TokenEntity()
+            {
+                TokenId = Guid.NewGuid(),
+                UserId = user.UserId,
+                CreationDate = DateTime.UtcNow,
+                ExpirationDate = DateTime.UtcNow.AddMinutes(20),
+                Metadata = "User confirmation token via email.",
+                IsValid = true,
+                TokenValue = TokenCryptography.sha256(Guid.NewGuid().ToString())
+            };
+            string uri = $"joyprofits.com/RecoverPassword?token={token.TokenValue}";
+
+            token.AddDomainEvent(new SendRecoverPasswordDomainEvent(user.UserId, uri.ToString()));
+
+            await _tokenRepository.AddAsync(token, cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
