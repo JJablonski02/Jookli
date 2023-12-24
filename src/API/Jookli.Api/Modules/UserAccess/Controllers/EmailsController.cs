3703476f0497a0de051c87bac7e2786923e4f084
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:45:31 +0100

    send emails controller

---
+++ src/API/Jookli.Api/Modules/UserAccess/Controllers/EmailsController.cs
@@ -0,0 +1,44 @@
+﻿using Jookli.Api.Configuration.Authorization;
+using Jookli.UserAccess.Application.Contracts;
+using Jookli.UserAccess.Application.Features.Email.SendAnonymousMessage.Command;
+using Jookli.UserAccess.Application.Features.Email.SendMessage.Command;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.UserAccess.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        private readonly IUserAccessModule _userAccessModule;
+
+        public EmailsController(IUserAccessModule userAccessModule)
+        {
+            _userAccessModule = userAccessModule;
+        }
+
+        [NoPermissionRequired]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("SendAnonymous")]
+        public async Task<IActionResult> SendAnonymous(SendAnonymousMessageCommand sendAnonymousMessage)
+        {
+            await _userAccessModule.ExecuteCommandAsync(sendAnonymousMessage);
+
+            return Ok();
+        }
+
+        [NoPermissionRequired]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Send")]
+        public async Task<IActionResult> Send(SendMessageCommand sendMessageCommand)
+        {
+            await _userAccessModule.ExecuteCommandAsync(sendMessageCommand);
+
+            return Ok();
+        }
+
+    }
+}
