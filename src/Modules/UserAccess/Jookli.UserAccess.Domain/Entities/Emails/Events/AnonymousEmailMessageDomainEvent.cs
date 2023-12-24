01aedd7838869b87c6ba76f121eb60c887e51141
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:47:18 +0100

    domain evnets

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Emails/Events/AnonymousEmailMessageDomainEvent.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Emails.Events
+{
+    public class AnonymousEmailMessageDomainEvent : DomainEventBase
+    {
+        public AnonymousEmailMessageDomainEvent(Guid emailId, string contactEmailAddress, string contactPhoneNumber, string message, string signature, DateTime receivedDate)
+        {
+            EmailId = emailId;
+            ContactEmailAddress = contactEmailAddress;
+            ContactPhoneNumber = contactPhoneNumber;
+            Message = message;
+            Signature = signature;
+            ReceivedDate = receivedDate;
+        }
+
+        public Guid EmailId { get; set; }
+        public string ContactEmailAddress { get; set; }
+        public string? ContactPhoneNumber { get; set; }
+        public string Message { get; set; }
+        public string Signature { get; set; }
+        public DateTime ReceivedDate { get; set; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Emails/Events/EmailMessageDomainEvent.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Emails.Events
+{
+    public class EmailMessageDomainEvent : DomainEventBase
+    {
+        public EmailMessageDomainEvent(Guid emailId, Guid userId, string message, DateTime receivedDate)
+        {
+            EmailId = emailId;
+            UserId = userId;
+            Message = message;
+            ReceivedDate = receivedDate;
+        }
+        public Guid EmailId { get; set; }
+        public Guid UserId { get; set; }
+        public string Message { get; set; }
+        public DateTime ReceivedDate { get; set; }
+    }
+}
