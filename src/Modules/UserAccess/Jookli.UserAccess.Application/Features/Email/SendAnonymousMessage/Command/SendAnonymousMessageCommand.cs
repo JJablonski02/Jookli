3f90e78b5570ad674f882362d220085a633cb1a9
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:46:40 +0100

    send anonymous message command

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendAnonymousMessage/Command/SendAnonymousMessageCommand.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendAnonymousMessage.Command
+{
+    public class SendAnonymousMessageCommand : CommandBase
+    {
+        public string? ContactPhoneNumber { get; set; }
+        public string ContactEmailAddress { get; set; }
+        public string Message { get; set; }
+        public string Signature { get; set; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendAnonymousMessage/Command/SendAnonymousMessageCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.Emails;
+using Jookli.UserAccess.Domain.Entities.Emails.Events;
+using Jookli.UserAccess.Domain.Entities.Emails.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendAnonymousMessage.Command
+{
+    public class SendAnonymousMessageCommandHandler : ICommandHandler<SendAnonymousMessageCommand>
+    {
+        private readonly IAnonymousEmailRepository _anonymousEmailRepository;
+
+        public SendAnonymousMessageCommandHandler(IAnonymousEmailRepository anonymousEmailRepository)
+        {
+            _anonymousEmailRepository = anonymousEmailRepository;
+        }
+        public async Task<Unit> Handle(SendAnonymousMessageCommand command, CancellationToken cancellationToken)
+        {
+            var email = new AnonymousEmailEntity
+            {
+                EmailId = Guid.NewGuid(),
+                ContactEmailAddress = command.ContactEmailAddress,
+                ContactPhoneNumber = command.ContactPhoneNumber,
+                Message = command.Message,
+                Signature = command.Signature,
+                ReceivedDate = DateTime.UtcNow,
+            };
+
+            email.AddDomainEvent(new AnonymousEmailMessageDomainEvent(
+                email.EmailId,
+                email.ContactEmailAddress,
+                email.ContactPhoneNumber,
+                email.Message,
+                email.Signature,
+                email.ReceivedDate
+                ));
+
+            await _anonymousEmailRepository.AddAsync(email, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
