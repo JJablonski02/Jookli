76d536b153d4e3529009f2fe5d4c259c8d07fd36
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:47:05 +0100

    send message notification

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendMessage/Notification/SendMessageNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.Emails.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendMessage.Notification
+{
+    public class SendMessageNotification : DomainNotificationBase<EmailMessageDomainEvent>
+    {
+        public SendMessageNotification(EmailMessageDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendMessage/Notification/SendMessagePublishEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendMessage.Notification
+{
+    public class SendMessagePublishEventHandler : INotificationHandler<SendMessageNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public SendMessagePublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+        public async Task Handle(SendMessageNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new SendMessageIntegrationEvent(
+                notification.Id,
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.EmailId,
+                notification.DomainEvent.UserId,
+                notification.DomainEvent.Message,
+                notification.DomainEvent.ReceivedDate
+                ));
+        }
+    }
+}
