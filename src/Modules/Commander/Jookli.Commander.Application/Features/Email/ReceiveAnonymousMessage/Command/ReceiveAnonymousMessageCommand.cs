b37c876356ed70376df6486c1598898ba686fdc6
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:45:46 +0100

    receive anonymous command

---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/ReceiveAnonymousMessage/Command/ReceiveAnonymousMessageCommand.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.Email.ReceiveAnonymousMessage.Command
+{
+    public class ReceiveAnonymousMessageCommand : InternalCommandBase
+    {
+        public ReceiveAnonymousMessageCommand(Guid id, Guid emailId, string contactEmailAddress, string contactPhoneNumber, string message, string signature, DateTime receivedDate) : base(id)
+        {
+            EmailId = emailId;
+            ContactEmailAddress = contactEmailAddress;
+            ContactPhoneNumber = contactPhoneNumber;
+            Message = message;
+            Signature = signature;
+            ReceivedDate = receivedDate;
+        }
+
+        public Guid EmailId { get; }
+        public string ContactEmailAddress { get; }
+        public string? ContactPhoneNumber { get; }
+        public string Message { get; }
+        public string Signature { get; }
+        public DateTime ReceivedDate { get; }
+    }
+}
---
+++ src/Modules/Commander/Jookli.Commander.Application/Features/Email/ReceiveAnonymousMessage/Command/ReceiveAnonymousMessageCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Jookli.Commander.Application.Configuration.Command;
+using Jookli.Commander.Domain.Entites.Email;
+using Jookli.Commander.Domain.Entites.Email.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Commander.Application.Features.Email.ReceiveAnonymousMessage.Command
+{
+    internal class ReceiveAnonymousMessageCommandHandler : ICommandHandler<ReceiveAnonymousMessageCommand>
+    {
+        private readonly IEmailRepository _emailRepository;
+
+        public ReceiveAnonymousMessageCommandHandler(IEmailRepository emailRepository)
+        {
+            _emailRepository = emailRepository;
+        }
+        public async Task<Unit> Handle(ReceiveAnonymousMessageCommand request, CancellationToken cancellationToken)
+        {
+            return Unit.Value;
+        }
+    }
+}
