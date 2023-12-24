5a345e58315b4cfe875fa49dd520abf6c03aeaab
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:46:49 +0100

    send anonymous notification

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendAnonymousMessage/Notification/SendAnonymousMessageNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.Emails.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendAnonymousMessage.Notification
+{
+    public class SendAnonymousMessageNotification : DomainNotificationBase<AnonymousEmailMessageDomainEvent>
+    {
+        public SendAnonymousMessageNotification(AnonymousEmailMessageDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendAnonymousMessage/Notification/SendAnonymousMessagePublishEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendAnonymousMessage.Notification
+{
+    public class SendAnonymousMessagePublishEventHandler : INotificationHandler<SendAnonymousMessageNotification>
+    {
+        private readonly IEventsBus _eventsBus;
+
+        public SendAnonymousMessagePublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus;
+        }
+        public async Task Handle(SendAnonymousMessageNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new SendAnonymousMessageIntegrationEvent(
+                notification.Id, 
+                notification.DomainEvent.OccuredOn,
+                notification.DomainEvent.EmailId,
+                notification.DomainEvent.ContactEmailAddress,
+                notification.DomainEvent.ContactPhoneNumber,
+                notification.DomainEvent.Message,
+                notification.DomainEvent.Signature,
+                notification.DomainEvent.ReceivedDate));
+        }
+    }
+}
