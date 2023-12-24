4e8785a90e0a8a0f8571cf267e12f6da5a26ed11
JJablonski02 <kuba2805@icloud.com>
2023-12-24 16:41:14 +0100

    update user domain event

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/User/Events/UpdateUserAccountStatusDomainEvent.cs
@@ -0,0 +1,22 @@
+﻿using Jookli.BuildingBlocks.Domain;
+using Jookli.UserAccess.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.User.Events
+{
+    public class UpdateUserAccountStatusDomainEvent : DomainEventBase
+    {
+        public UpdateUserAccountStatusDomainEvent(Guid userId, AccountStatus accountStatus)
+        {
+            UserId = userId;
+            AccStatus = accountStatus;
+        }
+        
+        public Guid UserId { get; set; }
+        public AccountStatus AccStatus { get; set; }
+    }
+}
