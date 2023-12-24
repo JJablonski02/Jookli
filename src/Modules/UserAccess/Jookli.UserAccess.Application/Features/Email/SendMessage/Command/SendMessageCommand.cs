87db670b99cfe08b0c631b82aef5102822f60f83
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:46:57 +0100

    send message command

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendMessage/Command/SendMessageCommand.cs
@@ -0,0 +1,15 @@
+﻿using Jookli.UserAccess.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendMessage.Command
+{
+    public class SendMessageCommand : CommandBase
+    {
+        public Guid UserId { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/Email/SendMessage/Command/SendMessageCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Jookli.UserAccess.Application.Configuration.Command;
+using Jookli.UserAccess.Domain.Entities.Emails;
+using Jookli.UserAccess.Domain.Entities.Emails.Events;
+using Jookli.UserAccess.Domain.Entities.Emails.Repository;
+using Jookli.UserAccess.Domain.Entities.User.RepositoryContract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.Email.SendMessage.Command
+{
+    public class SendMessageCommandHandler : ICommandHandler<SendMessageCommand>
+    {
+        private readonly IEmailRepository _emailRepository;
+        private readonly IUserRepository _userRepository;
+        public SendMessageCommandHandler(IEmailRepository emailRepository, IUserRepository userRepository)
+        {
+            _emailRepository = emailRepository;
+            _userRepository = userRepository;
+        }
+        public async Task<Unit> Handle(SendMessageCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUserIdAsync(command.UserId, cancellationToken);
+
+            if(user == null)
+            {
+                throw new ArgumentException($"User with id {command.UserId} does not exist");
+            }
+
+            var email = new EmailEntity
+            {
+                EmailId = Guid.NewGuid(),
+                UserId = command.UserId,
+                Message = command.Message,
+                ReceivedDate = DateTime.UtcNow,
+            };
+
+            email.AddDomainEvent(new EmailMessageDomainEvent(email.EmailId, email.UserId, email.Message, email.ReceivedDate));
+
+            await _emailRepository.AddAsync(email, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
