5333d50df77aeedc479e419a7870fe30977211fd
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:47:33 +0100

    new interfaces

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Emails/Repository/IAnonymousEmailRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Emails.Repository
+{
+    public interface IAnonymousEmailRepository
+    {
+        Task<AnonymousEmailEntity?> GetByEmailIdAsync(Guid emailId, CancellationToken cancellationToken);
+        Task AddAsync(AnonymousEmailEntity entity, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Domain/Entities/Emails/Repository/IEmailRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Domain.Entities.Emails.Repository
+{
+    public interface IEmailRepository
+    {
+        Task<EmailEntity?> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<EmailEntity?> GetByEmailIdAsync(Guid emailId, CancellationToken cancellationToken);
+        Task AddAsync(EmailEntity entity, CancellationToken cancellationToken);
+    }
+}
