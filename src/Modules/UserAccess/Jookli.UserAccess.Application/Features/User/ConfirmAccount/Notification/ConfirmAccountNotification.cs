3c1b893cb708b6873b9f712b85d4552a011770e7
JJablonski02 <kuba2805@icloud.com>
2023-12-24 16:40:40 +0100

    confirm account notification

---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ConfirmAccount/Notification/ConfirmAccountNotification.cs
@@ -0,0 +1,17 @@
+﻿using Jookli.BuildingBlocks.Application.Events;
+using Jookli.UserAccess.Domain.Entities.User.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ConfirmAccount.Notification
+{
+    public class ConfirmAccountNotification : DomainNotificationBase<UpdateUserAccountStatusDomainEvent>
+    {
+        public ConfirmAccountNotification(UpdateUserAccountStatusDomainEvent domainEvent, Guid id) : base(domainEvent, id)
+        {
+        }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.Application/Features/User/ConfirmAccount/Notification/ConfirmAccountPublishEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using Jookli.UserAccess.IntegrationEvents;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.Application.Features.User.ConfirmAccount.Notification
+{
+    public class ConfirmAccountPublishEventHandler : INotificationHandler<ConfirmAccountNotification>
+    {
+        public readonly IEventsBus _eventsBus;
+
+        public ConfirmAccountPublishEventHandler(IEventsBus eventsBus)
+        {
+            _eventsBus = eventsBus; 
+        }
+        public async Task Handle(ConfirmAccountNotification notification, CancellationToken cancellationToken)
+        {
+            await _eventsBus.Publish(new UpdateUserAccountStatusIntegrationEvent(
+                notification.Id, 
+                notification.DomainEvent.OccuredOn, 
+                notification.DomainEvent.UserId, 
+                notification.DomainEvent.AccStatus));
+        }
+    }
+}
