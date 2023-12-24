9a67f13d33848f58207a93829dfccfd7382815c5
JJablonski02 <kuba2805@icloud.com>
2023-12-24 23:48:15 +0100

    message/anonymous integration events

---
+++ src/Modules/UserAccess/Jookli.UserAccess.IntegrationEvents/SendAnonymousMessageIntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.IntegrationEvents
+{
+    public class SendAnonymousMessageIntegrationEvent : IntegrationEvent
+    {
+        public SendAnonymousMessageIntegrationEvent(Guid id, DateTime occuredOn, Guid emailId, 
+            string contactEmailAddress, string? contactPhoneNumber, string message, string signature, DateTime receivedDate) : base(id, occuredOn)
+        {
+            EmailId = emailId;
+            ContactEmailAddress = contactEmailAddress;
+            ContactPhoneNumber = contactPhoneNumber;
+            Message = message;
+            Signature = signature;
+        }
+        public Guid EmailId { get; }
+        public string ContactEmailAddress { get; }
+        public string? ContactPhoneNumber { get; }
+        public string Message { get; }
+        public string Signature { get; }
+        public DateTime ReceivedDate { get; }
+    }
+}
---
+++ src/Modules/UserAccess/Jookli.UserAccess.IntegrationEvents/SendMessageIntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using Jookli.BuildingBlocks.Infrastructure.EventsBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.UserAccess.IntegrationEvents
+{
+    public class SendMessageIntegrationEvent : IntegrationEvent
+    {
+        public SendMessageIntegrationEvent(Guid id, DateTime occuredOn, Guid emailId, Guid userId, string message, DateTime receivedDate) : base(id, occuredOn)
+        {
+            EmailId = emailId;
+            UserId = userId;
+            Message = message;
+            ReceivedDate = receivedDate;
+        }
+        public Guid EmailId { get; }
+        public Guid UserId { get; }
+        public string Message { get; }
+        public DateTime ReceivedDate { get; }
+    }
+}
