8da139316ca3deeeff0e1577a48285b6ea0649f7
JJablonski02 <kuba2805@icloud.com>
2023-12-27 20:30:42 +0100

    Administrator Controller

---
+++ src/API/Jookli.Api/Modules/Administrator/AdministratorAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Jookli.Administrator.Application.Contracts;
+using Jookli.Games.Infrastructure;
+
+namespace Jookli.Api.Modules.Administrator
+{
+    public class AdministratorAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AdministratorModule>()
+                .As<IAdministratorModule>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ src/API/Jookli.Api/Modules/Administrator/Controllers/AdministratorController.cs
@@ -0,0 +1,29 @@
+﻿using Jookli.Administrator.Application.Contracts;
+using Jookli.Api.Configuration.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.Administrator.Controllers
+{
+    [Route("api/administrator")]
+    [ApiController]
+    public class AdministratorController : ControllerBase
+    {
+        private readonly IAdministratorModule _administratorModule;
+
+        public AdministratorController(IAdministratorModule administratorModule)
+        {
+            _administratorModule = administratorModule;
+        }
+
+        [AllowAnonymous]
+        [NoPermissionRequired]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAdministratorStatus()
+        {
+            //To do
+            return Ok();
+        }
+    }
+}
