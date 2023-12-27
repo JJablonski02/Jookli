ae09256c56d412c27c0e497a8f3a6898bcb65381
JJablonski02 <kuba2805@icloud.com>
2023-12-27 20:37:21 +0100

    administrator context

---
+++ src/Modules/Administrator/Jookli.Administrator.Infrastructure/AdministratorContext.cs
@@ -0,0 +1,27 @@
+﻿using Jookli.BuildingBlocks.Application.Outbox;
+using Jookli.BuildingBlocks.Infrastructure.Inbox;
+using Jookli.BuildingBlocks.Infrastructure.InternalCommands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Jookli.Administrator.Infrastructure
+{
+    public class AdministratorContext : DbContext
+    {
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+        public DbSet<InternalCommand> InternalCommands { get; set; }
+        public DbSet<InboxMessage> InboxMessage { get; set; }
+
+        private readonly ILoggerFactory _logger;
+        public AdministratorContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _logger = loggerFactory;
+        } 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
