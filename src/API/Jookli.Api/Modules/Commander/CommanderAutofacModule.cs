36cb1a502ed247cb6c287cfc4fc2da9968f2469c
JJablonski02 <kuba2805@icloud.com>
2023-12-27 20:30:53 +0100

    Commander Controller

---
+++ src/API/Jookli.Api/Modules/Commander/CommanderAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Jookli.Commander.Application.Contracts;
+using Jookli.Commander.Infrastructure;
+
+namespace Jookli.Api.Modules.Commander
+{
+    public class CommanderAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CommanderModule>()
+                .As<ICommanderModule>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ src/API/Jookli.Api/Modules/Commander/Controllers/CommanderController.cs
@@ -0,0 +1,27 @@
+﻿using Jookli.Api.Configuration.Authorization;
+using Jookli.Commander.Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jookli.Api.Modules.Commander.Controllers
+{
+    [Route("api/commander")]
+    [ApiController]
+    public class CommanderController : ControllerBase
+    {
+        private readonly ICommanderModule _commanderModule;
+
+        public CommanderController(ICommanderModule commanderModule)
+        {
+            _commanderModule = commanderModule;
+        }
+        [AllowAnonymous]
+        [NoPermissionRequired]
+        [HttpGet("")]
+        public async Task<IActionResult> GetModuleStatus()
+        {
+            //To do
+            return Ok();
+        }
+    }
+}
