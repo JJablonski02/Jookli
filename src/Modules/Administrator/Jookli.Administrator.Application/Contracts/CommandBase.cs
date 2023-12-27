33955373b63fe04eabf1ee412bef74fdac8bbc9d
JJablonski02 <kuba2805@icloud.com>
2023-12-27 20:36:18 +0100

    Contracts within administraor application/infrastructure

---
+++ src/Modules/Administrator/Jookli.Administrator.Application/Contracts/CommandBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Administrator.Application.Contracts
+{
+    public abstract class CommandBase : ICommand
+    {
+        public Guid Id { get; }
+        protected CommandBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        protected CommandBase(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+
+    public abstract class CommandBase<TResult> : ICommand<TResult>
+    {
+        public Guid Id { get; }
+        public CommandBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public CommandBase(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Modules/Administrator/Jookli.Administrator.Application/Contracts/IAdministratorModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jookli.Administrator.Application.Contracts
+{
+    public interface IAdministratorModule
+    {
+        Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+        Task ExecuteCommandAsync(ICommand command);
+        Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+    }
+}
