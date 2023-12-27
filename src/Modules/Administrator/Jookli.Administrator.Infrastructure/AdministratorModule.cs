3a3bf7c7e38f81e221ae6de76e3a65497993b1be
JJablonski02 <kuba2805@icloud.com>
2023-12-27 20:37:29 +0100

    administrator module

---
+++ src/Modules/Administrator/Jookli.Administrator.Infrastructure/AdministratorModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Jookli.Administrator.Application.Contracts;
+using Jookli.Administrator.Infrastructure.Configuration;
+using Jookli.Administrator.Infrastructure.Configuration.Processing;
+using MediatR;
+
+namespace Jookli.Games.Infrastructure
+{
+    public class AdministratorModule : IAdministratorModule
+    {
+        public async Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command)
+        {
+            return await CommandsExecutor.Execute(command);
+        }
+
+        public async Task ExecuteCommandAsync(ICommand command)
+        {
+            await CommandsExecutor.Execute(command);
+        }
+
+        public async Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
+        {
+            using (var scope = AdministratorCompositionRoot.BeginLifetimeScope())
+            {
+                var mediator = scope.Resolve<IMediator>();
+                return await mediator.Send(query);
+            }
+        }
+    }
+}
